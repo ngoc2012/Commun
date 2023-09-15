b914040448908754dd9a043ff4a7b87aa3121ce5
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-15 22:06:01 +0200

    all

--- Minishell/test.sh
+++ Minishell/test.sh
@@ -1,8 +1,8 @@
 #make re && valgrind --suppressions=rl.supp --leak-check=full --show-leak-kinds=all ./minishell
-make re && valgrind --leak-check=full --show-leak-kinds=all ./minishell
+#make re && valgrind --leak-check=full --show-leak-kinds=all ./minishell
 #make && valgrind --leak-check=full ./minishell
 #make re && valgrind --leak-check=full --track-fds=yes --show-leak-kinds=all ./minishell
-#make re && valgrind --suppressions=rl.supp --leak-check=full --track-fds=yes --show-leak-kinds=all ./minishell
+make re && valgrind --suppressions=rl.supp --leak-check=full --track-fds=yes --show-leak-kinds=all ./minishell
 #make && valgrind --tool=helgrind --suppressions=rl.supp --track-fds=yes ./minishell
 
 #echo "echo test tout;echo $?; exit" | ./minishell
