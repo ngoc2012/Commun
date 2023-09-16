2d86fc8a6b3f5f4d97b2fee0c72cf80e2846026c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-16 21:20:01 +0200

    all

--- Minishell/libft/ft_array_int.c
+++ Minishell/libft/ft_array_int.c
@@ -6,14 +6,14 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 19:24:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/16 21:17:41 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/16 21:18:10 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-int	aint_addback(int *a, int n)
+int	*aint_addback(int *a, int n)
 {
 	int	*new;
 	int	i;
@@ -21,6 +21,8 @@ int	aint_addback(int *a, int n)
 
 	len = sizeof(a) / sizeof(int);
 	new = malloc(sizeof(int) * (len + 1));
+	if (!new)
+		return (a);
 	i = -1;
 	while (++i < len) 
 		new[i] = a[i];
