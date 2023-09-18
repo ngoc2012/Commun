ffccc41529bee4ba63fa8dc06a23d0ded2f7e2b2
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-18 14:10:01 +0200

    all

--- Minishell/srcs/prompt.c
+++ Minishell/srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 21:34:57 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/18 14:04:42 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/18 14:08:48 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,6 @@ void	get_prompt(t_m *m, char *prompt)
 	char	*s;
 
 	ft_strlcpy(prompt, "minishell:", PATH_MAX);
-	ft_putstr_fd(prompt, 1);
-	/*
-	//ft_strlcpy(prompt, COLOR_BOLD_GREEN, PATH_MAX);
-	//ft_strlcpy(prompt, "minishell:", PATH_MAX);
-	//ft_strlcat(prompt, COLOR_OFF, PATH_MAX);
-	ft_strlcat(prompt, COLOR_BOLD_CYAN, PATH_MAX);
 	s = short_link(m->cwd, m);
 	if (s)
 	{
@@ -43,9 +37,7 @@ void	get_prompt(t_m *m, char *prompt)
 	}
 	else
 		ft_strlcat(prompt, m->cwd, PATH_MAX);
-	//ft_strlcat(prompt, COLOR_OFF, PATH_MAX);
 	ft_strlcat(prompt, "$ ", PATH_MAX);
-	*/
 }
 
 void	append_hc(t_m *m)
