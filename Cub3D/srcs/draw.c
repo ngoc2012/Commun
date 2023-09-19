c1b47e91b436b3a45c36052f3da8048eff78e4ac
Minh ngoc NGUYEN <minh-ngu@made-f0Cr3s1.clusters.42paris.fr>
2023-09-19 14:48:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/19 14:45:00 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/09/19 14:47:37 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,18 @@ void	draw_wall(t_game *g)
 		printf("2 - ai = %f\n", ai );
 		// CHECKING HORIZONTAL INTERSECTIONS
 		//Find A
+		if (ai == 90)
+		{
+			Apy = (g->pos.py / BOX_SIZE) * BOX_SIZE - 1;
+			dy = BOX_SIZE;
+			dx = 0;
+		}
+		else if (ai == -90)
+		{
+			Apy = (g->pos.py / BOX_SIZE) * BOX_SIZE + BOX_SIZE;
+			dy = BOX_SIZE;
+			dx = 0;
+		}
 		if (ai == 90)
 		{
 			Apy = (g->pos.py / BOX_SIZE) * BOX_SIZE - 1;
