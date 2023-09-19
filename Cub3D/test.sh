36481e8ce81ae7ed52a3132bf42f640b446ec49e
Minh ngoc NGUYEN <minh-ngu@made-f0Cr3s1.clusters.42paris.fr>
2023-09-19 13:32:02 +0200

    all

--- Cub3D/test.sh
+++ Cub3D/test.sh
@@ -1,10 +1,2 @@
-clear && make && make clean && valgrind --leak-check=full --show-leak-kinds=all ./cub3D
-#make && valgrind --leak-check=full ./minishell
-#make && valgrind --suppressions=rl.supp --leak-check=full --track-fds=yes --show-leak-kinds=all ./minishell
-#make && valgrind --tool=helgrind --suppressions=rl.supp --track-fds=yes ./minishell
-
-#echo "echo test tout;echo $?; exit" | ./minishell
-#echo "echo test tout;echo $?; exit" | bash
-#TEST2=$(echo "echo test tout; exit" | bash 2>&-)
-#echo "$TEST2"
+clear && make re && make clean && valgrind --leak-check=full --show-leak-kinds=all ./cub3D
 
