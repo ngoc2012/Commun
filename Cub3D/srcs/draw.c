78a1d4e5c162afc7bd260e1a6d79da3da2a92a98
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-20 14:04:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/20 14:01:32 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/20 14:03:52 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void	draw_wall(t_game *g)
 	tol_h= 10.0 / (double) g->map.h / BOX_SIZE;
 	tol_l = 10.0 / (double) g->map.l / BOX_SIZE;
 	printf("tol_h = %f, tol_l = %f\n", tol_h, tol_l);
-	g->pos.alpha = 150;
+	g->pos.alpha = 155;
 	int	deli = 0;
 	ix = WIDTH / 2 - 1 + deli;
 	while (++ix < WIDTH / 2 + 1 + deli)
@@ -143,16 +143,16 @@ void	draw_wall(t_game *g)
 			printf("First Apx = %f, dpx = %.20e\n", Apx, dpx);
 			Ax = Apx / BOX_SIZE;
 			Ay = Apy / BOX_SIZE;
-			if (!(Ax >= 0 && Ax <= g->map.l - 1))
+			if (!(Apx >= 0 && Ax <= g->map.l - 1))
 				dA = INFINI;
-			while (Ax >= 0 && Ax <= g->map.l - 1 && !g->map.v[Ay][Ax])
+			while (Apx >= 0 && Ax <= g->map.l - 1 && !g->map.v[Ay][Ax])
 			{
 				Apx += dpx;
 				Apy += dpy;
 				Ax = Apx / BOX_SIZE;
 				Ay = Apy / BOX_SIZE;
 			}
-			if (Ax < 0 || Ax > g->map.l - 1)
+			if (Apx < 0 || Ax > g->map.l - 1)
 				dA = INFINI;
 			else
 			{
