a6a9b9031b00fdd750c726794927d7ec842348e9
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-20 22:02:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/20 21:59:58 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/20 22:01:38 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ void	draw_wall(t_game *g)
 	tol_h= 1.0 / (double) g->map.h / BOX_SIZE;
 	tol_l = 1.0 / (double) g->map.l / BOX_SIZE;
 	printf("tol_h = %f, tol_l = %f\n", tol_h, tol_l);
-	g->pos.alpha = 195;
+	g->pos.alpha = 270;
 	int	deli = 0;
 	ix = WIDTH / 2 - 1 + deli;
 	while (++ix < WIDTH / 2 + 1 + deli)
