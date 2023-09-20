5e7a8b4aab96f021e3479f6441479d7402bdccbe
Minh ngoc NGUYEN <minh-ngu@made-f0Ar4s11.clusters.42paris.fr>
2023-09-20 10:04:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/20 10:01:37 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/09/20 10:03:42 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,7 +73,7 @@ void	draw_wall(t_game *g)
 				Bpx = (g->pos.px / BOX_SIZE) * BOX_SIZE - 1;
 				dpx = -BOX_SIZE;
 			}
-			Bpy = g->pos.py + (Bpx - g->pos.px) * tan(ai * PI / 180.0);
+			Bpy = g->pos.py + (g->pos.px - Bpx) * tan(ai * PI / 180.0);
 			dpy = BOX_SIZE * tan(ai * PI / 180.0);
 			Bx = Bpx / BOX_SIZE;
 			By = g->pos.y;
@@ -101,7 +101,7 @@ void	draw_wall(t_game *g)
 				Apy = (g->pos.py / BOX_SIZE) * BOX_SIZE + BOX_SIZE;
 				dpy = BOX_SIZE;
 			}
-			Apx = g->pos.px + (Apy - g->pos.py) * cos(ai * PI / 180.0) / sin(ai * PI / 180.0);
+			Apx = g->pos.px + (g->pos.py - Apy) * cos(ai * PI / 180.0) / sin(ai * PI / 180.0);
 			dpx = BOX_SIZE * cos(ai * PI / 180.0) / sin(ai * PI / 180.0);
 			Ay = Apy / BOX_SIZE;
 			Ax = g->pos.x;
