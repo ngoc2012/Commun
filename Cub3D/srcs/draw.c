58445223ab4c9c803287e69e91e2674951a9dcd4
Minh ngoc NGUYEN <minh-ngu@made-f0Ar4s11.clusters.42paris.fr>
2023-09-20 09:24:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/20 09:18:04 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/09/20 09:23:57 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,10 @@ void	draw_wall(t_game *g)
 		//if (ai == 0.0 || 180.0)
 		if ((-tol < ai && ai < tol) || (180.0 - tol < ai && ai < 180.0 + tol))
 		{
-			Bpx = (g->pos.px / BOX_SIZE) * BOX_SIZE + BOX_SIZE;
+			if (ai < tol)
+				Bpx = (g->pos.px / BOX_SIZE) * BOX_SIZE + BOX_SIZE;
+			else
+				Bpx = (g->pos.px / BOX_SIZE) * BOX_SIZE - 1;
 			Bpy = g->pos.py + (Bpx - g->pos.px) * tan(ai * PI / 180.0);
 			dpx = BOX_SIZE;
 			dpy = BOX_SIZE * tan(ai * PI / 180.0);
