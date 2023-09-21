64373dc70e1d76733b1b3b1c7f98cab53fa3a008
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-21 12:32:18 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/21 12:14:01 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/21 12:18:41 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	redraw(t_game *g)
  
 void	draw_wall(t_game *g)
 {
-	printf("draw wall\n");
+	//printf("draw wall\n");
 	int	ix;
 	int	Xp;
 	double	ai;
