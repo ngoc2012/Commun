0479978883ffb3318c3f4526f1c70f8b96054a02
Minh ngoc NGUYEN <minh-ngu@made-f0Br3s8.clusters.42paris.fr>
2023-09-21 16:18:02 +0200

    all

--- Minishell/srcs/main.c
+++ Minishell/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nbechon <nbechon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 09:51:49 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/21 15:47:33 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/09/21 16:17:14 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,7 +115,6 @@ int	main(int argc, char **argv, char **env)
 		m.env = astr_copy(env);
 		if (match_env(m.env, "SHLVL") == -1)
 			m.env = astr_addback(m.env, ft_strdup("SHLVL=1"));
-
 	}
 	init(&m);
 	tcgetattr(STDIN_FILENO, &term);
