e4aa490bc1316c8fffefe58ff82b14e0ca215852
Minh ngoc NGUYEN <minh-ngu@made-f0Br3s8.clusters.42paris.fr>
2023-09-21 16:34:01 +0200

    all

--- Minishell/test.sh
+++ Minishell/test.sh
@@ -2,9 +2,9 @@
 #make re && valgrind --leak-check=full --show-leak-kinds=all ./minishell
 #make && valgrind --leak-check=full ./minishell
 #make re && valgrind --leak-check=full --track-fds=yes --show-leak-kinds=all ./minishell
-#make re && make clean && valgrind --trace-children=yes --suppressions=rl.supp --leak-check=full --track-fds=yes --show-leak-kinds=all ./minishell
+make re && make clean && valgrind --trace-children=yes --suppressions=rl.supp --leak-check=full --track-fds=yes --show-leak-kinds=all ./minishell
 #make re && make clean && valgrind --trace-children=yes --suppressions=rl.supp --leak-check=full --track-fds=yes --show-leak-kinds=all env -i ./minishell
-make re && make clean && valgrind --trace-children=yes --track-fds=yes env -i ./minishell
+#make re && make clean && valgrind --trace-children=yes --track-fds=yes env -i ./minishell
 #make && valgrind --tool=helgrind --suppressions=rl.supp --track-fds=yes ./minishell
 
 #echo "echo test tout;echo $?; exit" | ./minishell
