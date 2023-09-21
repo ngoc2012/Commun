15e1118982e6293d3acf346c6b37fd98a887524c
Minh ngoc NGUYEN <minh-ngu@made-f0Br3s8.clusters.42paris.fr>
2023-09-21 16:40:02 +0200

    all

--- Minishell/srcs/priorities_operators.c
+++ Minishell/srcs/priorities_operators.c
@@ -6,7 +6,7 @@
 /*   By: nbechon <nbechon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 08:41:16 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/21 16:36:04 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/09/21 16:39:27 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,8 @@ static int	parentheses(char *s, t_m *m, t_c *c)
 	while (s[++(c->i)] && ft_strchr(" 	\n", s[c->i]))
 		;
 	if ((d == '(' && s[c->i] && ft_strchr(")&|", s[c->i]))
-		|| (d == ')' && s[c->i] && ft_strchr("(", s[c->i])))
-		//|| (d == ')' && s[c->i] && !ft_strchr(")&|", s[c->i])))
+		//|| (d == ')' && s[c->i] && ft_strchr("(", s[c->i])))
+		|| (d == ')' && s[c->i] && !ft_strchr(")&|", s[c->i])))
 		return (return_error(m, "syntaxe error 2", 2, 0));
 	c->i0 = c->i;
 	return (1);
