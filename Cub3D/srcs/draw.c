4209152488e3a87ca211216f0c054809448ed10a
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-22 06:30:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/22 06:26:52 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/22 06:28:45 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -236,26 +236,26 @@ void	draw_wall(t_game *g)
 		int	*addr;
 		addr = (int *)g->mlx.addr;
 		addr += ix;
-		int	start = HEIGHT / 2 - (int) h_slide / 2;
-		int	end = HEIGHT / 2 + (int) h_slide / 2;
+		int	start = HEIGHT / 2 - h_slide / 2;
+		int	end = HEIGHT / 2 + h_slide / 2;
 		addr += start * WIDTH;
 		yp = -1;
 		while (++yp < end - start)
 		{
-			//if (g->map.v[y][x] == 1)
-			//	*addr = create_trgb(1, 255, 0, 0);
-			//if (g->map.v[y][x] == 2)
-			//	*addr = create_trgb(1, 0, 255, 0);
-			//if (g->map.v[y][x] == 3)
-			//	*addr = create_trgb(1, 0, 0, 255);
-			//if (g->map.v[y][x] == 4)
-			//	*addr = create_trgb(1, 255, 255, 0);
-			//if (g->map.v[y][x] == 5)
-			//	*addr = create_trgb(1, 0, 255, 255);
-			//if (g->map.v[y][x] == 6)
-			//	*addr = create_trgb(1, 255, 0, 255);
-			//if (g->map.v[y][x] == 7)
-			//	*addr = create_trgb(1, 255, 255, 255);
+			if (g->map.v[y][x] == 1)
+				*addr = create_trgb(1, 255, 0, 0);
+			if (g->map.v[y][x] == 2)
+				*addr = create_trgb(1, 0, 255, 0);
+			if (g->map.v[y][x] == 3)
+				*addr = create_trgb(1, 0, 0, 255);
+			if (g->map.v[y][x] == 4)
+				*addr = create_trgb(1, 255, 255, 0);
+			if (g->map.v[y][x] == 5)
+				*addr = create_trgb(1, 0, 255, 255);
+			if (g->map.v[y][x] == 6)
+				*addr = create_trgb(1, 255, 0, 255);
+			if (g->map.v[y][x] == 7)
+				*addr = create_trgb(1, 255, 255, 255);
 			addr += WIDTH;
 		}
 		mlx_put_image_to_window(g->mlx.mlx, g->mlx.win, g->mlx.img, 0, 0);
