6e8feb5768bde20973960e995b9ac9798a19c4ee
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-22 09:58:01 +0200

    all

--- Cub3D/srcs/texture.c
+++ Cub3D/srcs/texture.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 05:38:38 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/22 09:33:07 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/22 09:57:50 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	get_textures(t_game *g)
 	int	ll;
 	int	ed;
 
-	g->tex_n.img = mlx_xpm_file_to_image(g->mlx.mlx, "colorstone.xpm", &g->texWidth, &g->texHeight);
+	g->tex_n.img = mlx_xpm_file_to_image(g->mlx.mlx, "colorstone.xpm", g->tex_n.l, g->tex_n.h);
 	if (!g->tex_n.img)
 		printf("Fails\n");
 	g->tex_n.addr = mlx_get_data_addr(g->tex_n.img, &g->tex_n.bpp, &g->tex_n.ll, &g->tex_n.ed);
