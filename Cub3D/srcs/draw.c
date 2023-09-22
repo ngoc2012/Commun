940549a2ff81bd05a3c879086d7ca7d660f7a3cd
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-22 15:58:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/22 15:54:45 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/22 15:57:59 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,8 +56,12 @@ void	draw_wall(t_game *g)
 	double	tol_l;
 	int	*addr;
 	int	*addr_t;
+	int	*addr_f;
+	int	*addr_c;
 	t_tex	*tex;
 
+	addr_f = (int *)g->tex_f.addr;
+	addr_c = (int *)g->tex_c.addr;
 	g->pos.Ax = -1;
 	g->pos.Ay = -1;
 	g->pos.Bx = -1;
@@ -255,11 +259,11 @@ void	draw_wall(t_game *g)
 		while (++yp < start)
 		{
 			dh = g->dpp * HEIGHT / 2 / (HEIGHT / 2 - yp);
-			xph = g->pos.xp - dh * cos(ai *PI / 180.0);
-			yph = g->pos.yp + dh * sin(ai *PI / 180.0);
+			xph = g->pos.px - dh * cos(ai *PI / 180.0);
+			yph = g->pos.py + dh * sin(ai *PI / 180.0);
 			xh = (int) (xph - ((int) xph / BOX_SIZE) * BOX_SIZE);
 			yh = (int) (yph - ((int) yph / BOX_SIZE) * BOX_SIZE);
-			*addr = *(addr_t + tx + ty * tex->l);
+			*addr = *(addr_f + xh + yh * g->tex_f.l);
 			//*addr = create_trgb(1, 255, 0, 255);
 			addr += WIDTH;
 		}
