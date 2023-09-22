383718b80fc8c83052a74790b8eaa16c201930b9
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-22 23:06:01 +0200

    all

--- Cub3D/includes/cub3D.h
+++ Cub3D/includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 09:21:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/22 22:38:54 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/22 23:04:55 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@
 # include "libft.h"
 # include "get_next_line.h"
 
-# define WIDTH 1800
-# define HEIGHT 1200
+# define WIDTH 1600
+# define HEIGHT 600
 # define FOV 90
 # define BOX_SIZE 64
 # define TRANS_SPEED 20
