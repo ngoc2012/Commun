53be2bc50c0487ae17b17720d350c2312e6657b9
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-22 22:36:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/22 22:33:35 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/22 22:35:11 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -266,8 +266,10 @@ void	draw_wall(t_game *g)
 		while (++yp < start)
 		{
 			dh = g->dpp * BOX_SIZE / 2 / (HEIGHT / 2 - yp) / cos(ai0 * PI / 180.0);
-			xph = g->pos.px - dh * cos(ai * PI / 180.0);
-			yph = g->pos.py + dh * sin(ai * PI / 180.0);
+			//xph = g->pos.px - dh * cos(ai * PI / 180.0);
+			//yph = g->pos.py + dh * sin(ai * PI / 180.0);
+			xph = g->pos.px - dh * g->cos_ai[ix][g->pos.rot];
+			yph = g->pos.py + dh * g->sin_ai[ix][g->pos.rot];
 			xh = (int) (xph - ((int) (xph / BOX_SIZE)) * BOX_SIZE);
 			yh = (int) (yph - ((int) (yph / BOX_SIZE)) * BOX_SIZE);
 			if (xh < BOX_SIZE && xh >= 0 && yh < BOX_SIZE && yh >= 0)
@@ -292,8 +294,10 @@ void	draw_wall(t_game *g)
 		while (++yp < HEIGHT)
 		{
 			dh = g->dpp * BOX_SIZE / 2 / (yp - HEIGHT / 2) / cos(ai0 * PI / 180.0);
-			xph = g->pos.px - dh * cos(ai * PI / 180.0);
-			yph = g->pos.py + dh * sin(ai * PI / 180.0);
+			//xph = g->pos.px - dh * cos(ai * PI / 180.0);
+			//yph = g->pos.py + dh * sin(ai * PI / 180.0);
+			xph = g->pos.px - dh * g->cos_ai[ix][g->pos.rot];
+			yph = g->pos.py + dh * g->sin_ai[ix][g->pos.rot];
 			xh = (int) (xph - ((int) (xph / BOX_SIZE)) * BOX_SIZE);
 			yh = (int) (yph - ((int) (yph / BOX_SIZE)) * BOX_SIZE);
 			if (xh < BOX_SIZE && xh >= 0 && yh < BOX_SIZE && yh >= 0)
