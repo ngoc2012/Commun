5f3fd9882b34d10d7ee3832f54b13b75fdfafcec
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-22 07:32:01 +0200

    all

--- Cub3D/srcs/texture.c
+++ Cub3D/srcs/texture.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 05:38:38 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/22 07:25:48 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/22 07:31:54 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	get_textures(t_game *g)
 {
 	//g->tex_n = mlx_xpm_file_to_image(g->mlx.mlx, "maps/redbrick.png", &g->texWidth, &g->texHeight);
-	g->tex_n = mlx_new_image(mlx_ptr, image_width, image_height);
+	g->tex_n = mlx_new_image(g->mlx.mlx, "maps/colorstone.xpm", BOX_SIZE, BOX_SIZE);
 	if (!g->tex_n)
 		printf("Fails\n");
 }
