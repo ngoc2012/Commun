e229e00eaa8b159c8798dac44c4dca81e63e790e
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-22 05:38:01 +0200

    all

--- Cub3D/includes/cub3D.h
+++ Cub3D/includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 09:21:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/22 05:34:20 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/22 05:38:00 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,6 +105,9 @@ typedef struct s_game {
 	t_mlx		mlx;
 	int		dpp;
 	void	*tex_n;
+	void	*tex_s;
+	void	*tex_w;
+	void	*tex_e;
 }	t_game;
 
 void	free_map(t_map *m);
