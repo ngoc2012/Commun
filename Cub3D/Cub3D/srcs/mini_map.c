79006bb1e6ecccfb63dc6bb99c1610e719973e33
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-22 13:10:01 +0200

    all

--- Cub3D/Cub3D/srcs/mini_map.c
+++
@@ -1,9 +0,0 @@
-
-
-#include "cub3D.h"
-
-void	draw_mini_map(t_game *g)
-{
-	printf("draw mini map\n");
-}
-
--- Cub3D/Cub3D/test.sh
+++
@@ -1,2 +0,0 @@
-clear && make re && make clean && valgrind --leak-check=full --show-leak-kinds=all ./cub3D
-
