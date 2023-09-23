a892fa1ab4a1c13b4ab0e0cba729d676895b04c9
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-23 18:06:01 +0200

    all

--- Cub3D/srcs/texture.c
+++ Cub3D/srcs/texture.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 05:38:38 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/23 18:03:59 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/23 18:04:31 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	get_texture(t_game *g, e_tex it, char *path)
 
 int	get_textures(t_game *g, char *fn)
 {
-	fd = open(fn, O_RDONLY);
-	if (fd == -1)
-		return (0);
+	//fd = open(fn, O_RDONLY);
+	//if (fd == -1)
+	//	return (0);
 }
