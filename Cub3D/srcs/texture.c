5e486ff50d5750c24f2cb44ab53af114679bb4ed
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-23 18:12:01 +0200

    all

--- Cub3D/srcs/texture.c
+++ Cub3D/srcs/texture.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 05:38:38 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/23 18:09:41 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/23 18:10:24 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ int	get_textures(t_game *g, char *fn)
 	get_texture(g, WE, "./walls/floorskin1695480473.xpm");
 	get_texture(g, EA, "./walls/floorskin1695480473.xpm");
 	get_texture(g, DO, "./walls/floorskin1695480473.xpm");
-	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
-	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
+	get_texture(g, FL, "./walls/floorskin1695480473.xpm");
+	get_texture(g, CL, "./walls/floorskin1695480473.xpm");
 	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
 	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
 	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
