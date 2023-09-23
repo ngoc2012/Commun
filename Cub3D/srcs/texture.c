3d9b138bd0fcbaa852446b72e7f7f65d22051630
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-23 18:10:01 +0200

    all

--- Cub3D/srcs/texture.c
+++ Cub3D/srcs/texture.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 05:38:38 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/23 18:04:31 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/23 18:09:41 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,18 @@ int	get_texture(t_game *g, e_tex it, char *path)
 
 int	get_textures(t_game *g, char *fn)
 {
+	get_texture(g, NO, "./walls/holeskin1695480474.xpm");
+	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
+	get_texture(g, WE, "./walls/floorskin1695480473.xpm");
+	get_texture(g, EA, "./walls/floorskin1695480473.xpm");
+	get_texture(g, DO, "./walls/floorskin1695480473.xpm");
+	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
+	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
+	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
+	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
+	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
+	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
+	get_texture(g, SO, "./walls/floorskin1695480473.xpm");
 	//fd = open(fn, O_RDONLY);
 	//if (fd == -1)
 	//	return (0);
