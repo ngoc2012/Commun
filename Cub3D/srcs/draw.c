2e33cb2d9a65e3d2973a6f9bda812b46b58737a4
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-23 10:06:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/23 10:04:00 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/23 10:04:54 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -160,8 +160,8 @@ void	draw_wall(t_game *g)
 				{
 					Apx += dpx;
 					Apy += dpy;
-					//Ax = Apx / BOX_SIZE;
-					Ax = (Apx + 1) / BOX_SIZE;
+					Ax = Apx / BOX_SIZE;
+					//Ax = (Apx + 1) / BOX_SIZE;
 					Ay = Apy / BOX_SIZE;
 				}
 				if (Apx < 0 || Apx >= g->map.pl)
