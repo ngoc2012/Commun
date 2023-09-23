a3c7dbb525ffec5dfd526f30324ef7069cf9dc7f
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-23 19:14:01 +0200

    all

--- Cub3D/srcs/texture.c
+++ Cub3D/srcs/texture.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 05:38:38 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/23 19:10:00 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/23 19:14:00 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,8 +53,31 @@ int	get_textures(t_game *g, char *fn)
 		{
 			s[ft_strlen(s) - 2] = 0;
 			ss = ft_split(s, ' ');
-			if (!ft_strncmp("NO", ss[0], 3) && !get_texture(g, NO, ss[1]))
-				return (return_error(s, ss));
+			if (astr_len(ss) > 1)
+			{
+				if (!ft_strncmp("NO", ss[0], 3) && !get_texture(g, NO, ss[1]))
+					return (return_error(s, ss));
+				if (!ft_strncmp("SO", ss[0], 3) && !get_texture(g, SO, ss[1]))
+					return (return_error(s, ss));
+				if (!ft_strncmp("WE", ss[0], 3) && !get_texture(g, WE, ss[1]))
+					return (return_error(s, ss));
+				if (!ft_strncmp("EA", ss[0], 3) && !get_texture(g, EA, ss[1]))
+					return (return_error(s, ss));
+				if (!ft_strncmp("DO", ss[0], 3) && !get_texture(g, DO, ss[1]))
+					return (return_error(s, ss));
+				if (!ft_strncmp("FL", ss[0], 3) && !get_texture(g, FL, ss[1]))
+					return (return_error(s, ss));
+				if (!ft_strncmp("CL", ss[0], 3) && !get_texture(g, CL, ss[1]))
+					return (return_error(s, ss));
+				if (!ft_strncmp("D3", ss[0], 3) && !get_texture(g, D3, ss[1]))
+					return (return_error(s, ss));
+				if (!ft_strncmp("D4", ss[0], 3) && !get_texture(g, D4, ss[1]))
+					return (return_error(s, ss));
+				if (!ft_strncmp("D5", ss[0], 3) && !get_texture(g, D5, ss[1]))
+					return (return_error(s, ss));
+				if (!ft_strncmp("D6", ss[0], 3) && !get_texture(g, D6, ss[1]))
+					return (return_error(s, ss));
+			}
 		}
 		free(s);
 		s = get_next_line(fd);
