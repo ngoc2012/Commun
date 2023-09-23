28fa1b00bdc3b3c163ee903657aaf72b69811b42
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-23 12:20:02 +0200

    all

--- Cub3D/includes/cub3D.h
+++ Cub3D/includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 09:21:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/23 12:16:48 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/23 12:19:12 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@
 # define FOV 90
 # define BOX_SIZE 64
 # define TRANS_SPEED 20
-# define ROT_SPEED 12
+# define ROT_SPEED 15
 # define PI 3.141592654
 # define INFINI INT_MAX
 # define COLOR_BOLD_SLOW_BLINKING      "\033[1;5m"
