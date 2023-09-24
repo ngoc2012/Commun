de44ad41e1f325fa2b499fb526049f3cad271818
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-24 16:02:01 +0200

    all

--- Cub3D/includes/cub3D.h
+++ Cub3D/includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 09:21:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/24 15:49:09 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/24 16:01:52 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@
 # define SCALE 3
 # define FOV 90
 # define BOX_SIZE 64
+# define WALL_COLISION 32
 # define TRANS_SPEED 32
 # define ROT_SPEED 15
 # define PI 3.141592654
