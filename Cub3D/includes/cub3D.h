3c2ac65510faafde0709f6113f0826c0422ff223
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-24 17:28:01 +0200

    all

--- Cub3D/includes/cub3D.h
+++ Cub3D/includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 09:21:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/24 16:20:09 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/24 17:27:55 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@
 # define WALL_COLISION 32
 # define TRANS_STEP 32
 # define ROT_STEP 15
-# define TRANS_SPEED 10
+# define TRANS_SPEED 3
 # define ROT_SPEED 10
 # define PI 3.141592654
 # define INFINI INT_MAX
