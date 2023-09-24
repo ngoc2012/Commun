f0bde095121a71570faca2f52e3458886f69bab4
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-24 16:18:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/24 15:41:01 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/24 16:16:54 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -377,6 +377,10 @@ void	draw_map(t_game *g)
 
 int	draw(t_game *g)
 {
+	if (g->frames < MAX_FRAMES)
+		g->frames++;
+	else
+		g->frames = 0;
 	render_backgroud(g);
 	render_object(&g->gun[0], (int *) g->mlx.addr, WIDTH / 2 - g->gun[0].l / 2, HEIGHT - g->gun[0].h);
 	mlx_put_image_to_window(g->mlx.mlx, g->mlx.win, g->gun[0].img, 0, 0);
