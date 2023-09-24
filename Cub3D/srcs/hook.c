45d1f7c689434ee4e90c9879363d2fbe4fcd111e
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-24 16:08:01 +0200

    all

--- Cub3D/srcs/hook.c
+++ Cub3D/srcs/hook.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/24 16:04:19 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/24 16:06:06 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,7 @@ int	key_press(int keycode, t_game *g)
 		}
 		x = (int) ((g->pos.px - dx) / BOX_SIZE);
 		y = (int) ((g->pos.py + dy) / BOX_SIZE);
-		if (x > 0 && y > 0 && x < g->map.l && y < g->map.h && !g->map.v[y][x]
-			&& )
+		if (x > 0 && y > 0 && x < g->map.l && y < g->map.h && !g->map.v[y][x])
 		{
 			g->pos.px -= dx;
 			g->pos.py += dy;
