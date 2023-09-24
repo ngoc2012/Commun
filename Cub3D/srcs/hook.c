87cb59f74db72ce9f7f9a0d7194d5ad44bfc01e4
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-24 17:22:01 +0200

    all

--- Cub3D/srcs/hook.c
+++ Cub3D/srcs/hook.c
@@ -6,12 +6,16 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/24 16:26:45 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/24 17:21:39 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "cub3D.h"
+
+int	convert_frames(int fr, int sp)
+{
+}
 
 int	key_press(int keycode, t_game *g)
 {
@@ -33,9 +37,9 @@ int	key_press(int keycode, t_game *g)
 		}
 		x = (int) ((g->pos.px - dx) / BOX_SIZE);
 		y = (int) ((g->pos.py + dy) / BOX_SIZE);
-		if ((keycode == XK_Up && g->fr_up - g->frames > TRANS_SPEED) ||
+		if (((keycode == XK_Up && g->fr_up - g->frames > TRANS_SPEED) ||
 			(keycode == XK_Down && g->fr_down - g->frames > TRANS_SPEED)) &&
-				(x > 0 && y > 0 && x < g->map.l && y < g->map.h && !g->map.v[y][x])
+				(x > 0 && y > 0 && x < g->map.l && y < g->map.h && !g->map.v[y][x]))
 		{
 			g->pos.px -= dx;
 			g->pos.py += dy;
