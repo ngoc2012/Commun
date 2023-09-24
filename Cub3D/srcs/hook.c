e22ebaecce63b34938c1921eee7033248505ec6f
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-24 11:38:01 +0200

    all

--- Cub3D/srcs/hook.c
+++ Cub3D/srcs/hook.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/24 11:35:52 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/24 11:37:54 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,7 @@ int	key_hook(int keycode, t_game *g)
 	}
 	if (keycode == XK_Control_L)
 	{
-		render_backgroud(g);
+		//render_backgroud(g);
 		render_object(&g->gun[1], (int *) g->mlx.addr, WIDTH / 2 - g->gun[1].l / 2, HEIGHT - g->gun[1].h);
 		scale_window(g);
 		//usleep(1000000);
