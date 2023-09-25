c2267e21f55d44eb5db090353118040c89aab484
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-25 20:54:01 +0200

    all

--- Cub3D/srcs/hook.c
+++ Cub3D/srcs/hook.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/25 17:38:33 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/25 20:53:15 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,8 +78,8 @@ int	key_release(int keycode, t_game *g)
 	if (keycode == XK_space)
 	{
 		printf("Space\n");
-		if (g->map.v[pos.y - 1][pos.x] == B_DOOR)
-		else if (g->map.v[pos.y - 1][pos.x] == B_DOOR)
+		//if (g->map.v[pos.y - 1][pos.x] == B_DOOR)
+		//else if (g->map.v[pos.y - 1][pos.x] == B_DOOR)
 	}
 	if (keycode == XK_Control_L || keycode == XK_Control_R)
 	{
--- Cub3D/srcs/main.c
+++ Cub3D/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/25 20:49:22 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/25 20:52:36 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,9 +90,9 @@ void	init(t_game *g)
 	g->tan_ai = 0;
 	g->cos_ai = 0;
 	g->sin_ai = 0;
-	g->opened_door_x = 0;
-	g->opened_door_y = 0;
-	g->hidden_door = 0;
+	g->opened_door_x = 5;
+	g->opened_door_y = 4;
+	g->hidden_door = 20;
 	g->gun_tex = &g->gun[0];
 }
 
