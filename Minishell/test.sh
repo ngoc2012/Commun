dfbd28484de99e11299e974c11566d4691540494
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-25 14:36:02 +0200

    all

--- Minishell/test.sh
+++ Minishell/test.sh
@@ -2,8 +2,8 @@
 #make re && valgrind --leak-check=full --show-leak-kinds=all ./minishell
 #make && valgrind --leak-check=full ./minishell
 #make re && valgrind --suppressions=rl.supp --leak-check=full --track-fds=yes --show-leak-kinds=all ./minishell
-#make re && make clean && valgrind --trace-children=yes --suppressions=rl.supp --leak-check=full --track-fds=yes --show-leak-kinds=all ./minishell
-make re && make clean && valgrind --trace-children=yes --suppressions=rl.supp --leak-check=full --track-fds=yes --show-leak-kinds=all env -i ./minishell
+make re && make clean && valgrind --trace-children=yes --suppressions=rl.supp --leak-check=full --track-fds=yes --show-leak-kinds=all ./minishell
+#make re && make clean && valgrind --trace-children=yes --suppressions=rl.supp --leak-check=full --track-fds=yes --show-leak-kinds=all env -i ./minishell
 #make re && make clean && valgrind --trace-children=yes --track-fds=yes env -i ./minishell
 #make && valgrind --tool=helgrind --suppressions=rl.supp --track-fds=yes ./minishell
 
