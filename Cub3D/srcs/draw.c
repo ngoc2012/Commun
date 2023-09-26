a78d2b6f4a3c1a7d1b51ae8ea9af08d0fa75b284
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-26 17:40:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/26 17:37:23 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/26 17:39:13 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -460,6 +460,7 @@ int	draw(t_game *g)
 
 	if (g->frames[FR_DOOR])
 	{
+		printf("frames door = %d\n", g->frames[FR_DOOR]);
 		if (g->opened && g->hidden_door < BOX_SIZE)
 			g->hidden_door++;
 		else if (g->hidden_door == BOX_SIZE && g->frames[FR_DOOR] < DOOR_IDLE)
