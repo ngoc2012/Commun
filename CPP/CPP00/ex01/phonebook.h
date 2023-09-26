054fffc0400fd4431593ae30bc101fdbdc61df0a
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-26 21:30:01 +0200

    all

--- CPP/CPP00/ex01/phonebook.h
+++ CPP/CPP00/ex01/phonebook.h
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 21:20:40 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/26 21:23:27 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/26 21:28:44 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,14 @@
 class Contact
 {
 	private:
+		std::string _firstName;
+		std::string _lastName;
+		std::string _nickname;
+		std::string _phoneNumber;
+		std::string _darkestSecret;
 	public:
+		Contact(void);
+		~Contact(void);
 }
 
 class PhoneBook
