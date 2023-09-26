64d4ed6372199966b40ec6e092d3249f77338c8a
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-26 21:34:01 +0200

    all

--- CPP/CPP00/ex01/phonebook.cpp
+++ CPP/CPP00/ex01/phonebook.cpp
@@ -6,22 +6,14 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/26 21:31:32 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/26 21:33:59 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main(void)
 {
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			for (int j = 0; argv[i][j]; j++)
-				argv[i][j] = toupper(argv[i][j]);
-			std::cout << argv[i];
-		}
-		std::cout << std::endl;
-	}
+	PhoneBook	b;
+
+	std::string	input = "";
+	std::cout << "Welcome\n";
 }
