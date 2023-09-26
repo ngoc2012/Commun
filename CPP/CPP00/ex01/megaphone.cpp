104d73fbc4ba1bccc5bcda2878790e71071564ca
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-26 21:20:01 +0200

    all

---
+++ CPP/CPP00/ex01/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
+/*   Updated: 2023/09/26 21:11:05 by ngoc             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				argv[i][j] = toupper(argv[i][j]);
+			std::cout << argv[i];
+		}
+		std::cout << std::endl;
+	}
+}
