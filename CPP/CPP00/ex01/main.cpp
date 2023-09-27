c35f295082afd3af4987dfe708b2cdfbedfb07ac
Minh ngoc NGUYEN <minh-ngu@made-f0Ar2s12.clusters.42paris.fr>
2023-09-27 09:46:01 +0200

    all

--- CPP/CPP00/ex01/main.cpp
+++ CPP/CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/27 09:43:57 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/09/27 09:45:46 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,13 @@ int	main(void)
 	std::string	input = "";
 	std::cout << "Phone Book\n> ";
 	while (std::cin >> input) {
-		if (input.compare("ADD") == 0)
+		if (!input.compare("ADD"))
 			b.add();
-		else if (input.compare("SEARCH") == 0)
+		else if (!input.compare("SEARCH"))
 			b.search();
-		else if (input.compare("EXIT") == 0)
-			break;
+		else if (!input.compare("EXIT"))
+			return (0);
 		std::cout << "> " << std::flush;
 	}
-	//if (std::cin.eof()) {
-	//	std::cout << "End of input reached (Ctrl+D was pressed)." << std::endl;
-	//} else {
-	//	std::cout << "Input error occurred." << std::endl;
-	//}
 	return (0);
 }
