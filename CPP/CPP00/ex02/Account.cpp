e09eb94d89b9933cdb86bbb599e7d1a71e6a6f14
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-27 05:56:01 +0200

    all

--- CPP/CPP00/ex02/Account.cpp
+++ CPP/CPP00/ex02/Account.cpp
@@ -6,9 +6,13 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/27 05:52:59 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/27 05:56:01 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount= 0;
+int Account::_totalNbDeposits= 0;
+int Account::_totalNbWithdrawals = 0;
