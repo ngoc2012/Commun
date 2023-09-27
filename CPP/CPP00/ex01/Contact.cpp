05407f7c2ae5c9b6981aacf27867166f789d2e11
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-27 06:32:01 +0200

    all

--- CPP/CPP00/ex01/Contact.cpp
+++ CPP/CPP00/ex01/Contact.cpp
@@ -6,14 +6,16 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/27 06:25:54 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/27 06:31:31 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+# define WIDTH 10
 
 #include "Contact.hpp"
 
 std::string printLen(std::string str, int len) {
-	if (str.length() > len)
+	if ((int) str.length() > len)
 		return str.substr(0, len - 1) + ".";
 	return str;
 }
@@ -29,55 +31,4 @@ void Contact::show(int index) const
 	std::cout << "|" << std::setw(WIDTH) << printLen( this->_lastName, WIDTH) << std::flush;
 	std::cout << "|" << std::setw(WIDTH) << printLen( this->_nickname, WIDTH) << std::flush;
 	std::cout << "|" << std::endl;
-}
-
-PhoneBook::PhoneBook(){}
-
-PhoneBook::~PhoneBook(){}
-
-void PhoneBook::add(void)
-{
-	static int	i;
-	Contact	*new = this->contacts[i % N_CONTACTS];
-
-	std::cout << "add\n";
-	//std::cout << "> " << std::flush;
-	//std::cin >> input;
-	i++
-}
-
-void PhoneBook::search(void) const
-{
-	std::cout << "search\n";
-	//std::cout << "> " << std::flush;
-	//std::cin >> input;
-}
-
-void PhoneBook::show(void) const
-{
-	std::cout << "show\n";
-	for (int i = 0; i < N_CONTACTS; i++)
-	{
-	}
-	//std::cout << "> " << std::flush;
-	//std::cin >> input;
-}
-
-int	main(void)
-{
-	PhoneBook	b;
-
-	std::string	input = "";
-	std::cout << "Phone Book\n";
-	while (input.compare("EXIT")) {
-		if (input.compare("ADD") == 0)
-			b.add();
-		else if (input.compare("SEARCH") == 0) {
-			b.show();
-			b.search();
-		}
-		std::cout << "> " << std::flush;
-		std::cin >> input;
-	}
-	return (0);
 }
