ff605d2f08f112401816049bfc14e68028910f3e
Minh ngoc NGUYEN <minh-ngu@made-f0Ar2s12.clusters.42paris.fr>
2023-09-27 09:44:02 +0200

    all

--- CPP/CPP00/ex01/main.cpp
+++ CPP/CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/27 09:41:49 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/09/27 09:43:57 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@ int	main(void)
 	PhoneBook	b;
 
 	std::string	input = "";
-	std::cout << "Phone Book\n";
+	std::cout << "Phone Book\n> ";
 	while (std::cin >> input) {
 		if (input.compare("ADD") == 0)
 			b.add();
 		else if (input.compare("SEARCH") == 0)
 			b.search();
-		else if (input.compare("SEARCH") == 0)
+		else if (input.compare("EXIT") == 0)
 			break;
 		std::cout << "> " << std::flush;
 	}
-	if (std::cin.eof()) {
-		std::cout << "End of input reached (Ctrl+D was pressed)." << std::endl;
-	} else {
-		std::cout << "Input error occurred." << std::endl;
-	}
+	//if (std::cin.eof()) {
+	//	std::cout << "End of input reached (Ctrl+D was pressed)." << std::endl;
+	//} else {
+	//	std::cout << "Input error occurred." << std::endl;
+	//}
 	return (0);
 }
