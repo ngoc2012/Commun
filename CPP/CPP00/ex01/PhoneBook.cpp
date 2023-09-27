2aeadefb861ca1431f2cef86c0077f645bd6c26c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-27 06:46:01 +0200

    all

--- CPP/CPP00/ex01/PhoneBook.cpp
+++ CPP/CPP00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/27 06:43:43 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/27 06:44:42 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ PhoneBook::PhoneBook(){}
 
 PhoneBook::~PhoneBook(){}
 
-bool	containsOnlySpaceAndTab(const std::string& str) {
+bool	emptyEntry(const std::string& str) {
 	for (char c : str) {
 		if (c != ' ' && c != '\t') {
 			return false;
@@ -29,7 +29,7 @@ std::string	get_field(void)
 {
 	std::string	input = "";
 
-	while (containsOnlySpaceAndTab(input)) {
+	while (emptyEntry(input)) {
 		std::cout << "> " << std::flush;
 		std::cin >> input;
 	}
