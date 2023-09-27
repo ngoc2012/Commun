d8aac6c81f24af3aeddd957b9fde4215bc1a9cc9
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-27 06:00:01 +0200

    all

--- CPP/CPP00/ex02/Account.cpp
+++ CPP/CPP00/ex02/Account.cpp
@@ -6,13 +6,27 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/27 05:56:01 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/27 05:59:25 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
-int Account::_nbAccounts = 0;
-int Account::_totalAmount= 0;
-int Account::_totalNbDeposits= 0;
-int Account::_totalNbWithdrawals = 0;
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount= 0;
+int	Account::_nbDeposits= 0;
+int	Account::_nbWithdrawals = 0;
+
+int	Account::getNbAccounts(void) {return _nbAccounts;}
+int	Account::getTotalAmount(void) {return _totalAmount; }
+int	Account::getNbDeposits(void) {return _nbDeposits; }
+int	Account::getNbWithdrawals(void) {return _nbWithdrawals; }
+
+void    Account::displayAccountsInfos( void ) {
+    timestamp();
+    std::cout << "accounts:" << getNbAccounts() << ";"
+                << "total:" << getTotalAmount() << ";"
+                << "deposits:" << getNbDeposits() << ";"
+                << "withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
