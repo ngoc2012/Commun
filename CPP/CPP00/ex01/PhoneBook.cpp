cdb9879e71e31cae0ce0bfa6b956c34321d11187
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-27 06:30:01 +0200

    all

--- CPP/CPP00/ex01/PhoneBook.cpp
+++ CPP/CPP00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/27 06:27:51 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/27 06:29:27 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ PhoneBook::~PhoneBook(){}
 
 void PhoneBook::add(void)
 {
-	static int	i;
-	Contact	*new = this->contacts[i % N_CONTACTS];
+	//static int	i;
+	//Contact	*new_contact = &this->contacts[i % N_CONTACTS];
 
 	std::cout << "add\n";
 	//std::cout << "> " << std::flush;
 	//std::cin >> input;
-	i++
+	//i++;
 }
 
 void PhoneBook::search(void) const
