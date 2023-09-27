f5820a47a7333d02a46bb6ac66942a1133516f30
Minh ngoc NGUYEN <minh-ngu@made-f0Ar2s12.clusters.42paris.fr>
2023-09-27 08:44:02 +0200

    all

--- CPP/CPP00/ex01/PhoneBook.cpp
+++ CPP/CPP00/ex01/PhoneBook.cpp
@@ -6,11 +6,12 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/27 08:41:04 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/09/27 08:43:01 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
+#include <cstdlib> // for atoi
 
 PhoneBook::PhoneBook(){}
 
@@ -59,7 +60,7 @@ void PhoneBook::search(void) const
 	int	num = std::stoi(input);
 	if (num < 1 || num > 8)
 	{
-		std::cerr << "Contact id does not exist\n";
+		std::cout << "Contact id does not exist\n";
 	}
 }
 
