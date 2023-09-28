0c9a05e900be49109334b7fc9984fb0868c23ad1
Minh ngoc NGUYEN <minh-ngu@made-f0Ar8s3.clusters.42paris.fr>
2023-09-28 15:04:02 +0200

    all

---
+++ CPP/CPP02/ex00/Fixed.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 21:20:40 by ngoc              #+#    #+#             */
+/*   Updated: 2023/09/28 15:03:21 by minh-ngu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+	private:
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+	public:
+		Fixed(void);
+		~Fixed(void);
+
+		void	complain( std::string level );
+};
+
+#endif
