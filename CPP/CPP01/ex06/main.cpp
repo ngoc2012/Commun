7f16fac75b77509d73698e2fa90e5d8f0f8177be
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-28 12:42:01 +0200

    all

--- CPP/CPP01/ex06/main.cpp
+++ CPP/CPP01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/28 12:39:28 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/28 12:41:18 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,7 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 		return (1);
-	std::string	input = "";
-	std::cout << "Harl program\n";
-	std::cout << "Command: DEBUG INFO WARNING ERROR EXIT\n> ";
-	while (std::getline(std::cin, input))
-	{
-		if (!input.compare("EXIT"))
-			return (0);
-		h.complain(input);
-		std::cout << "> " << std::flush;
-	}
+	h.complain(argv[1]);
 	std::cout << "[ Probably complaining about insignificant problems ]\n";
 	return (0);
 }
