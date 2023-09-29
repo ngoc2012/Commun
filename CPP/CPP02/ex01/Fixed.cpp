30b212d845222a10efc2073f468d7aab2948f6ae
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-29 14:40:01 +0200

    all

--- CPP/CPP02/ex01/Fixed.cpp
+++ CPP/CPP02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 08:44:11 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/29 14:37:54 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/29 14:39:14 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -181,7 +181,7 @@ int	Fixed::toInt( void ) const
     return fp >> fb;
 }
 
-std::ostream	&operator<<( std::ostream &o, const Fixed &n )
+std::ostream	&operator<<( std::ostream &o, Fixed &n )
 {
     o << n.toFloat();
     return o;
