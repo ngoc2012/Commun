52d67962189df53d55d151a6c775ef416a3c2442
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-29 10:04:01 +0200

    all

--- CPP/CPP00/ex01/Contact.cpp
+++ CPP/CPP00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/27 09:35:55 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/09/29 10:02:22 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,44 +30,44 @@ Contact::~Contact(){}
 void Contact::show_table(int index) const
 {
 	std::cout << "|" << std::setw(WIDTH) << index << std::flush;
-	std::cout << "|" << std::setw(WIDTH) << printLen(this->_firstName, WIDTH) << std::flush;
-	std::cout << "|" << std::setw(WIDTH) << printLen( this->_lastName, WIDTH) << std::flush;
-	std::cout << "|" << std::setw(WIDTH) << printLen( this->_nickName, WIDTH) << std::flush;
+	std::cout << "|" << std::setw(WIDTH) << printLen(_firstName, WIDTH) << std::flush;
+	std::cout << "|" << std::setw(WIDTH) << printLen( _lastName, WIDTH) << std::flush;
+	std::cout << "|" << std::setw(WIDTH) << printLen( _nickName, WIDTH) << std::flush;
 	std::cout << "|" << std::endl;
 }
 
 void Contact::show(void) const
 {
-	std::cout << "First name: " << this->_firstName << std::endl;
-	std::cout << "Last name: " << this->_lastName << std::endl;
-	std::cout << "Nick name: " << this->_nickName << std::endl;
-	std::cout << "Phone number: " << this->_phoneNumber << std::endl;
-	std::cout << "Darkest secret: " << this->_darkestSecret << std::endl;
+	std::cout << "First name: " << _firstName << std::endl;
+	std::cout << "Last name: " << _lastName << std::endl;
+	std::cout << "Nick name: " << _nickName << std::endl;
+	std::cout << "Phone number: " << _phoneNumber << std::endl;
+	std::cout << "Darkest secret: " << _darkestSecret << std::endl;
 }
 
 void	Contact::set_firstName(std::string str)
 {
-	this->_firstName = str;
+	_firstName = str;
 }
 
 void 	Contact::set_lastName(std::string str)
 {
-	this->_lastName = str;
+	_lastName = str;
 }
 
 void 	Contact::set_nickName(std::string str)
 {
-	this->_nickName = str;
+	_nickName = str;
 }
 
 void 	Contact::set_phoneNumber(std::string str)
 {
-	this->_phoneNumber = str;
+	_phoneNumber = str;
 }
 
 void 	Contact::set_darkestSecret(std::string str)
 {
-	this->_darkestSecret = str;
+	_darkestSecret = str;
 }
 
 #endif
