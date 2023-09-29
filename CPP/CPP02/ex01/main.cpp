898df12a09ef7865e57e7137b7ce8a17ac3cedf4
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-29 20:50:01 +0200

    all

--- CPP/CPP02/ex01/main.cpp
+++ CPP/CPP02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/29 20:27:04 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/29 20:48:30 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ int main(void) {
 	Fixed const c( 42.42f );
 	Fixed const d( b );
 
-	//a = Fixed( 1234.4321f );
-	a = Fixed( 42.42f );
+	a = Fixed( 1234.4321f );
+	//a = Fixed( 42.42f );
 	//a = Fixed( 12345678.87654321f );
-	//std::cout << "a is " << a << std::endl;
-	//std::cout << "b is " << b << std::endl;
-	//std::cout << "c is " << c << std::endl;
-	//std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
 
 	//std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	//std::cout << "b is " << b.toInt() << " as integer" << std::endl;
