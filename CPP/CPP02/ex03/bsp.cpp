8d1c6f29b66e34f6483f6342e3db1b15b0067e3f
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-30 17:58:01 +0200

    all

--- CPP/CPP02/ex03/bsp.cpp
+++ CPP/CPP02/ex03/bsp.cpp
@@ -6,22 +6,19 @@
 /*   By: ael-khni <ael-khni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 22:13:17 by ael-khni          #+#    #+#             */
-/*   Updated: 2023/09/30 17:51:41 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/30 17:56:52 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-//static Fixed   abs(Fixed x) {
-//    if (x < 0)
-//        return x * -1;
-//    return x;
-//}
-
-static Fixed   area( Point const a, Point const b, Point const c ) {
-    return ( ( ( a.getX() * ( b.getY() - c.getY() ) ) +
-             ( b.getX() * ( c.getY() - a.getY() ) ) +
-             ( c.getX() * ( a.getY() - b.getY() ) ) ) / 2 );
+static Fixed   area( Point const p1, Point const p2, Point const p3 ) {
+	Fixed	r;
+	r += p1.getX() * p2.getY() + p2.getX() * p3.getY() + p3.getX() * p1.getY();
+	r -= p2.getX() * p1.getY() + p3.getX() * p2.getY() + p1.getX() * p3.getY();
+	if (r < 0)
+		r = -r;
+	return (r / 2);
 }
 
 bool bsp( Point const a, Point const b, Point const c, Point const point) {
