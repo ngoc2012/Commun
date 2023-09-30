2461996fecd615083c1d084e4c0f4955123491c1
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-30 18:24:01 +0200

    all

--- CPP/CPP02/ex03/bsp.cpp
+++ CPP/CPP02/ex03/bsp.cpp
@@ -6,16 +6,18 @@
 /*   By: ael-khni <ael-khni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 22:13:17 by ael-khni          #+#    #+#             */
-/*   Updated: 2023/09/30 18:21:45 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/30 18:24:01 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
 static Fixed	area2( Point const p1, Point const p2, Point const p3 ) {
+	(void)p3;
 	Fixed	r;
-	r = r + p1.getX() * p2.getY() + p2.getX() * p3.getY() + p3.getX() * p1.getY();
-	r = r - p2.getX() * p1.getY() - p3.getX() * p2.getY() - p1.getX() * p3.getY();
+	r = p1.getX() * p2.getY();
+	//r = r + p1.getX() * p2.getY() + p2.getX() * p3.getY() + p3.getX() * p1.getY();
+	//r = r - p2.getX() * p1.getY() - p3.getX() * p2.getY() - p1.getX() * p3.getY();
 	if (r < 0)
 		r = Fixed() - r;
 	std::cout << r << std::endl;
