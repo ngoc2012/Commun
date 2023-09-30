c3808fdfaef991e29ec7412fddf91616d27d880b
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-30 15:52:01 +0200

    all

--- CPP/CPP02/ex02/Fixed.hpp
+++ CPP/CPP02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 21:20:40 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/30 15:31:55 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/30 15:50:28 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,42 @@ class Fixed
 
 		float   toFloat( void ) const;
 		int     toInt( void ) const;
+
+		bool    operator>( const Fixed &rhs ) const ;
+		bool    operator<( const Fixed &rhs ) const ;
+		bool    operator>=( const Fixed &rhs ) const ;
+		bool    operator<=( const Fixed &rhs ) const ;
+		bool    operator==( const Fixed &rhs ) const ;
+		bool    operator!=( const Fixed &rhs ) const ;
+
+		Fixed   operator+( const Fixed &rhs ) const ;
+		Fixed   operator-( const Fixed &rhs ) const ;
+		Fixed   operator*( const Fixed &rhs ) const ;
+		Fixed   operator/( const Fixed &rhs ) const ;
+
+		Fixed&  operator++( void ); // prefix
+		Fixed   operator++( int ); // postfix
+		Fixed&  operator--( void ); // prefix
+		Fixed   operator--( int ); // postfix
+
+		static Fixed& min( Fixed &a, Fixed &b );
+		static const Fixed& min( const Fixed &a, const Fixed &b );
+		static Fixed& max( Fixed &a, Fixed &b );
+		static const Fixed& max( const Fixed &a, const Fixed &b );
+		/*
+		   Static Function Members
+		   By declaring a function member as static, you make it independent of any
+		   particular object of the class. A static member function can be called
+		   even if no objects of the class exist and the static functions are accessed
+		   using only the class name and the scope resolution operator ::.
+
+		   A static member function can only access static data member, other static
+		   member functions and any other functions from outside the class.
+
+		   Static member functions have a class scope and they do not have access
+		   to the this pointer of the class. You could use a static member function
+		   to determine whether some objects of the class have been created or not.
+		 */
 
 		bool	getOverFlow( void ) const;
 		void	setOverFlow( bool );
