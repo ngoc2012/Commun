5b87f122387faa7ae8d19a347eb305c28911210c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-30 17:52:01 +0200

    all

--- CPP/CPP02/ex03/bsp.cpp
+++ CPP/CPP02/ex03/bsp.cpp
@@ -6,17 +6,17 @@
 /*   By: ael-khni <ael-khni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 22:13:17 by ael-khni          #+#    #+#             */
-/*   Updated: 2022/07/23 12:18:58y ael-khni         ###   ########.fr       */
+/*   Updated: 2023/09/30 17:51:41 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-static Fixed   abs(Fixed x) {
-    if (x < 0)
-        return x * -1;
-    return x;
-}
+//static Fixed   abs(Fixed x) {
+//    if (x < 0)
+//        return x * -1;
+//    return x;
+//}
 
 static Fixed   area( Point const a, Point const b, Point const c ) {
     return ( ( ( a.getX() * ( b.getY() - c.getY() ) ) +
@@ -25,10 +25,10 @@ static Fixed   area( Point const a, Point const b, Point const c ) {
 }
 
 bool bsp( Point const a, Point const b, Point const c, Point const point) {
-    Fixed   abcArea = abs(area( a, b, c ));
-    Fixed   pabArea = abs(area( point, a, b ));
-    Fixed   pbcArea = abs(area( point, b, c ));
-    Fixed   pcaArea = abs(area( point, c, a ));
+	Fixed	abcArea = abs(area( a, b, c ));
+	Fixed	pabArea = abs(area( point, a, b ));
+	Fixed	pbcArea = abs(area( point, b, c ));
+	Fixed	pcaArea = abs(area( point, c, a ));
 
-    return  ( abcArea == pabArea + pbcArea + pcaArea );
+	return  ( area( a, b, c ) == pabArea + pbcArea + pcaArea );
 }
