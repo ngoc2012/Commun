0e1b87ccab58fd6ca5ece6d35e5dab2f26e855f9
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-30 19:00:01 +0200

    all

--- CPP/CPP02/ex03/main.cpp
+++ CPP/CPP02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 18:48:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/30 18:48:51 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/30 18:59:40 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,22 +34,19 @@ Explanation:
              /       \      P
             /         \
      A(0,0) ----------- C(20,0) 
-
-Solution: 
-Let the coordinates of three corners be (x1, y1), (x2, y2) and (x3, y3). And coordinates of the given point P be (x, y)
-
-Calculate area of the given triangle, i.e., area of the triangle ABC in the above diagram. 
-Area A = [ x1(y2 – y3) + x2(y3 – y1) + x3(y1-y2) ] / 2 
-Calculate area of the triangle PAB. We can use the same formula for this. Let this area be A1. 
-Calculate area of the triangle PBC. Let this area be A2. 
-Calculate area of the triangle PAC. Let this area be A3. 
-If P lies inside the triangle, then A1 + A2 + A3 must be equal to A. 
-
 */
 
 int	main ( void ) {
-	//Point p = Point(0, 0);
-	//std::cout << p.getX() << " " << p.getY() << std::endl;
+	if ( bsp( Point(0, 0), Point(1, 0), Point(0, 0), Point(0, 1) ) == true ) {
+		std::cout << "Point is in the triangle" << std::endl;
+	} else {
+		std::cout << "Point is not in the triangle" << std::endl;
+	}
+	if ( bsp( Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 1) ) == true ) {
+		std::cout << "Point is in the triangle" << std::endl;
+	} else {
+		std::cout << "Point is not in the triangle" << std::endl;
+	}
 	if ( bsp( Point(0, 0), Point(10, 30), Point(20, 0), Point(10, 15) ) == true ) {
 		std::cout << "Point is in the triangle" << std::endl;
 	} else {
