8fc1649099e88e6ef704ae0c8d034b25f4b4b852
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-30 18:36:01 +0200

    all

--- CPP/CPP02/ex03/bsp.cpp
+++ CPP/CPP02/ex03/bsp.cpp
@@ -6,12 +6,12 @@
 /*   By: ael-khni <ael-khni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 22:13:17 by ael-khni          #+#    #+#             */
-/*   Updated: 2023/09/30 18:26:18 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/30 18:35:23 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
-
+/*
 static Fixed	area2( Point const p1, Point const p2, Point const p3 ) {
 	(void)p3;
 	Fixed	r;
@@ -24,7 +24,12 @@ static Fixed	area2( Point const p1, Point const p2, Point const p3 ) {
 	std::cout << r << std::endl;
 	return (r);
 }
-
+*/
 bool	bsp( Point const a, Point const b, Point const c, Point const p) {
-	return  ( area2( a, b, c ) == area2( p, a, b ) + area2( p, b, c ) + area2( p, c, a ) );
+	std::cout << a.getX() << " " << a.getY() << std::endl;
+	std::cout << b.getX() << " " << b.getY() << std::endl;
+	std::cout << c.getX() << " " << c.getY() << std::endl;
+	std::cout << p.getX() << " " << p.getY() << std::endl;
+	return (false);
+	//return  ( area2( a, b, c ) == area2( p, a, b ) + area2( p, b, c ) + area2( p, c, a ) );
 }
