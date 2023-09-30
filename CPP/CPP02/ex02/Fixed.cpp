384ac2fc97366cf5bc019196f281c400815aae57
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-30 17:00:01 +0200

    all

--- CPP/CPP02/ex02/Fixed.cpp
+++ CPP/CPP02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 08:44:11 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/30 16:57:57 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/30 16:59:06 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -168,13 +168,32 @@ bool	Fixed::operator==( const Fixed &val ) const { return (getRawBits() == val.g
 bool	Fixed::operator!=( const Fixed &val ) const { return (getRawBits() != val.getRawBits()); }
 Fixed	Fixed::operator+ ( const Fixed &val ) const
 {
-	if (getOverFlow() || val.getOverFlow)
+	if (getOverFlow() || val.getOverFlow())
 		return (0);
 	return (Fixed( toFloat() + val.toFloat() ));
 }
-Fixed	Fixed::operator- ( const Fixed &val ) const { return (Fixed( toFloat() - val.toFloat() )); }
-Fixed	Fixed::operator* ( const Fixed &val ) const { return (Fixed( toFloat() * val.toFloat() )); }
-Fixed	Fixed::operator/ ( const Fixed &val ) const { return (Fixed( toFloat() / val.toFloat() )); }
+
+Fixed	Fixed::operator- ( const Fixed &val ) const
+{
+	if (getOverFlow() || val.getOverFlow())
+		return (0);
+	return (Fixed( toFloat() - val.toFloat() ));
+}
+
+Fixed	Fixed::operator* ( const Fixed &val ) const
+{
+	if (getOverFlow() || val.getOverFlow())
+		return (0);
+	return (Fixed( toFloat() * val.toFloat() ));
+}
+
+Fixed	Fixed::operator/ ( const Fixed &val ) const
+{
+	if (getOverFlow() || val.getOverFlow())
+		return (0);
+	return (Fixed( toFloat() / val.toFloat() ));
+}
+
 // Prefix
 Fixed&	Fixed::operator++( void ) { fp++; return *this; }
 Fixed&	Fixed::operator--( void ) { fp--; return *this; }
