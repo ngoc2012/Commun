8f507c38ce020fa155d3dbceaee9dfbf9ba29747
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-30 18:16:01 +0200

    all

--- CPP/CPP02/ex03/Point.cpp
+++ CPP/CPP02/ex03/Point.cpp
@@ -6,14 +6,14 @@
 /*   By: ael-khni <ael-khni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 22:05:11 by ael-khni          #+#    #+#             */
-/*   Updated: 2023/09/30 17:42:19 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/30 18:14:12 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
 Point::Point( void ) : x(Fixed()), y(Fixed()) { }
-Point::Point( const Fixed x, const Fixed y ) : x(x0), y(y0) {}
+Point::Point( const Fixed x0, const Fixed y0 ) : x(x0), y(y0) {}
 Point::Point( const Point &src ) : x(src.x), y(src.y) {}
 Point::~Point() {}
 
