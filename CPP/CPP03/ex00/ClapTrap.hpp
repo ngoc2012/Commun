7481f440ab0328ab7b5ff99b75d34d0744b2be5e
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-30 19:58:01 +0200

    all

--- CPP/CPP03/ex00/ClapTrap.hpp
+++ CPP/CPP03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/30 19:54:12 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/30 19:57:36 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,19 @@ class ClapTrap
 	private:
 		std::string	name;
 		unsigned int	hit_points;
-		unsigned int	max_hit_points;
 		unsigned int	energy_points;
-		unsigned int	max_energy_points;
-		unsigned int	level;
-		unsigned int	melee_attack_damage;
-		unsigned int	ranged_attack_damage;
-		unsigned int	armor_damage_reduction;
-		void		mathAttack(std::string const & target);
-		void		questionAttack(std::string const & target);
-		void		danseAttack(std::string const & target);
-		void		lostAttack(std::string const & target);
-		void		monologueAttack(std::string const & target);
+		unsigned int	attack_damage;
+		//unsigned int	max_hit_points;
+		//unsigned int	max_energy_points;
+		//unsigned int	level;
+		//unsigned int	melee_attack_damage;
+		//unsigned int	ranged_attack_damage;
+		//unsigned int	armor_damage_reduction;
+		//void		mathAttack(std::string const & target);
+		//void		questionAttack(std::string const & target);
+		//void		danseAttack(std::string const & target);
+		//void		lostAttack(std::string const & target);
+		//void		monologueAttack(std::string const & target);
 
 	public:
 		ClapTrap();
@@ -44,11 +45,12 @@ class ClapTrap
 		ClapTrap(const ClapTrap&);
 		virtual ~ClapTrap();
 		ClapTrap &operator=(const ClapTrap& op);
-		void		rangedAttack(std::string const & target);
-		void		meleeAttack(std::string const & target);
+		//void		rangedAttack(std::string const & target);
+		//void		meleeAttack(std::string const & target);
+		//void		vaulthunter_dot_exe(const std::string& target);
+		void		attack(const std::string& target);
 		void		takeDamage(unsigned int amount);
 		void		beRepaired(unsigned int amount);
-		void		vaulthunter_dot_exe(std::string const & target);
 };
 
 #endif
