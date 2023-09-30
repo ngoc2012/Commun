4f94408371c1069bd3e3d35c9ee4122199790391
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-09-30 18:28:01 +0200

    all

--- CPP/CPP02/ex03/bsp.cpp
+++ CPP/CPP02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-khni <ael-khni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 22:13:17 by ael-khni          #+#    #+#             */
-/*   Updated: 2023/09/30 18:24:01 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/09/30 18:26:18 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 static Fixed	area2( Point const p1, Point const p2, Point const p3 ) {
 	(void)p3;
 	Fixed	r;
+	std::cout << p1.getX() << " " << p2.getY() << std::endl;
 	r = p1.getX() * p2.getY();
 	//r = r + p1.getX() * p2.getY() + p2.getX() * p3.getY() + p3.getX() * p1.getY();
 	//r = r - p2.getX() * p1.getY() - p3.getX() * p2.getY() - p1.getX() * p3.getY();
