11eb18d3cf29c0701599173adabd797f37f0c7c0
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-01 20:30:01 +0200

    all

--- CPP/CPP03/ex00/ClapTrap.cpp
+++ CPP/CPP03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/01 20:26:17 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/01 20:29:20 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,7 @@ void	ClapTrap::takeDamage(unsigned int amount)
 {
 	std::cout << "ClapTrap " << name << " takes " << amount <<" damages." << std::endl;
 	hit_points -= amount;
+	std::cout << "ClapTrap " << name << " now has " << hit_points <<" hits _points." << std::endl;
 }
 
 void	ClapTrap::beRepaired(unsigned int amount)
@@ -94,12 +95,12 @@ void		ClapTrap::setAttackDamage(unsigned int n)
 	attack_damage = n;
 }
 
-void		ClapTrap::realAttack(ClapTrap& target)
-{
-	attack(target.getName());
-	target.setHitPoints(target.getHitPoints() - attack_damage);
-	std::cout << "ClapTrap " << name << " now has "
-	<< energy_points << " energy points, and "
-	<< target.getName() << " now has "
-	<< target.getHitPoints() << " hit points." << std::endl;
-}
+//void		ClapTrap::realAttack(ClapTrap& target)
+//{
+//	attack(target.getName());
+//	target.setHitPoints(target.getHitPoints() - attack_damage);
+//	std::cout << "ClapTrap " << name << " now has "
+//	<< energy_points << " energy points, and "
+//	<< target.getName() << " now has "
+//	<< target.getHitPoints() << " hit points." << std::endl;
+//}
