db3f8b6c7ac3ebc218e76ab928a6a4b9b82f39da
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-01 17:18:01 +0200

    all

--- CPP/CPP03/ex00/main.cpp
+++ CPP/CPP03/ex00/main.cpp
@@ -6,34 +6,34 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/09/30 19:17:52 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/01 17:17:41 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "FragTrap.hpp"
+#include "ClapTrap.hpp"
 
 int main()
 {
 	std::cout << std::endl;
-    FragTrap clap("Pierrot");
-    FragTrap trap("Michel");
-    
+	ClapTrap clap("Pierrot");
+	ClapTrap trap("Michel");
+
 	std::cout << std::endl;
-    clap.rangedAttack("Michel");
-    trap.takeDamage(20);
-    clap.meleeAttack("Michel");
-    trap.takeDamage(30);
-    trap.beRepaired(35);
-    trap.beRepaired(80);
-    trap.takeDamage(120);
-    trap.beRepaired(33);
-    
-    clap.vaulthunter_dot_exe("Michel");
-    clap.vaulthunter_dot_exe("Michel");
-    clap.vaulthunter_dot_exe("Michel");
-    clap.vaulthunter_dot_exe("Michel");
-    clap.vaulthunter_dot_exe("Michel");
-    
+	clap.rangedAttack("Michel");
+	trap.takeDamage(20);
+	clap.meleeAttack("Michel");
+	trap.takeDamage(30);
+	trap.beRepaired(35);
+	trap.beRepaired(80);
+	trap.takeDamage(120);
+	trap.beRepaired(33);
+
+	clap.vaulthunter_dot_exe("Michel");
+	clap.vaulthunter_dot_exe("Michel");
+	clap.vaulthunter_dot_exe("Michel");
+	clap.vaulthunter_dot_exe("Michel");
+	clap.vaulthunter_dot_exe("Michel");
+
 	std::cout << std::endl;
-    return (0);
+	return (0);
 }
