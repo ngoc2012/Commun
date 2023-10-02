62b5a6b762b2d66453e41aa999d566370428611f
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-02 14:04:01 +0200

    all

--- CPP/CPP03/ex01/ScavTrap.hpp
+++ CPP/CPP03/ex01/ScavTrap.hpp
@@ -6,23 +6,19 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/02 14:01:28 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/02 14:03:55 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
 # define SCAVTRAP_HPP
 
-#include <iostream>
+#include "ClapTrap.hpp"
 
-class ScavTrap
+class ScavTrap : ClapTrap
 {
 	private:
-		std::string	name;
-		unsigned int	hit_points;
-		unsigned int	energy_points;
-		unsigned int	attack_damage;
-		static const unsigned int	max_hit = 10;
+		static const unsigned int	max_hit = 100;
 	public:
 		ScavTrap();
 		ScavTrap(std::string name);
@@ -31,20 +27,6 @@ class ScavTrap
 		~ScavTrap();
 
 		void		attack(const std::string& target);
-		void		takeDamage(unsigned int amount);
-		void		beRepaired(unsigned int amount);
-
-		std::string	getName(void) const;
-		unsigned int	getHitPoints(void) const;
-		unsigned int	getEnergyPoints(void) const;
-		unsigned int	getAttackDamage(void) const;
-
-		void		setName(std::string);
-		void		setHitPoints(unsigned int);
-		void		setEnergyPoints(unsigned int);
-		void		setAttackDamage(unsigned int);
-
-		void		status(void);
 };
 
 #endif
