404d61ddead5541a0cb1aaee27bd51586037b2df
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-02 14:06:01 +0200

    all

--- CPP/CPP03/ex01/ScavTrap.cpp
+++ CPP/CPP03/ex01/ScavTrap.cpp
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/01 21:08:21 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/02 14:05:25 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
-void	welcome(ClapTrap &c) {
-	std::cout << "ClapTrap " << c.getName()
+void	welcome(ScavTrap &c) {
+	std::cout << "ScavTrap " << c.getName()
 	<< " (hit points: " << c.getHitPoints()
 	<< ", energy points: " << c.getEnergyPoints()
 	<< ", attack_damage: " << c.getAttackDamage()
 	<< ") join the game." << std::endl;
 }
 
-ClapTrap::ClapTrap()
+ScavTrap::ScavTrap()
 {
 	name = std::string("RandomClap");
 	hit_points = 10;
@@ -29,7 +29,7 @@ ClapTrap::ClapTrap()
 	welcome(*this); 
 }
 
-ClapTrap::ClapTrap(std::string name)
+ScavTrap::ScavTrap(std::string name)
 :
 	name(name),
 	hit_points(10),
@@ -37,7 +37,7 @@ ClapTrap::ClapTrap(std::string name)
 	attack_damage(0)
 { std::cout << "Another "; welcome(*this); }
 
-ClapTrap::ClapTrap(const ClapTrap& src)
+ScavTrap::ScavTrap(const ScavTrap& src)
 :
 	name(src.name),
 	hit_points(src.hit_points),
@@ -45,62 +45,62 @@ ClapTrap::ClapTrap(const ClapTrap& src)
 	attack_damage(src.attack_damage)
 { welcome(*this); }
 
-ClapTrap::~ClapTrap() { std::cout << "ClapTrap " << name << " quit game." << std::endl; }
+ScavTrap::~ScavTrap() { std::cout << "ScavTrap " << name << " quit game." << std::endl; }
 
-void	ClapTrap::attack(std::string const & target)
+void	ScavTrap::attack(std::string const & target)
 {
 	if (energy_points && hit_points)
 	{
-		std::cout << "ClapTrap " << name << " attack " << target << ", causing " << attack_damage << " points of damage!" << std::endl;
+		std::cout << "ScavTrap " << name << " attack " << target << ", causing " << attack_damage << " points of damage!" << std::endl;
 		energy_points--;
 	}
 	else
-		std::cout << "Alert!!! ClapTrap " << name << " tried to attack but has no more energy or hit." << std::endl;
+		std::cout << "Alert!!! ScavTrap " << name << " tried to attack but has no more energy or hit." << std::endl;
 }
 
-void	ClapTrap::takeDamage(unsigned int amount)
+void	ScavTrap::takeDamage(unsigned int amount)
 {
-	std::cout << "ClapTrap " << name << " takes " << amount <<" damages." << std::endl;
+	std::cout << "ScavTrap " << name << " takes " << amount <<" damages." << std::endl;
 	if (amount > hit_points)
 		amount = hit_points;
 	hit_points -= amount;
 }
 
-void	ClapTrap::beRepaired(unsigned int amount)
+void	ScavTrap::beRepaired(unsigned int amount)
 {
 	if (energy_points && hit_points)
 	{
 		hit_points += amount;
 		if (hit_points > max_hit)
 			hit_points = max_hit;
-		std::cout << "ClapTrap " << name << " is repaired " << amount << " points (" << hit_points << "  hit points)." << std::endl;
+		std::cout << "ScavTrap " << name << " is repaired " << amount << " points (" << hit_points << "  hit points)." << std::endl;
 		energy_points--;
 	}
 	else
-		std::cout << "Alert!!! ClapTrap " << name << " tried to be repaired but has no more energy or hit." << std::endl;
+		std::cout << "Alert!!! ScavTrap " << name << " tried to be repaired but has no more energy or hit." << std::endl;
 }
 
-std::string	ClapTrap::getName(void) const { return (name); }
-unsigned int	ClapTrap::getHitPoints(void) const { return (hit_points); }
-unsigned int	ClapTrap::getEnergyPoints(void) const { return (energy_points); }
-unsigned int	ClapTrap::getAttackDamage(void) const { return (attack_damage); }
+std::string	ScavTrap::getName(void) const { return (name); }
+unsigned int	ScavTrap::getHitPoints(void) const { return (hit_points); }
+unsigned int	ScavTrap::getEnergyPoints(void) const { return (energy_points); }
+unsigned int	ScavTrap::getAttackDamage(void) const { return (attack_damage); }
 
-void		ClapTrap::setName(std::string s)
+void		ScavTrap::setName(std::string s)
 {
-	std::cout << "ClapTrap " << name << " now named " << s << "." << std::endl;
+	std::cout << "ScavTrap " << name << " now named " << s << "." << std::endl;
 	name = s;
 }
-void		ClapTrap::setHitPoints(unsigned int n) { hit_points = n; }
-void		ClapTrap::setEnergyPoints(unsigned int n) { energy_points = n; }
-void		ClapTrap::setAttackDamage(unsigned int n)
+void		ScavTrap::setHitPoints(unsigned int n) { hit_points = n; }
+void		ScavTrap::setEnergyPoints(unsigned int n) { energy_points = n; }
+void		ScavTrap::setAttackDamage(unsigned int n)
 {
-	std::cout << "ClapTrap " << name << " now has " << n << " attack damage." << std::endl;
+	std::cout << "ScavTrap " << name << " now has " << n << " attack damage." << std::endl;
 	attack_damage = n;
 }
 
-void		ClapTrap::status(void)
+void		ScavTrap::status(void)
 {
-	std::cout << "ClapTrap " << name << " now has "
+	std::cout << "ScavTrap " << name << " now has "
 	<< hit_points << " hit points, "
 	<< energy_points << " energy point." << std::endl;
 }
--- CPP/CPP03/ex01/ScavTrap.hpp
+++ CPP/CPP03/ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/02 14:03:55 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/02 14:04:03 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,10 @@
 class ScavTrap : ClapTrap
 {
 	private:
+		//std::string	name;
+		//unsigned int	hit_points;
+		//unsigned int	energy_points;
+		//unsigned int	attack_damage;
 		static const unsigned int	max_hit = 100;
 	public:
 		ScavTrap();
