ed641c0d0afdbef9ae227b99b4bef4c7ce310739
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-02 16:10:01 +0200

    all

--- Cub3D/srcs/mini_map.c
+++ Cub3D/srcs/mini_map.c
@@ -6,7 +6,7 @@
 /*   By: nbechon <nbechon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 14:35:06 by nbechon           #+#    #+#             */
-/*   Updated: 2023/09/26 10:48:13 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/10/02 15:57:20 by nbechon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ static int	create_trgb(unsigned char t, unsigned char r,
 	return (t << 24 | r << 16 | g << 8 | b);
 }
 
-void	carrer_perso(int yp, int xp, int *addr)
+void	draw_hero(int yp, int xp, int *addr)
 {
 	int	i;
 	int	j;
 
-	yp -= 2;
-	xp -= 2;
+	yp -= 3;
+	xp -= 5;
 	i = 0;
-	while (i < 10)
+	while (i < 5)
 	{
 		j = 0;
-		while (j < 10)
+		while (j < 5)
 		{
 			addr[yp * WIDTH + (xp + j)] = create_trgb(1, 255, 0, 0);
 			j++;
@@ -39,7 +39,7 @@ void	carrer_perso(int yp, int xp, int *addr)
 	}
 }
 
-void	wall(int *addr, int p, int k)
+void	wall(int *addr, int p, int k, int count)
 {
 	int	v;
 	int	i;
@@ -54,82 +54,71 @@ void	wall(int *addr, int p, int k)
 		v = 1;
 		while (v < l)
 		{
-			addr[(i + (k * h)) * WIDTH + (v + (p * l))] = create_trgb(1, 255, 255, 255);
+			if (count == 1)
+				addr[(i + (p * h)) * WIDTH + (v + (k * l))] = create_trgb(1,
+						0, 255, 255);
+			if (count == 0)
+				addr[(i + (p * h)) * WIDTH + (v + (k * l))] = create_trgb(1,
+						255, 255, 255);
 			v++;
 		}
 		i++;
 	}
 }
 
-void	draw_hero(t_game *g, int l, int h)
+void	draw_hero_and_wall(t_game *g, int l, int h, int *addr)
 {
-	int		*addr;
 	int		i;
 	int		j;
 	int		k;
 	int		p;
 
-	addr = (int *)g->mlx.addr;
-	carrer_perso(h / 2, l / 2, addr);
-	j = g->pos.y - 4;
+	i = g->pos.y - 4;
 	p = 0;
-	while (j <= g->pos.y + 4)
+	while (i <= g->pos.y + 4)
 	{
 		k = 0;
-		i = g->pos.x - 4;
-		while (i <= g->pos.x + 4)
+		j = g->pos.x - 4;
+		while (j <= g->pos.x + 4)
 		{
 			if (i >= 0 && i < g->map.h && j >= 0 && j < g->map.l
 				&& g->map.v[i][j] == B_WALL)
-				wall(addr, p, k);
+				wall(addr, p, k, 0);
+			if (i >= 0 && i < g->map.h && j >= 0 && j < g->map.l
+				&& g->map.v[i][j] == B_DOOR)
+				wall(addr, p, k, 1);
 			k++;
-			i++;
+			j++;
 		}
-		j++;
+		i++;
 		p++;
 	}
+	draw_hero(h / 2, l / 2, addr);
 }
 
-void	draw_box(int h, int l, int *addr, int count)
+draw_life(int *addr, int l)
 {
 	int	i;
 	int	j;
 
-	i = 0;
-	j = 0;
-	while (j <= h)
+	i = 1;
+	while (i < 25)
 	{
-		i = 0;
-		while (i < l)
+		j = 6;
+		while (j < l)
 		{
-			if (count == 1)
-				addr[j * WIDTH + i] = create_trgb(1, 255, 0, 255);
-			else
-				addr[i * WIDTH + j] = create_trgb(1, 255, 0, 255);
-			i++;
+			addr[(HEIGHT - 30 + i) * WIDTH + (WIDTH - j)] = create_trgb(1, 255, 0, 0);
+			j++;
 		}
-		j += (h / 9);
+		i++;
 	}
-}
-
-void	draw_square(t_game *g, int x, int y, int l, int h)
-{
-	int	xp;
-	int	yp;
-	int	*addr;
-
-	addr = (int *)g->mlx.addr;
-	draw_box(h, l, addr, 1);
-	draw_box(l, h, addr, 0);
-	draw_hero(g, l, h);
 }
 
 void	draw_mini_map(t_game *g)
 {
-	int	xmap;
-	int	ymap;
+	int	*addr;
 
-	xmap = WIDTH / 4;
-	ymap = HEIGHT / 4;
-	draw_square(g, 0, 0, xmap, ymap);
+	addr = (int *)g->mlx.addr;
+	draw_hero_and_wall(g, WIDTH / 4, HEIGHT / 4, addr);
+	draw_life(addr, WIDTH / 3);
 }
