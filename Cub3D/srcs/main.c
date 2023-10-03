af75197bdd21ecfeec76af7d4bc7218b85de49af
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-03 18:58:01 +0200

    all

--- Cub3D/srcs/main.c
+++ Cub3D/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/03 18:14:54 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/03 18:56:44 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -273,7 +273,7 @@ int	main(int argc, char **argv)
 	mlx_hook(g.mlx.win, 2, KeyPressMask, &key_press, &g);
 	mlx_hook(g.mlx.win, 3, KeyReleaseMask, &key_release, &g);
 	mlx_hook(g.mlx.win, ClientMessage, LeaveWindowMask, &end_game, &g);
-	mlx_loop_hook(g.mlx.mlx, &draw, &g);
-	//draw(&g);
+	//mlx_loop_hook(g.mlx.mlx, &draw, &g);
+	draw(&g);
 	mlx_loop(g.mlx.mlx);
 }
