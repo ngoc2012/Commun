27d7958ddb6c3310911969d7910458569dfb5ace
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-03 16:12:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/03 15:49:00 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/03 16:11:21 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -446,6 +446,8 @@ void	render_backgroud(t_game *g)
 							tx = (g->eq.x - start_x + 0.5) / (end_x - start_x) * g->sp_tex[0].l - 1;
 						else
 							tx = (g->eq.y - start_y + 0.5) / (end_y - start_y) * g->sp_tex[0].l - 1;
+						h = BOX_SIZE / dsp * g->dpp;
+						p = 1.0 / dsp * g->dpp;
 						h_slide = (int) (BOX_SIZE / dsp * g->dpp);
 						if (h_slide > HEIGHT)
 							h_slide = HEIGHT;
