c3dfe803ad7feab3fbd79b9a4cede3525f0c5002
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-03 14:04:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/03 14:00:41 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/03 14:02:49 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -423,8 +423,8 @@ void	render_backgroud(t_game *g)
 				g->eq.c2 = g->eq.a2 * g->pos.px + g->eq.b2 * g->pos.py;
 				g->eq.det = 1.0;
 				g->eq.getXY(&g->eq);
-				double	start_x = g->sprites[i].px - g->sprite.l / 2 * g->sin_a1[g->pos.rot];
-				double	start_y = g->sprites[i].py + g->sprite.l / 2 * g->cos_a1[g->pos.rot];
+				double	start_x = g->sprites[i].px - g->sp_tex[0].l / 2 * g->sin_a1[g->pos.rot];
+				double	start_y = g->sprites[i].py + g->sp_tex[0].l / 2 * g->cos_a1[g->pos.rot];
 				/*
 				if ((45.0 < ai && ai < 135.0) || (-135.0 < ai && ai < -45))
 					dsp = (g->eq.y - g->pos.py) / g->sin_ai[ix][g->pos.rot];
