9c5f918f8ec3cadf1f6e0460f65c55f47c1d79f7
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-03 21:50:01 +0200

    all

--- Cub3D/srcs/draw.c
+++ Cub3D/srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/03 21:47:19 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/03 21:48:47 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -412,12 +412,9 @@ void	render_backgroud(t_game *g)
 		addr_t = (int *)tex->addr;
 		double	dsp;
 		int	i = -1;
-		double	dd = d * d;
-		double vx = g->cos_a1[g->pos.rot];
-		double vy = -g->sin_a1[g->pos.rot];
 		while (++i < g->n_sprites)
 		{
-			if (vx * (g->sprites[i].px - g->pos.px) + vy * (g->sprites[i].py - g->pos.py) >= 0)
+			if (g->cos_a1[g->pos.rot] * (g->sprites[i].px - g->pos.px) - g->sin_a1[g->pos.rot] * (g->sprites[i].py - g->pos.py) >= 0)
 			{
 				g->eq.a1 =  g->cos_a1[g->pos.rot];
 				g->eq.b1 = -g->sin_a1[g->pos.rot];
