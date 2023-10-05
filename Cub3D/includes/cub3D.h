d8793cf9281b99b9d530400bd17ae8a4b9e11928
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-05 21:26:01 +0200

    all

--- Cub3D/includes/cub3D.h
+++ Cub3D/includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 09:21:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/05 17:12:17 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/05 21:25:46 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@
 # define DOOR_SPEED 2
 # define DOOR_IDLE 40
 # define SPRITE_IDLE 5
-# define SPRITE_STATE 3
+# define SPRITE_STATE 6
 # define HEALTH_SPRITE 2
 # define PI 3.141592654
 # define INFINI INT_MAX
