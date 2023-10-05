2df4919b8fb3c56f05c7c794910e77b85d6304f1
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-05 10:02:01 +0200

    all

--- CPP/CPP03/ex01/main.cpp
+++ CPP/CPP03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/04 16:41:48 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/05 10:01:47 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,39 @@
 
 int main()
 {
-	std::cout << std::endl;
-	ClapTrap clap1("Clap1");
-	ClapTrap clap2;
-	ClapTrap clap3(clap1);
+	//std::cout << std::endl;
+	//ClapTrap clap1("Clap1");
+	//ClapTrap clap2;
+	//ClapTrap clap3(clap1);
 	ScavTrap clap4;
 
-	clap2.setName("Clap2");
-	clap3.setName("Clap3");
-	clap4.setName("Clap4");
-	clap4.guardGate();
-	clap1.setAttackDamage(1);
-	clap2.setAttackDamage(2);
-	clap3.setAttackDamage(3);
-	clap1.attack(clap2.getName());
-	clap2.takeDamage(clap1.getAttackDamage());
-	clap1.status();
-	clap2.status();
-	clap3.attack(clap1.getName());
-	clap1.takeDamage(clap3.getAttackDamage());
-	clap1.status();
-	clap3.status();
-	for (int i = 0; i < 10; i++)
-	{
-		clap1.attack(clap2.getName());
-		clap2.takeDamage(clap1.getAttackDamage());
-		clap1.status();
-		clap2.status();
-	}
-	clap2.beRepaired(5);
-	clap1.beRepaired(2);
-	clap3.beRepaired(1);
-	clap1.status();
-	clap2.status();
-	clap3.status();
+	//clap2.setName("Clap2");
+	//clap3.setName("Clap3");
+	//clap4.setName("Clap4");
+	//clap4.guardGate();
+	//clap1.setAttackDamage(1);
+	//clap2.setAttackDamage(2);
+	//clap3.setAttackDamage(3);
+	//clap1.attack(clap2.getName());
+	//clap2.takeDamage(clap1.getAttackDamage());
+	//clap1.status();
+	//clap2.status();
+	//clap3.attack(clap1.getName());
+	//clap1.takeDamage(clap3.getAttackDamage());
+	//clap1.status();
+	//clap3.status();
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	clap1.attack(clap2.getName());
+	//	clap2.takeDamage(clap1.getAttackDamage());
+	//	clap1.status();
+	//	clap2.status();
+	//}
+	//clap2.beRepaired(5);
+	//clap1.beRepaired(2);
+	//clap3.beRepaired(1);
+	//clap1.status();
+	//clap2.status();
+	//clap3.status();
 	return (0);
 }
