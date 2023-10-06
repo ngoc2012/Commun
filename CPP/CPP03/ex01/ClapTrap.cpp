a4e2830e5717acb92730595ce2a861b8b2029a3a
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-06 13:20:01 +0200

    all

--- CPP/CPP03/ex01/ClapTrap.cpp
+++ CPP/CPP03/ex01/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/06 13:08:36 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/06 13:18:42 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,17 +116,8 @@ void		ClapTrap::setName(std::string s)
 	std::cout << "ClapTrap " << name << " now named " << s << "." << std::endl;
 	name = s;
 }
-void		ClapTrap::setHitPoints(unsigned int n) { hit_points = n; }
-void		ClapTrap::setEnergyPoints(unsigned int n) { energy_points = n; }
 void		ClapTrap::setAttackDamage(unsigned int n)
 {
 	std::cout << "ClapTrap " << name << " now has " << n << " attack damage." << std::endl;
 	attack_damage = n;
-}
-
-void		ClapTrap::status(void)
-{
-	std::cout << "ClapTrap " << name << " now has "
-	<< hit_points << " hit points, "
-	<< energy_points << " energy point." << std::endl;
 }
