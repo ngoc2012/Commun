c5c1c9abcb6866cf7bf0368bcc44602c678be5c0
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-06 17:14:01 +0200

    all

---
+++ CPP/CPP03/ex02/ClapTrap.cpp
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
+/*   Updated: 2023/10/06 13:45:36 by ngoc             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+void	ClapTrap::welcome(void) {
+	std::cout << "ClapTrap " << getName()
+	<< " (hit points: " << getHitPoints()
+	<< ", energy points: " << getEnergyPoints()
+	<< ", attack damage: " << getAttackDamage()
+	<< ", max hit: " << getMaxHit()
+	<< ") join the game." << std::endl;
+}
+
+ClapTrap::ClapTrap()
+{
+	name = std::string("RandomClap");
+	hit_points = 10;
+	energy_points = 10;
+	attack_damage = 0;
+	max_hit = 10;
+	welcome(); 
+}
+
+ClapTrap::ClapTrap(std::string name)
+:
+	name(name),
+	hit_points(10),
+	energy_points(10),
+	attack_damage(0),
+	max_hit(10)
+{ welcome(); }
+
+ClapTrap::ClapTrap(const ClapTrap& src)
+:
+	name(src.name),
+	hit_points(src.hit_points),
+	energy_points(src.energy_points),
+	attack_damage(src.attack_damage),
+	max_hit(src.max_hit)
+{
+	std::cout << "Another ";
+	welcome();
+}
+
+ClapTrap&	ClapTrap::operator=( ClapTrap const & src )
+{
+	name = src.name;
+	hit_points = src.hit_points;
+	energy_points = src.energy_points;
+	attack_damage = src.attack_damage;
+	max_hit = src.max_hit;
+	std::cout << "Another ";
+	return (*this);
+}
+
+ClapTrap::~ClapTrap() { std::cout << "ClapTrap " << name << " quit game." << std::endl; }
+
+std::ostream    &operator<<( std::ostream &o, const ClapTrap &c )
+{
+	o << "ClapTrap " << c.getName() << " now has "
+	<< c.getHitPoints() << " hit points, "
+	<< c.getEnergyPoints() << " energy point." << std::endl;
+	return o;
+}
+
+void	ClapTrap::attack(std::string const & target)
+{
+	if (energy_points && hit_points)
+	{
+		std::cout << "ClapTrap " << name << " attack " << target << ", causing " << attack_damage << " points of damage!" << std::endl;
+		energy_points--;
+	}
+	else
+		std::cout << "Alert!!! ClapTrap " << name << " tried to attack but has no more energy or hit." << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap " << name << " takes " << amount <<" damages." << std::endl;
+	if (amount > hit_points)
+		amount = hit_points;
+	hit_points -= amount;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (energy_points && hit_points)
+	{
+		hit_points += amount;
+		if (hit_points > max_hit)
+			hit_points = max_hit;
+		std::cout << "ClapTrap " << name << " is repaired " << amount << " points (" << hit_points << "  hit points)." << std::endl;
+		energy_points--;
+	}
+	else
+		std::cout << "Alert!!! ClapTrap " << name << " tried to be repaired but has no more energy or hit." << std::endl;
+}
+
+std::string	ClapTrap::getName(void) const { return (name); }
+unsigned int	ClapTrap::getHitPoints(void) const { return (hit_points); }
+unsigned int	ClapTrap::getEnergyPoints(void) const { return (energy_points); }
+unsigned int	ClapTrap::getAttackDamage(void) const { return (attack_damage); }
+unsigned int	ClapTrap::getMaxHit(void) const { return (max_hit); }
+
+void		ClapTrap::setName(std::string s)
+{
+	std::cout << "ClapTrap " << name << " now named " << s << "." << std::endl;
+	name = s;
+}
+void		ClapTrap::setAttackDamage(unsigned int n)
+{
+	std::cout << "ClapTrap " << name << " now has " << n << " attack damage." << std::endl;
+	attack_damage = n;
+}
---
+++ CPP/CPP03/ex02/main.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
+/*   Updated: 2023/10/06 13:56:07 by ngoc             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int main()
+{
+	std::cout << "Test constructor and destructor" << std::endl;
+	ScavTrap clap4;
+	std::cout << "====================================" << std::endl;
+	std::cout << "Test methods" << std::endl;
+	clap4.setName("Clap4");
+	ScavTrap clap5(clap4);
+	clap5.setName("Clap5");
+	clap5.guardGate();
+	std::cout << "====================================" << std::endl;
+	ClapTrap clap1("Clap1");
+	ScavTrap clap6 = clap5;
+	clap6.setName("Clap6");
+	clap6.guardGate();
+	std::cout << "====================================" << std::endl;
+	ClapTrap clap2;
+	ClapTrap clap3(clap1);
+
+	clap2.setName("Clap2");
+	clap3.setName("Clap3");
+	clap4.setName("Clap4");
+	clap4.guardGate();
+	clap1.setAttackDamage(1);
+	clap2.setAttackDamage(2);
+	clap3.setAttackDamage(3);
+	clap1.attack(clap2.getName());
+	clap2.takeDamage(clap1.getAttackDamage());
+	std::cout << clap1;
+	std::cout << clap2;
+	clap3.attack(clap1.getName());
+	clap1.takeDamage(clap3.getAttackDamage());
+	std::cout << clap1;
+	std::cout << clap3;
+	for (int i = 0; i < 10; i++)
+	{
+		clap1.attack(clap2.getName());
+		clap2.takeDamage(clap1.getAttackDamage());
+		std::cout << clap1;
+		std::cout << clap2;
+	}
+	clap3.attack(clap4.getName());
+	clap4.takeDamage(clap3.getAttackDamage());
+	std::cout << clap3;
+	std::cout << clap4;
+	clap4.beRepaired(1);
+	std::cout << clap4;
+	clap4.attack(clap3.getName());
+	clap3.takeDamage(clap4.getAttackDamage());
+	std::cout << clap3;
+	std::cout << clap4;
+	clap2.beRepaired(5);
+	clap1.beRepaired(2);
+	clap3.beRepaired(1);
+	std::cout << clap1;
+	std::cout << clap2;
+	std::cout << clap3;
+	std::cout << clap6;
+	return (0);
+}
