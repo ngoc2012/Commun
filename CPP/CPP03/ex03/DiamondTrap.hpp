87af323ae6e074993963aea0abaefe38b1682dea
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-06 17:32:01 +0200

    all

--- CPP/CPP03/ex03/DiamondTrap.hpp
+++ CPP/CPP03/ex03/DiamondTrap.hpp
@@ -1,19 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/06 17:26:58 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/06 17:30:12 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FRAGTRAP_HPP
-# define FRAGTRAP_HPP
+#ifndef DIAMONDTRAP_HPP
+# define DIAMONDTRAP_HPP
 
 #include "ClapTrap.hpp"
+#include "FragTrap.hpp"
 
 class FragTrap : public ClapTrap
 {
