dc8a81805cac436c30bbaf3e38260d3b6bdfee15
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-07 13:28:01 +0200

    all

--- Cub3D/srcs/walls.c
+++ Cub3D/srcs/walls.c
@@ -6,11 +6,114 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/07 12:45:54 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/07 13:27:05 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+static void	render(t_game *g, int ix)
+{
+	int	tx;
+	int	ty;
+	int	h_slide;
+	float	h;
+	float	p;
+	if (g->pos.dA > g->pos.dB)
+	{
+		d = g->pos.dB / g->cos_ai0[ix];
+		tx = (int) (Bpy - BOX_SIZE * (float) g->pos.By);
+		if (g->map.v[g->pos.By][g->pos.Bx] == B_DOOR)
+		{
+			if (g->pos.By == g->opened_door_y && g->pos.Bx == g->opened_door_x)
+				tx -= g->hidden_door;
+			tex = &g->tex[DO];
+		}
+		else if (ai > -90 && ai < 90)
+			tex = &g->tex[WE];
+		else
+			tex = &g->tex[EA];
+	}
+	else
+	{
+		d = g->pos.dA / g->cos_ai0[ix];
+		tx = (int) (Apx - BOX_SIZE * (float) g->pos.Ax);
+		if (g->map.v[g->pos.Ay][g->pos.Ax] == B_DOOR)
+		{
+			if (g->pos.Ay == g->opened_door_y && g->pos.Ax == g->opened_door_x)
+				tx -= g->hidden_door;
+			tex = &g->tex[DO];
+		}
+		else if (ai > 0)
+			tex = &g->tex[NO];
+		else
+			tex = &g->tex[SO];
+	}
+	if (d < 0)
+		d = -d;
+	h = BOX_SIZE / d * g->dpp;
+	p = 1.0 / d * g->dpp;
+	h_slide = (int) (BOX_SIZE / d * g->dpp);
+	if (h_slide > HEIGHT)
+		h_slide = HEIGHT;
+	int	yp;
+	addr = (int *)g->mlx.addr;
+	addr_t = (int *)tex->addr;
+	addr += ix;
+	int	start = HEIGHT / 2 - h_slide / 2;
+	float	dh;
+	int	xh;
+	int	yh;
+	float	xph;
+	float	yph;
+	yp = -1;
+	while (++yp < start)
+	{
+		if (g->cl_color)
+			*addr = g->cl_color;
+		else
+		{
+			dh = g->dpp * BOX_SIZE / 2 / (HEIGHT / 2 - yp) * g->cos_ai0[ix];
+			xph = g->pos.px + dh * g->cos_ai[ix][g->pos.rot];
+			yph = g->pos.py - dh * g->sin_ai[ix][g->pos.rot];
+			xh = (int) (xph - ((int) (xph / BOX_SIZE)) * BOX_SIZE);
+			yh = (int) (yph - ((int) (yph / BOX_SIZE)) * BOX_SIZE);
+			if (xh < BOX_SIZE && xh >= 0 && yh < BOX_SIZE && yh >= 0)
+				*addr = *(addr_c + xh + yh * g->tex[CL].l);
+		}
+		addr += WIDTH;
+	}
+	if (tx < BOX_SIZE && tx >= 0)
+	{
+		yp = -1;
+		while (++yp < h_slide)
+		{
+			ty = (int) (((h - (float) h_slide) / 2.0 + (double) yp) / p);
+			if (ty < BOX_SIZE && ty >= 0)
+				*addr = *(addr_t + tx + ty * tex->l);
+			addr += WIDTH;
+		}
+	}
+	else
+		addr += h_slide * WIDTH;
+	yp = start + h_slide - 1;
+	while (++yp < HEIGHT)
+	{
+		if (g->fl_color)
+			*addr = g->fl_color;
+		else
+		{
+			dh = g->dpp * BOX_SIZE / 2 / (yp - HEIGHT / 2) * g->cos_ai0[ix];
+			xph = g->pos.px + dh * g->cos_ai[ix][g->pos.rot];
+			yph = g->pos.py - dh * g->sin_ai[ix][g->pos.rot];
+			xh = (int) (xph - ((int) (xph / BOX_SIZE)) * BOX_SIZE);
+			yh = (int) (yph - ((int) (yph / BOX_SIZE)) * BOX_SIZE);
+			if (xh < BOX_SIZE && xh >= 0 && yh < BOX_SIZE && yh >= 0)
+				*addr = *(addr_f + xh + yh * g->tex[FL].l);
+		}
+		addr += WIDTH;
+	}
+}
 
 float	render_walls(t_game *g, int ix)
 {
@@ -265,105 +368,6 @@ float	render_walls(t_game *g, int ix)
 			}
 		}
 	}
-	int	tx;
-	int	ty;
-	int	h_slide;
-	float	h;
-	float	p;
-	if (g->pos.dA > g->pos.dB)
-	{
-		d = g->pos.dB / g->cos_ai0[ix];
-		tx = (int) (Bpy - BOX_SIZE * (float) g->pos.By);
-		if (g->map.v[g->pos.By][g->pos.Bx] == B_DOOR)
-		{
-			if (g->pos.By == g->opened_door_y && g->pos.Bx == g->opened_door_x)
-				tx -= g->hidden_door;
-			tex = &g->tex[DO];
-		}
-		else if (ai > -90 && ai < 90)
-			tex = &g->tex[WE];
-		else
-			tex = &g->tex[EA];
-	}
-	else
-	{
-		d = g->pos.dA / g->cos_ai0[ix];
-		tx = (int) (Apx - BOX_SIZE * (float) g->pos.Ax);
-		if (g->map.v[g->pos.Ay][g->pos.Ax] == B_DOOR)
-		{
-			if (g->pos.Ay == g->opened_door_y && g->pos.Ax == g->opened_door_x)
-				tx -= g->hidden_door;
-			tex = &g->tex[DO];
-		}
-		else if (ai > 0)
-			tex = &g->tex[NO];
-		else
-			tex = &g->tex[SO];
-	}
-	if (d < 0)
-		d = -d;
-	h = BOX_SIZE / d * g->dpp;
-	p = 1.0 / d * g->dpp;
-	h_slide = (int) (BOX_SIZE / d * g->dpp);
-	if (h_slide > HEIGHT)
-		h_slide = HEIGHT;
-	int	yp;
-	addr = (int *)g->mlx.addr;
-	addr_t = (int *)tex->addr;
-	addr += ix;
-	int	start = HEIGHT / 2 - h_slide / 2;
-	float	dh;
-	int	xh;
-	int	yh;
-	float	xph;
-	float	yph;
-	yp = -1;
-	while (++yp < start)
-	{
-		if (g->cl_color)
-			*addr = g->cl_color;
-		else
-		{
-			dh = g->dpp * BOX_SIZE / 2 / (HEIGHT / 2 - yp) * g->cos_ai0[ix];
-			xph = g->pos.px + dh * g->cos_ai[ix][g->pos.rot];
-			yph = g->pos.py - dh * g->sin_ai[ix][g->pos.rot];
-			xh = (int) (xph - ((int) (xph / BOX_SIZE)) * BOX_SIZE);
-			yh = (int) (yph - ((int) (yph / BOX_SIZE)) * BOX_SIZE);
-			if (xh < BOX_SIZE && xh >= 0 && yh < BOX_SIZE && yh >= 0)
-				*addr = *(addr_c + xh + yh * g->tex[CL].l);
-		}
-		addr += WIDTH;
-	}
-	if (tx < BOX_SIZE && tx >= 0)
-	{
-		yp = -1;
-		while (++yp < h_slide)
-		{
-			ty = (int) (((h - (float) h_slide) / 2.0 + (double) yp) / p);
-			if (ty < BOX_SIZE && ty >= 0)
-				*addr = *(addr_t + tx + ty * tex->l);
-			addr += WIDTH;
-		}
-	}
-	else
-		addr += h_slide * WIDTH;
-	yp = start + h_slide - 1;
-	while (++yp < HEIGHT)
-	{
-		if (g->fl_color)
-			*addr = g->fl_color;
-		else
-		{
-			dh = g->dpp * BOX_SIZE / 2 / (yp - HEIGHT / 2) * g->cos_ai0[ix];
-			xph = g->pos.px + dh * g->cos_ai[ix][g->pos.rot];
-			yph = g->pos.py - dh * g->sin_ai[ix][g->pos.rot];
-			xh = (int) (xph - ((int) (xph / BOX_SIZE)) * BOX_SIZE);
-			yh = (int) (yph - ((int) (yph / BOX_SIZE)) * BOX_SIZE);
-			if (xh < BOX_SIZE && xh >= 0 && yh < BOX_SIZE && yh >= 0)
-				*addr = *(addr_f + xh + yh * g->tex[FL].l);
-		}
-		addr += WIDTH;
-	}
+	render(g, ix);
 	return (d);
-
 }
