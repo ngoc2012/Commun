6b1e8ed96daf8a1f9f25379b43aeac845b1e883d
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-07 13:36:01 +0200

    all

--- Cub3D/includes/cub3D.h
+++ Cub3D/includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 09:21:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/07 12:47:30 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/07 13:35:43 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@
 
 # define WIDTH 600
 # define HEIGHT 300
-# define SCALE 6
+# define SCALE 4
 # define FOV 90
 # define BOX_SIZE 64
 # define WALL_COLISION 8
