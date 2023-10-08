38153c690503a829d2bfd37990a66d5f6c27252e
Minh ngoc NGUYEN <minh-ngu@bess-f2r5s1.clusters.42paris.fr>
2023-10-08 14:42:02 +0200

    all

--- CPP/CPP04/ex00/Aminal.hpp
+++ CPP/CPP04/ex00/Aminal.hpp
@@ -6,12 +6,12 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/08 14:39:21 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/10/08 14:40:26 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLAPTRAP_HPP
-# define CLAPTRAP_HPP
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
 
 #include <iostream>
 
@@ -19,27 +19,6 @@ class Animal
 {
 	protected:
 		std::string	type;
-	public:
-		ClapTrap();
-		ClapTrap(std::string name);
-		ClapTrap(const ClapTrap&);
-		ClapTrap &operator=(const ClapTrap& op);
-		~ClapTrap();
-
-		void		welcome(void);
-		void		attack(const std::string& target);
-		void		takeDamage(unsigned int amount);
-		void		beRepaired(unsigned int amount);
-
-		std::string	getName(void) const;
-		unsigned int	getHitPoints(void) const;
-		unsigned int	getEnergyPoints(void) const;
-		unsigned int	getAttackDamage(void) const;
-
-		void		setName(std::string);
-		void		setAttackDamage(unsigned int);
 };
-
-std::ostream&	operator<<( std::ostream& o, const ClapTrap& n );
 
 #endif
