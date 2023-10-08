578c7d6eab1b9e72d50e4f7bd542c8826e764b73
Minh ngoc NGUYEN <minh-ngu@bess-f2r5s1.clusters.42paris.fr>
2023-10-08 14:40:02 +0200

    all

--- CPP/CPP04/ex00/Aminal.hpp
+++ CPP/CPP04/ex00/Aminal.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*   Aminal.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/08 07:27:10 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/08 14:39:21 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,10 @@
 
 #include <iostream>
 
-class ClapTrap
+class Animal
 {
-	private:
-		std::string	name;
-		unsigned int	hit_points;
-		unsigned int	energy_points;
-		unsigned int	attack_damage;
-		static const unsigned int	max_hit = 10;
+	protected:
+		std::string	type;
 	public:
 		ClapTrap();
 		ClapTrap(std::string name);
