c4babbcf0ebb42208e47627f23fd4c694bfae259
Minh ngoc NGUYEN <minh-ngu@bess-f2r5s1.clusters.42paris.fr>
2023-10-08 15:44:01 +0200

    all

--- CPP/CPP04/ex00/Cat.cpp
+++ CPP/CPP04/ex00/Cat.cpp
@@ -6,25 +6,27 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/08 15:40:01 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/10/08 15:43:47 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 
-Cat::Cat() {std::cout << "Cat " << type << " default constructor." << std::endl;}
+Cat::Cat() {
+	type = "Cat";
+	std::cout << type << " default constructor." << std::endl;
+}
 Cat::Cat(const Cat& src) {
 	*this = src;
-	std::cout << "Cat " << type << " copy constructor." << std::endl
+	std::cout << type << " copy constructor." << std::endl
 }
 Cat&	Cat::operator=( Cat const & src )
 {
 	type = src.type;
 	return (*this);
 }
-Cat::~Cat() { std::cout << "Cat " << type << " destructor." << std::endl; }
+Cat::~Cat() { std::cout << type << " destructor." << std::endl; }
 void    makeSound( void ) const
 {
-	std::cout << "Cat make sound." << std::endl;
+	std::cout << "Meo..." << std::endl;
 }
-std::string     getType( void ) const {return (type);}
