bce5b76c5bd6409ceb7c14ae20a1e9e9f645bb78
Minh ngoc NGUYEN <minh-ngu@bess-f2r3s20.clusters.42paris.fr>
2023-10-08 17:22:01 +0200

    all

---
+++ CPP/CPP04/ex00/Dog.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
+/*   Updated: 2023/10/08 15:43:47 by minh-ngu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cat.hpp"
+
+Cat::Cat() {
+	type = "Cat";
+	std::cout << type << " default constructor." << std::endl;
+}
+Cat::Cat(const Cat& src) {
+	*this = src;
+	std::cout << type << " copy constructor." << std::endl
+}
+Cat&	Cat::operator=( Cat const & src )
+{
+	type = src.type;
+	return (*this);
+}
+Cat::~Cat() { std::cout << type << " destructor." << std::endl; }
+void    makeSound( void ) const
+{
+	std::cout << "Meo..." << std::endl;
+}
