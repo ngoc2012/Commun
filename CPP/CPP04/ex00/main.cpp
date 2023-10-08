5112dc2d964da50fafd0dca1462d425138da068c
Minh ngoc NGUYEN <minh-ngu@bess-f2r3s20.clusters.42paris.fr>
2023-10-08 17:28:01 +0200

    all

--- CPP/CPP04/ex00/main.cpp
+++ CPP/CPP04/ex00/main.cpp
@@ -6,50 +6,22 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/06 13:26:37 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/08 17:27:09 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
 
 int main()
 {
-	std::cout << std::endl;
-	std::cout << "clap1 constructor" << std::endl;
-	ClapTrap clap1("Clap1");
-	std::cout << "clap2 constructor" << std::endl;
-	ClapTrap clap2;
-	std::cout << "clap3 constructor" << std::endl;
-	ClapTrap clap3(clap1);
-	std::cout << "clap4 constructor" << std::endl;
-	ClapTrap clap4 = clap1;
-
-	clap2.setName("Clap2");
-	clap3.setName("Clap3");
-	clap4.setName("Clap4");
-	clap1.setAttackDamage(1);
-	clap2.setAttackDamage(2);
-	clap3.setAttackDamage(3);
-	clap1.attack(clap2.getName());
-	clap2.takeDamage(clap1.getAttackDamage());
-	std::cout << clap1;
-	std::cout << clap2;
-	clap3.attack(clap1.getName());
-	clap1.takeDamage(clap3.getAttackDamage());
-	std::cout << clap1;
-	std::cout << clap3;
-	for (int i = 0; i < 10; i++)
-	{
-		clap1.attack(clap2.getName());
-		clap2.takeDamage(clap1.getAttackDamage());
-		std::cout << clap1;
-		std::cout << clap2;
-	}
-	clap2.beRepaired(5);
-	clap1.beRepaired(2);
-	clap3.beRepaired(1);
-	std::cout << clap1;
-	std::cout << clap2;
-	std::cout << clap3;
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
 	return (0);
 }
