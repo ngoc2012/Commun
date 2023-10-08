f430dbd977dd0661e2c1192e2ea9ed8601763aa3
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-08 21:36:01 +0200

    all

--- Cub3D/srcs/walls.c
+++ Cub3D/srcs/walls.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 12:57:31 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/08 21:33:39 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/08 21:35:51 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -244,21 +244,18 @@ void	get_A1(t_game *g, int ix, float ai)
 		g->pos.dA = INFINI;
 		return ;
 	}
-	if (g->map.v[Ay][Ax] == B_DOOR && ai > 0.0)
+	if (g->map.v[g->pos.Ay][g->pos.Ax] == B_DOOR && ai > 0.0)
 	{
-		g->pos.dA = (g->pos.py - Apy + BOX_SIZE / 2) / g->sin_ai[ix][g->pos.rot];
-		Apx += dpx / 2;
+		g->pos.dA = (g->pos.py - g->pos.Apy + BOX_SIZE / 2) / g->sin_ai[ix][g->pos.rot];
+		g->pos.Apx += g->pos.dpx / 2;
 	}
-	else if (g->map.v[Ay][Ax] == B_DOOR)
+	else if (g->map.v[g->pos.Ay][g->pos.Ax] == B_DOOR)
 	{
-		g->pos.dA = (g->pos.py - Apy - BOX_SIZE / 2) / g->sin_ai[ix][g->pos.rot];
-		Apx += dpx / 2;
+		g->pos.dA = (g->pos.py - g->pos.Apy - BOX_SIZE / 2) / g->sin_ai[ix][g->pos.rot];
+		g->pos.Apx += g->pos.dpx / 2;
 	}
 	else
-		g->pos.dA = (g->pos.py - Apy) / g->sin_ai[ix][g->pos.rot];
-	g->pos.Ax = Ax;
-	g->pos.Ay = Ay;
-	g->pos.Apx = Apx;
+		g->pos.dA = (g->pos.py - g->pos.Apy) / g->sin_ai[ix][g->pos.rot];
 }
 
 void	get_A1_(t_game *g, int ix, float ai)
