59e741b7a2afc417b4d9d39f62dcad7de012a923
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-09 21:40:01 +0200

    all

--- Cub3D/srcs/get_B.c
+++ Cub3D/srcs/get_B.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_B.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   by: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/09 05:30:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/09 21:36:43 by ngoc             ###   ########.fr       */
+/*   Created: 2023/10/09 21:38:22 by ngoc              #+#    #+#             */
+/*   Updated: 2023/10/09 21:40:01 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,19 @@ void	get_b2(t_game *g, int ix, float ai)
 	}
 	if (g->map.v[g->pos.by][g->pos.bx] == b_door && ai > -90.0 && ai < 90.0)
 	{
-		g->pos.db = (g->pos.bpx - g->pos.px + BOX_SIZE / 2) / g->cos_ai[ix][g->pos.rot];
+		g->pos.db = (g->pos.bpx - g->pos.px + BOX_SIZE / 2)
+			/ g->cos_ai[ix][g->pos.rot];
 		g->pos.bpy += g->pos.dpy / 2;
 	}
 	else if (g->map.v[g->pos.by][g->pos.bx] == b_door)
 	{
-		g->pos.db = (g->pos.bpx - g->pos.px - BOX_SIZE / 2) / g->cos_ai[ix][g->pos.rot];
+		g->pos.db = (g->pos.bpx - g->pos.px - BOX_SIZE / 2)
+			/ g->cos_ai[ix][g->pos.rot];
 		g->pos.bpy += g->pos.dpy / 2;
 	}
 	else
-		g->pos.db = (g->pos.bpx - g->pos.px) / g->cos_ai[ix][g->pos.rot];
+		g->pos.db = (g->pos.bpx - g->pos.px)
+			/ g->cos_ai[ix][g->pos.rot];
 }
 
 void	get_b1(t_game *g, int ix, float ai)
@@ -42,10 +45,15 @@ void	get_b1(t_game *g, int ix, float ai)
 		g->pos.bx = g->pos.bpx / BOX_SIZE;
 	else
 		g->pos.bx = g->pos.bpx / BOX_SIZE - 1;
-	door_coor = (int) (g->pos.bpy + g->pos.dpy / 2 - BOX_SIZE * (float) g->pos.by);
-	while ((g->pos.bx >= 0 && g->pos.bx < g->map.l) && (g->pos.by >= 0 && g->pos.by < g->map.h) &&
-			((g->map.v[g->pos.by][g->pos.bx] != b_wall && g->map.v[g->pos.by][g->pos.bx] != b_door)
-			 || (g->pos.by == g->opened_door_y && g->pos.bx == g->opened_door_x && g->map.v[g->pos.by][g->pos.bx] == b_door && door_coor < g->hidden_door)))
+	door_coor = (int)(g->pos.bpy + g->pos.dpy / 2
+			- BOX_SIZE * (float) g->pos.by);
+	while ((g->pos.bx >= 0 && g->pos.bx < g->map.l)
+		&& (g->pos.by >= 0 && g->pos.by < g->map.h)
+		&& ((g->map.v[g->pos.by][g->pos.bx] != b_wall
+			&& g->map.v[g->pos.by][g->pos.bx] != b_door)
+			|| (g->pos.by == g->opened_door_y && g->pos.bx == g->opened_door_x
+			&& g->map.v[g->pos.by][g->pos.bx] == b_door
+			&& door_coor < g->hidden_door)))
 	{
 		g->pos.bpx += g->pos.dpx;
 		g->pos.bpy += g->pos.dpy;
