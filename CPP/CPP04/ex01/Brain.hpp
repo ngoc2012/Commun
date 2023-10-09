9e91c470b8de2b88100f11c829f9e3ea604ea58d
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-09 13:40:02 +0200

    all

--- CPP/CPP04/ex01/Brain.hpp
+++ CPP/CPP04/ex01/Brain.hpp
@@ -6,18 +6,19 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/09 13:37:39 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/09 13:38:44 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CAT_HPP
-# define CAT_HPP
+#ifndef BRAIN_HPP
+# define BRAIN_HPP
 
 #include <iostream>
-#include "Animal.hpp"
 
-class Brain : public Animal
+class Brain
 {
+	private:
+		std::string	ideas[100];
 	public:
 		Brain();
 		Brain(const Brain&);
