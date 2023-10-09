763cbc0c59b54e97fdf0e60e8beb8f6db7364389
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-09 13:20:02 +0200

    all

--- CPP/CPP04/ex00/Animal.hpp
+++ CPP/CPP04/ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/09 13:14:48 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/09 13:18:51 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,10 @@ class Animal
 		Animal();
 		Animal(const Animal&);
 		Animal &operator=(const Animal& op);
-		~Animal();
+		/*
+		Deleting Through Base Class Pointer: The warning occurs when you use the delete operator to destroy an object through a pointer or reference to a base class (e.g., Animal* ptr = new Dog; delete ptr;). If the base class has a non-virtual destructor, it may not correctly call the derived class's destructor, potentially leading to resource leaks or undefined behavior.
+		*/
+		virtual ~Animal();
 
 		virtual void    makeSound( void ) const;
 		std::string     getType( void ) const;
