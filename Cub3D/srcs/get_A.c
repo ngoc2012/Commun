f3cfbed4e2fc807545d0b6a1afbdcc093c7a6368
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-09 10:02:01 +0200

    all

--- Cub3D/srcs/get_A.c
+++ Cub3D/srcs/get_A.c
@@ -6,11 +6,32 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 05:27:15 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/09 08:14:15 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/09 10:01:20 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+static void	get_A2(t_game *g, int ix)
+{
+	if (g->pos.Ax < 0 || g->pos.Ax >= g->map.l || g->pos.Ay < 0 || g->pos.Ay >= g->map.h)
+	{
+		g->pos.dA = INFINI;
+		return ;
+	}
+	if (g->map.v[g->pos.Ay][g->pos.Ax] == B_DOOR && ai > 0.0)
+	{
+		g->pos.dA = (g->pos.py - g->pos.Apy + BOX_SIZE / 2) / g->sin_ai[ix][g->pos.rot];
+		g->pos.Apx += g->pos.dpx / 2;
+	}
+	else if (g->map.v[g->pos.Ay][g->pos.Ax] == B_DOOR)
+	{
+		g->pos.dA = (g->pos.py - g->pos.Apy - BOX_SIZE / 2) / g->sin_ai[ix][g->pos.rot];
+		g->pos.Apx += g->pos.dpx / 2;
+	}
+	else
+		g->pos.dA = (g->pos.py - g->pos.Apy) / g->sin_ai[ix][g->pos.rot];
+}
 
 static void	get_A(t_game *g, int ix, float ai)
 {
@@ -54,23 +75,7 @@ static void	get_A(t_game *g, int ix, float ai)
 			g->pos.Ay = g->pos.Apy / BOX_SIZE;
 		door_coor = (int) (g->pos.Apx + g->pos.dpx / 2 - BOX_SIZE * (float) g->pos.Ax);
 	}
-	if (g->pos.Ax < 0 || g->pos.Ax >= g->map.l || g->pos.Ay < 0 || g->pos.Ay >= g->map.h)
-	{
-		g->pos.dA = INFINI;
-		return ;
-	}
-	if (g->map.v[g->pos.Ay][g->pos.Ax] == B_DOOR && ai > 0.0)
-	{
-		g->pos.dA = (g->pos.py - g->pos.Apy + BOX_SIZE / 2) / g->sin_ai[ix][g->pos.rot];
-		g->pos.Apx += g->pos.dpx / 2;
-	}
-	else if (g->map.v[g->pos.Ay][g->pos.Ax] == B_DOOR)
-	{
-		g->pos.dA = (g->pos.py - g->pos.Apy - BOX_SIZE / 2) / g->sin_ai[ix][g->pos.rot];
-		g->pos.Apx += g->pos.dpx / 2;
-	}
-	else
-		g->pos.dA = (g->pos.py - g->pos.Apy) / g->sin_ai[ix][g->pos.rot];
+	get_A2(g, ix);
 }
 
 void	get_AB(t_game *g, int ix)
