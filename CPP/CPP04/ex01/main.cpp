e319ee775c021a484e6f09a70cb73d7f1bcd17c8
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-10 14:40:01 +0200

    all

--- CPP/CPP04/ex01/main.cpp
+++ CPP/CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/10 14:37:38 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/10 14:39:03 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ int main()
 {
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
+	j->setBrain("haha", 25);
+	j->showBrain();
 	j = k;
 	Dog k;
 	k = j;
+	k->showBrain();
 	delete(i);
 	delete(j);
 	return (0);
