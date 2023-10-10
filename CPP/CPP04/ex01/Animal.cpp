b73bd003502f98c2362c09c8603e54b912ceb5c0
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-10 15:02:01 +0200

    all

--- CPP/CPP04/ex01/Animal.cpp
+++ CPP/CPP04/ex01/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/08 17:30:55 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/10/10 15:01:19 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,9 @@ void    Animal::makeSound( void ) const
 	std::cout << "Animal make sound." << std::endl;
 }
 std::string     Animal::getType( void ) const {return (type);}
+void    Animal::showBrain( void ) const {}
+void	Animal::setBrain(std::string s, int i)
+{
+	(void)s;
+	(void)i;
+}
