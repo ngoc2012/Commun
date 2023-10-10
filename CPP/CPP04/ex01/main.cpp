da54a99868bd1f6ee4220a140f6028b4fe61cedc
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-10 13:08:02 +0200

    all

--- CPP/CPP04/ex01/main.cpp
+++ CPP/CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/10 12:56:44 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/10 13:06:24 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,9 @@
 
 int main()
 {
-	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
-	Animal k;
-	std::cout << "k:" << k.getType() << ", sound: ";
-	k.makeSound();
-	k = *i;
-	std::cout << "k:" << k.getType() << ", sound: ";
-	k.makeSound();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	const WrongAnimal* m = new WrongCat();
-	std::cout << "m:" << m->getType() << ", sound: ";
-	m->makeSound();
-	WrongAnimal n;
-	std::cout << "n:" << n.getType() << ", sound: ";
-	n.makeSound();
-	n = *m;
-	std::cout << "n:" << n.getType() << ", sound: ";
-	n.makeSound();
-	delete(meta);
 	delete(i);
 	delete(j);
-	delete(m);
 	return (0);
 }
