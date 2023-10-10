4cc2db6d879cbc09184cd482156810635770d883
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-10 13:04:02 +0200

    all

--- CPP/CPP04/ex01/Brain.cpp
+++ CPP/CPP04/ex01/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/10 13:01:25 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/10 13:03:39 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 Brain::Brain() {
 	std::cout << "Brain default constructor." << std::endl;
 }
-Brain::Brain(const Brain& src) : Animal(src) {
+Brain::Brain(const Brain& src){
 	std::cout << "Brain copy constructor." << std::endl;
 }
 Brain&	Brain::operator=( Brain const & src ) { return (*this); }
 Brain::~Brain() { std::cout << "Brain destructor." << std::endl; }
 void	Brain::show(void) const
 {
-	for (int i
+	for (int i = 0; i < MAX_BRAIN; i++)
+		std::cout << i << ": " << ideas.[i] << ",";
+	std::endl;
 }
--- CPP/CPP04/ex01/Brain.hpp
+++ CPP/CPP04/ex01/Brain.hpp
@@ -6,19 +6,20 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/10 13:00:50 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/10 13:03:26 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BRAIN_HPP
 # define BRAIN_HPP
+# define MAX_BRAIN 100
 
 #include <iostream>
 
 class Brain
 {
 	private:
-		std::string	ideas[100];
+		std::string	ideas[MAX_BRAIN];
 	public:
 		Brain();
 		Brain(const Brain&);
