c390803a0f773434eddd0a62781ff191ecf43b69
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-10 17:36:01 +0200

    all

--- CPP/CPP04/ex01/main.cpp
+++ CPP/CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/10 17:33:19 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/10 17:35:56 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,16 @@ int main()
 	k = *m;
 	k.showBrain();
 	delete(m);
-	/*
-	Cat* m = new Dog();
-	m->setBrain("haha", 25);
-	m->setBrain("hehe", 70);
-	m->setBrain("moemoe", -1);
-	m->setBrain("hoaoha", 100);
-	m->showBrain();
-	Dog k;
-	k = *m;
-	k.showBrain();
+	Cat* m1 = new Cat();
+	m1->setBrain("hoho", 1);
+	m1->setBrain("enen", 99);
+	m1->setBrain("moemoe", -1);
+	m1->setBrain("hoaoha", 100);
+	m1->showBrain();
+	Cat k1;
+	k1 = *m1;
+	k1.showBrain();
 	delete(m);
-	*/
 	//Animal	zoo[N_ANIMAL];
 	//for (int i = 0; i < N_ANIMAL; i++)
 	//{
