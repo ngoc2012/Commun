d935ec1db9c94ec6d2675ef3c7872c0a68492e0e
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-10 13:06:01 +0200

    all

--- CPP/CPP04/ex01/Brain.cpp
+++ CPP/CPP04/ex01/Brain.cpp
@@ -6,19 +6,19 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/10 13:03:39 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/10 13:05:26 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
 
-Brain::Brain() {
-	std::cout << "Brain default constructor." << std::endl;
+Brain::Brain() { std::cout << "Brain default constructor." << std::endl; }
+Brain::Brain(const Brain& src) { std::cout << "Brain copy constructor." << std::endl; }
+Brain&	Brain::operator=( Brain const & src ) {
+	for (int i = 0; i < MAX_BRAIN; i++)
+		ideas[i] = src.ideas[i];
+	return (*this);
 }
-Brain::Brain(const Brain& src){
-	std::cout << "Brain copy constructor." << std::endl;
-}
-Brain&	Brain::operator=( Brain const & src ) { return (*this); }
 Brain::~Brain() { std::cout << "Brain destructor." << std::endl; }
 void	Brain::show(void) const
 {
