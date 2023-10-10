aa60a7176cb8ceac842abf9db877fd2b4e3a64cc
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-10 13:14:16 +0200

    all

--- CPP/CPP04/ex01/Animal.hpp
+++ CPP/CPP04/ex01/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/10 13:06:55 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/10 13:08:09 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define ANIMAL_HPP
 
 #include <iostream>
-#include "Brain.hpp"
 
 class Animal
 {
