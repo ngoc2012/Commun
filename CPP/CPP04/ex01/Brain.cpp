1ca7642f244e1098590e45a9d6cafc2d5097d27a
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-10 14:32:01 +0200

    all

--- CPP/CPP04/ex01/Brain.cpp
+++ CPP/CPP04/ex01/Brain.cpp
@@ -6,14 +6,18 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/10 13:05:26 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/10 14:31:46 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
 
 Brain::Brain() { std::cout << "Brain default constructor." << std::endl; }
-Brain::Brain(const Brain& src) { std::cout << "Brain copy constructor." << std::endl; }
+Brain::Brain(const Brain& src) {
+	for (int i = 0; i < MAX_BRAIN; i++)
+		ideas[i] = src.ideas[i];
+	std::cout << "Brain copy constructor." << std::endl;
+}
 Brain&	Brain::operator=( Brain const & src ) {
 	for (int i = 0; i < MAX_BRAIN; i++)
 		ideas[i] = src.ideas[i];
@@ -23,6 +27,6 @@ Brain::~Brain() { std::cout << "Brain destructor." << std::endl; }
 void	Brain::show(void) const
 {
 	for (int i = 0; i < MAX_BRAIN; i++)
-		std::cout << i << ": " << ideas.[i] << ",";
-	std::endl;
+		std::cout << i << ": " << ideas[i] << ",";
+	std::cout << std::endl;
 }
