d23b799e95e4866074fc27e76b18867aaa1a87cb
Minh ngoc NGUYEN <minh-ngu@made-f0Ar3s7.clusters.42paris.fr>
2023-10-12 10:20:01 +0200

    all

--- CPP/CPP04/ex03/ICharacter.hpp
+++ CPP/CPP04/ex03/ICharacter.hpp
@@ -1,35 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Ice.hpp                                            :+:      :+:    :+:   */
+/*   Character.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 09:56:08 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/10/12 09:59:16 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ICE_HPP
-# define ICE_HPP
+#ifndef CHARACTER_HPP
+# define CHARACTER_HPP
+# define MAX_CHAR 4
 
 #include <iostream>
-#include "AMateria.hpp"
 
-class Ice
+class ICharacter
 {
-	protected:
-		Ice();
-		Ice(const Ice&);
-		std::string	type;
 	public:
-		Ice &operator=(const Ice& op);
-		Ice(std::string const & type);
-		virtual ~Ice();
-
-		//std::string const & getType() const; //Returns the materia type
-		//virtual Ice* clone() const = 0;
-		//virtual void use(ICharacter& target);
+		virtual ~ICharacter() {}
+		virtual std::string const & getName() const = 0;
+		virtual void equip(AMateria* m) = 0;
+		virtual void unequip(int idx) = 0;
+		virtual void use(int idx, ICharacter& target) = 0;
 };
 
 #endif
