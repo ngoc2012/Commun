7012178a2aac8e3be9aa557829168ad744d63154
Minh ngoc NGUYEN <minh-ngu@made-f0Ar3s7.clusters.42paris.fr>
2023-10-12 09:54:02 +0200

    all

--- CPP/CPP04/ex03/Ice.cpp
+++ CPP/CPP04/ex03/Ice.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 09:52:00 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/10/12 09:54:00 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,7 @@ Ice&	Ice::operator=( Ice const & src )
 	return (*this);
 }
 Ice::~Ice() { std::cout type << " destructor." << std::endl; }
+void	Ice::use(ICharacter& target)
+{
+	std::cout << "* shoots an ice bolt at " << target.type << " *" << std:endl;
+}
