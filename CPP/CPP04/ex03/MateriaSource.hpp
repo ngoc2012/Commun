6aa2cfa246959ed240015efc8a963489a5c846dd
Minh ngoc NGUYEN <minh-ngu@made-f0Ar3s7.clusters.42paris.fr>
2023-10-12 10:24:01 +0200

    all

--- CPP/CPP04/ex03/MateriaSource.hpp
+++ CPP/CPP04/ex03/MateriaSource.hpp
@@ -1,34 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Character.hpp                                      :+:      :+:    :+:   */
+/*   Ice.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 10:23:27 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/10/12 09:56:08 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CHARACTER_HPP
-# define CHARACTER_HPP
+#ifndef ICE_HPP
+# define ICE_HPP
 
 #include <iostream>
-#include "ICharacter.hpp"
+#include "AMateria.hpp"
 
-class Character
+class Ice
 {
 	protected:
-		Character();
-		Character(const Character&);
+		Ice();
+		Ice(const Ice&);
 		std::string	type;
 	public:
-		Character &operator=(const Character& op);
-		Character(std::string const & type);
-		virtual ~Character();
+		Ice &operator=(const Ice& op);
+		Ice(std::string const & type);
+		virtual ~Ice();
 
 		//std::string const & getType() const; //Returns the materia type
-		//virtual Character* clone() const = 0;
+		//virtual Ice* clone() const = 0;
 		//virtual void use(ICharacter& target);
 };
 
