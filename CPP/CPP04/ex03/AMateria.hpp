06464d0935074fcb409fba94e51acc99d5e89e6d
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-12 09:24:01 +0200

    all

--- CPP/CPP04/ex03/AMateria.hpp
+++ CPP/CPP04/ex03/AMateria.hpp
@@ -6,12 +6,12 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 09:05:45 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/12 09:10:06 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ANIMAL_HPP
-# define ANIMAL_HPP
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
 
 #include <iostream>
 
