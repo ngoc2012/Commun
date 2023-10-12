82f7ed7ccaeb649e2cdb6d14355dc59eb696333c
Minh ngoc NGUYEN <minh-ngu@made-f0Ar3s7.clusters.42paris.fr>
2023-10-12 13:40:01 +0200

    all

--- CPP/CPP04/ex03/Character.cpp
+++ CPP/CPP04/ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 13:36:14 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/10/12 13:38:05 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,9 @@ Character&	Character::operator=( Character const & src )
 	return (*this);
 }
 Character::~Character() { std::cout << "Character destructor." << std::endl; }
+void	Character::equip(AMateria* m)
+{
+}
 void	Character::use(ICharacter& target)
 {
 	std::cout << "* shoots an ice bolt at " << target.type << " *" << std:endl;
