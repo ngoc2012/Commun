a5fdd4ad4bc82d078b01e787cef6cbb0b74ff14e
Minh ngoc NGUYEN <minh-ngu@made-f0Ar3s7.clusters.42paris.fr>
2023-10-12 09:52:02 +0200

    all

--- CPP/CPP04/ex03/Ice.cpp
+++ CPP/CPP04/ex03/Ice.cpp
@@ -6,14 +6,19 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 09:46:42 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/10/12 09:52:00 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Ice.hpp"
 
-Ice::Ice() {std::cout << type << " default constructor." << std::endl;}
+Ice::Ice()
+{
+	type = "ice";
+	std::cout << type << " default constructor." << std::endl;
+}
 Ice::Ice(const Ice& src) {
+	type = src.type;
 	*this = src;
 	std::cout << type << " copy constructor." << std::endl;
 }
@@ -22,5 +27,4 @@ Ice&	Ice::operator=( Ice const & src )
 	type = src.type;
 	return (*this);
 }
-Ice::Ice(std::string const & _type) { type = _type; }
 Ice::~Ice() { std::cout type << " destructor." << std::endl; }
