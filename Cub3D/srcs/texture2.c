dc0cd5f1f56532822ea4d2cad71dfce0c214f1d5
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-12 15:40:01 +0200

    all

--- Cub3D/srcs/texture2.c
+++ Cub3D/srcs/texture2.c
@@ -6,7 +6,7 @@
 /*   By: nbechon <nbechon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 05:38:38 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 15:34:44 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/12 15:40:01 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,8 +129,8 @@ int	get_texture2(t_game *g, char **ss, char *s)
 		if (!g->cl_color)
 			return (return_error(s, ss));
 	}
-	else
-		return (return_error(s, ss));
+	//else
+	//	return (return_error(s, ss));
 	return (1);
 }
 
