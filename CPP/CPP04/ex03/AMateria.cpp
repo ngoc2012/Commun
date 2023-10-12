a019aecaf80fff29c1380beb96e42d313c68b067
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-12 09:10:01 +0200

    all

--- CPP/CPP04/ex03/AMateria.cpp
+++ CPP/CPP04/ex03/AMateria.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 09:07:54 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/12 09:09:20 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ AMateria&	AMateria::operator=( AMateria const & src )
 	type = src.type;
 	return (*this);
 }
+AMateria::AMateria(std::string const & _type) { type = _type; }
 AMateria::~AMateria() { std::cout << "AMateria " << type << " destructor." << std::endl; }
 std::string     AMateria::getType( void ) const {return (type);}
