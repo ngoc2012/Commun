fb2045c5c0eda10e90a381c3788c72362bc764ae
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-12 09:06:01 +0200

    all

--- CPP/CPP04/ex03/AMateria.hpp
+++ CPP/CPP04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 09:03:43 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/12 09:05:45 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,17 @@
 
 class AMateria
 {
-protected:
-[...]
-public:
-AMateria(std::string const & type);
-[...]
-std::string const & getType() const; //Returns the materia type
-virtual AMateria* clone() const = 0;
-virtual void use(ICharacter& target);
+	protected:
+		Animal();
+		Animal(const Animal&);
+		std::string	type;
+	public:
+		AMateria(std::string const & type);
+		virtual ~Animal();
+
+		std::string const & getType() const; //Returns the materia type
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
 };
 /*
 class Animal
