7918f16a4b3cf30413a4da0a738c6c6a1cfff323
Minh ngoc NGUYEN <minh-ngu@made-f0Ar3s7.clusters.42paris.fr>
2023-10-12 09:44:02 +0200

    all

--- CPP/CPP04/ex03/Ice.cpp
+++ CPP/CPP04/ex03/Ice.cpp
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   AMateria.cpp                                       :+:      :+:    :+:   */
+/*   Ice.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 09:09:20 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/12 09:42:50 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "AMateria.hpp"
+#include "Ice.hpp"
 
-AMateria::AMateria() {std::cout << "AMateria " << type << " default constructor." << std::endl;}
-AMateria::AMateria(const AMateria& src) {
+Ice::Ice() {std::cout << type << " default constructor." << std::endl;}
+Ice::Ice(const Ice& src) {
 	*this = src;
-	std::cout << "AMateria " << type << " copy constructor." << std::endl;
+	std::cout << type << " copy constructor." << std::endl;
 }
-AMateria&	AMateria::operator=( AMateria const & src )
+Ice&	Ice::operator=( Ice const & src )
 {
 	type = src.type;
 	return (*this);
 }
-AMateria::AMateria(std::string const & _type) { type = _type; }
-AMateria::~AMateria() { std::cout << "AMateria " << type << " destructor." << std::endl; }
-std::string     AMateria::getType( void ) const {return (type);}
+Ice::Ice(std::string const & _type) { type = _type; }
+Ice::~Ice() { std::cout type << " destructor." << std::endl; }
+std::string     Ice::getType( void ) const {return (type);}
