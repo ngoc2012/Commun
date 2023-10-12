668da5595c1d04af4492b79a2a5cffa2bcff6671
Minh ngoc NGUYEN <minh-ngu@made-f0Ar3s7.clusters.42paris.fr>
2023-10-12 10:22:02 +0200

    all

---
+++ CPP/CPP04/ex03/Character.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Ice.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
+/*   Updated: 2023/10/12 09:54:00 by minh-ngu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Ice.hpp"
+
+Ice::Ice()
+{
+	type = "ice";
+	std::cout << type << " default constructor." << std::endl;
+}
+Ice::Ice(const Ice& src) {
+	type = src.type;
+	*this = src;
+	std::cout << type << " copy constructor." << std::endl;
+}
+Ice&	Ice::operator=( Ice const & src )
+{
+	type = src.type;
+	return (*this);
+}
+Ice::~Ice() { std::cout type << " destructor." << std::endl; }
+void	Ice::use(ICharacter& target)
+{
+	std::cout << "* shoots an ice bolt at " << target.type << " *" << std:endl;
+}
