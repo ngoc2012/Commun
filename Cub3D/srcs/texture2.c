17140d4e0c6c576cfc1be2239072a568cfebca30
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-12 21:30:01 +0200

    all

--- Cub3D/srcs/texture2.c
+++ Cub3D/srcs/texture2.c
@@ -6,7 +6,7 @@
 /*   By: nbechon <nbechon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 05:38:38 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 20:08:17 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/12 21:29:58 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,19 @@ static int	g_t(t_game *g, t_tex *t, char *path)
 
 int	suite_g_t2(t_game *g, char **ss, char *s)
 {
-	if (!ft_strncmp("FL", ss[0], 3) && !g_t(g, &g->tex[t_fl], ss[1]))
-		return (return_error(s, ss));
-	if (!ft_strncmp("CL", ss[0], 3) && !g_t(g, &g->tex[t_cl], ss[1]))
-		return (return_error(s, ss));
+	if (!ft_strncmp("FL", ss[0], 3))
+	{
+		if (!g_t(g, &g->tex[t_fl], ss[1]))
+			return (return_error(s, ss));
+		g->fl_color = 0;
+	}
+	if (!ft_strncmp("CL", ss[0], 3))
+	{
+		if (!g_t(g, &g->tex[t_cl], ss[1]))
+			return (return_error(s, ss));
+		g->cl_color = 0;
+	}
+	return (1);
 }
 
 int	suite_g_t1(t_game *g, char **ss, char *s)
