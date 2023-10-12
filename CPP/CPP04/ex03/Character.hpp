6c525ebf30ae7865ea8abf2ecc7a4f20b86e1cbe
Minh ngoc NGUYEN <minh-ngu@made-f0Ar3s7.clusters.42paris.fr>
2023-10-12 13:34:02 +0200

    all

--- CPP/CPP04/ex03/Character.hpp
+++ CPP/CPP04/ex03/Character.hpp
@@ -6,19 +6,21 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 11:30:09 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/10/12 13:33:57 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHARACTER_HPP
 # define CHARACTER_HPP
+# define SLOTS 4
 
 #include <iostream>
 #include "ICharacter.hpp"
 
 class Character: public ICharacter
 {
-	protected:
+	private:
+		AMateria	*slots[SLOTS];
 		Character();
 		Character(const Character&);
 		std::string	type;
