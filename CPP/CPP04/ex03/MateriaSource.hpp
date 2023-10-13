63a15090fbbd42006472df3dc9c293070c882186
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-13 18:08:01 +0200

    all

--- CPP/CPP04/ex03/MateriaSource.hpp
+++ CPP/CPP04/ex03/MateriaSource.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/13 17:38:22 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/13 18:06:26 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # define SLOTS_MS 4
 
 #include <iostream>
+#include "ICharacter.hpp"
 #include "IMateriaSource.hpp"
 
 class MateriaSource: public IMateriaSource
