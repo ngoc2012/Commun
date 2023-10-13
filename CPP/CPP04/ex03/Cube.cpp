b98913bad036cc2cc7877da494333880564915cc
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-13 17:56:01 +0200

    all

---
+++ CPP/CPP04/ex03/Cube.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cube.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
+/*   Updated: 2023/10/13 17:56:00 by ngoc             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cub.hpp"
+
+Cub::Cub()
+{
+	type = "ice";
+	std::cout << type << " default constructor." << std::endl;
+}
+Cub::Cub(const Cub& src) {
+	*this = src;
+	std::cout << type << " copy constructor." << std::endl;
+}
+Cub&	Cub::operator=( Cub const & src )
+{
+	type = src.type;
+	return (*this);
+}
+Cub::~Cub() { std::cout type << " destructor." << std::endl; }
+void	Cub::use(ICharacter& target)
+{
+	std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std:endl;
+}
+
+Cub	*Cub::clone(void) const
+{
+	return (Cub(*this));
+}
---
+++ CPP/CPP04/ex03/Cube.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Ice.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
+/*   Updated: 2023/10/13 15:12:11 by ngoc             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ICE_HPP
+# define ICE_HPP
+
+#include <iostream>
+#include "AMateria.hpp"
+
+class Ice: public AMateria
+{
+	public:
+		Ice();
+		Ice(const Ice&);
+		Ice &operator=(const Ice& op);
+		Ice(std::string const & type);
+		virtual ~Ice();
+};
+
+#endif
