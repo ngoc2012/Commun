7a42578d555e2d60638672ce69808f3cafcdb6a6
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-13 18:04:01 +0200

    all

--- CPP/CPP04/ex03/ICharacter.hpp
+++ CPP/CPP04/ex03/ICharacter.hpp
@@ -1,25 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Character.hpp                                      :+:      :+:    :+:   */
+/*   ICharacter.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/12 09:59:16 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/10/13 18:03:22 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHARACTER_HPP
 # define CHARACTER_HPP
-# define MAX_CHAR 4
 
 #include <iostream>
 
 class ICharacter
 {
 	public:
-		virtual ~ICharacter() {}
+		virtual ~ICharacter() {};
 		virtual std::string const & getName() const = 0;
 		virtual void equip(AMateria* m) = 0;
 		virtual void unequip(int idx) = 0;
