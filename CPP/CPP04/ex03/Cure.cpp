094d1d839e26f1670e1e1403e19213132e0a5412
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-13 18:02:01 +0200

    all

--- CPP/CPP04/ex03/Cure.cpp
+++ CPP/CPP04/ex03/Cure.cpp
@@ -1,38 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Cube.cpp                                           :+:      :+:    :+:   */
+/*   Cure.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/13 17:56:48 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/13 18:00:23 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Cub.hpp"
+#include "Cure.hpp"
 
-Cub::Cub()
+Cure::Cure()
 {
 	type = "ice";
 	std::cout << type << " default constructor." << std::endl;
 }
-Cub::Cub(const Cub& src) {
+Cure::Cure(const Cure& src) {
 	*this = src;
 	std::cout << type << " copy constructor." << std::endl;
 }
-Cub&	Cub::operator=( Cub const & src )
+Cure&	Cure::operator=( Cure const & src )
 {
 	type = src.type;
 	return (*this);
 }
-Cub::~Cub() { std::cout type << " destructor." << std::endl; }
-void	Cub::use(ICharacter& target)
+Cure::~Cure() { std::cout type << " destructor." << std::endl; }
+void	Cure::use(ICharacter& target)
 {
 	std::cout << "* heals " << target.getName() << "'s wounds *" << std:endl;
 }
 
-Cub	*Cub::clone(void) const
+Cure	*Cure::clone(void) const
 {
-	return (Cub(*this));
+	return (Cure(*this));
 }
--- CPP/CPP04/ex03/Cure.hpp
+++ CPP/CPP04/ex03/Cure.hpp
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Cube.hpp                                           :+:      :+:    :+:   */
+/*   Cure.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/13 17:57:48 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/13 18:00:47 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUBE_HPP
-# define CUBE_HPP
+#ifndef CURE_HPP
+# define CURE_HPP
 
 #include <iostream>
 #include "AMateria.hpp"
 
-class Cub: public AMateria
+class Cure: public AMateria
 {
 	public:
-		Cub();
-		Cub(const Cub&);
-		Cub &operator=(const Cub& op);
-		Cub(std::string const & type);
-		virtual ~Cub();
+		Cure();
+		Cure(const Cure&);
+		Cure &operator=(const Cure& op);
+		Cure(std::string const & type);
+		virtual ~Cure();
 };
 
 #endif
