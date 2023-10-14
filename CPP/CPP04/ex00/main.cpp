6806b3902113e4587f10acc949f841b38718f6e1
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-14 16:30:01 +0200

    all

--- CPP/CPP04/ex00/main.cpp
+++ CPP/CPP04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/14 16:25:50 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/14 16:29:22 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,13 @@ int main()
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 	Animal k;
+	std::cout << "============ Test start ==========" << std::endl;
 	std::cout << "k:" << k.getType() << ", sound: ";
 	k.makeSound();
 	k = *i;
 	std::cout << "k:" << k.getType() << ", sound: ";
 	k.makeSound();
+	std::cout << "============ End Animal ==========" << std::endl;
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	i->makeSound(); //will output the cat sound!
