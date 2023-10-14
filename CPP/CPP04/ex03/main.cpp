6b25fca9209424001c6cb1649c4a60acdb31f87a
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-14 10:06:01 +0200

    all

--- CPP/CPP04/ex03/main.cpp
+++ CPP/CPP04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/14 10:02:08 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/14 10:04:41 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,11 @@ int main()
 	ICharacter* me = new Character("me");
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
 	std::cout << "============ tmp = src->createMateria(\"cure\"); ===========" << std::endl;
 	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	me->equip(tmp);
+	me->equip(tmp);
 
 	std::cout << "============= Character bob =================\n";
 	ICharacter* bob = new Character("bob");
