4669724203a52e91ee302ca92991058f4ba03c45
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-17 17:22:01 +0200

    all

--- Webserv/client.cpp
+++ Webserv/client.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/17 17:19:51 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/17 17:20:36 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,10 @@ int	main()
 	std::cout << response ;
 	//std::cout << std::endl;
 	//c = connect(s_fd, (struct sockaddr*)&addr, sizeof(addr)); 
-	//ret = recv(s_fd, response, BUFFER, 0);
-	//response[ret] = 0;
-	//std::cout << response ;
+	ret = recv(s_fd, response, BUFFER, 0);
+	std::cout << "ret = " << ret << std::endl;
+	response[ret] = 0;
+	std::cout << response ;
 	//while (ret && ret > 0)
 	//{
 	//	ret = recv(s_fd, response, BUFFER, 0);
