e70df823cb19349ef7029ca4dad3d3ff52a428be
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-17 17:06:01 +0200

    all

--- Webserv/client.cpp
+++ Webserv/client.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/17 17:03:36 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/17 17:05:42 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,10 @@ int	main()
 	int	c = connect(s_fd, (struct sockaddr*)&addr, sizeof(addr)); 
 	if (c < 0)
 		std::cerr << "Client : connect error" << std::endl;
-	char	response[BUFFER + 1];
-	int	ret = recv(s_fd, response, BUFFER, 0);
+	char* hello = "Hello from client";
+	send(s_fd, hello, strlen(hello), 0); 
+	char	response[BUFFER + 1] = {0};
+	int	ret = read(s_fd, response, BUFFER, 0);
 	response[ret] = 0;
 	std::cout << response ;
 	while (ret && ret > 0)
