dfe399306be156a57163d14ee31a40af4f8b624b
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-17 16:12:01 +0200

    all

--- Webserv/client.cpp
+++ Webserv/client.cpp
@@ -6,13 +6,20 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/17 15:58:58 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/17 16:11:46 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <
-#include <sys/socket.h> 
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
 
+# define SA struct sockaddr_in
 int	main()
 {
+	int	s = socket(AF_INET, SOCK_STREAM, 0);
+	SA	addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(9000);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	int	c = coonect(s_fd, 
 }
