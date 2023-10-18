95419d29550d86226196f120243eeaf072e8a2f3
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-18 07:44:01 +0200

    all

--- CPP/CPP05/ex00/Bureaucrat.hpp
+++ CPP/CPP05/ex00/Bureaucrat.hpp
@@ -6,12 +6,15 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/18 07:36:30 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/18 07:43:37 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
-#define BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# define MAX_GRADE 1
+# define MIN_GRADE 150
 
 #include <iostream>
 
@@ -29,8 +32,8 @@ class Bureaucrat
 		std::string	getName( void ) const;
 		int		getGrade( void ) const;
 
-		void		upGrade( void ) const;
-		void		downGrade( void ) const;
+		void		upGrade( void );
+		void		downGrade( void );
 
 		class GradeTooHighException : public std::exception
 		{
