ec99351c4c9c8ab74079946da211195366e526f1
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-18 08:02:01 +0200

    all

--- CPP/CPP05/ex00/main.cpp
+++ CPP/CPP05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/18 07:59:03 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/18 08:00:45 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 int	main()
 {
+	Bureaucrat b2("b2", 2);
+	std::cout << b2;
+	Bureaucrat b145("b145", 145);
+	std::cout << b145;
 	try {
-		Bureaucrat b2("b2", 2);
-		std::cout << b2;
 		for (int i = 0; i < 3; i++)
 			b2.upGrade();
-
-		Bureaucrat b145("b145", 145);
-		std::cout << b145 << std::endl;
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
 		for (int i = 0; i < 10; i++)
 			b145.downGrade();
 	} catch (std::exception &e) {
