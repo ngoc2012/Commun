d82f148f0a420daf11bdd19e181cecec0de5fb53
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-28 13:54:01 +0200

    all

--- CPP/CPP05/ex02/ShrubberyCreationForm.hpp
+++ CPP/CPP05/ex02/ShrubberyCreationForm.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/28 11:36:47 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/28 13:53:38 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,6 @@ class ShrubberyCreationForm: public AForm
 		AForm &operator=(const AForm& op);
 		AForm(const std::string, int, int);
 		virtual ~AForm();
-
-		std::string	getName( void ) const;
-		bool		getSign( void ) const;
-		int		getSignGrade( void ) const;
-		int		getExecGrade( void ) const;
-
-		void		beSigned( Bureaucrat& );
-		void		setSign( void );
-
-		class GradeTooHighException : public std::exception
-		{
-			public: virtual const char* what() const throw();
-		};
-		class GradeTooLowException : public std::exception
-		{
-			public: virtual const char* what() const throw();
-		};
 };
 
 #std::ostream&   operator<<(std::ostream&, const AForm&);
