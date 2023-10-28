be076b750494ad2d875cf89222b135cc8332b8d1
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-28 14:00:01 +0200

    all

--- CPP/CPP05/ex02/ShrubberyCreationForm.cpp
+++ CPP/CPP05/ex02/ShrubberyCreationForm.cpp
@@ -6,13 +6,18 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/28 13:55:42 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/28 13:58:25 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
 
-ShrubberyCreationForm::ShrubberyCreationForm() {std::cout << "ShrubberyCreationForm " << name << " default constructor." << std::endl;}
+ShrubberyCreationForm::ShrubberyCreationForm()
+{
+	sign_grade = 145;
+	exec_grade = 137;
+	std::cout << "ShrubberyCreationForm " << name << " default constructor." << std::endl;
+}
 
 ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& src) {
 	*this = src;
@@ -26,12 +31,6 @@ ShrubberyCreationForm&	ShrubberyCreationForm::operator=( ShrubberyCreationForm c
 	sign_grade = src.sign_grade;
 	exec_grade = src.exec_grade;
 	return (*this);
-}
-
-ShrubberyCreationForm::ShrubberyCreationForm(const std::string n, int sg, int eg) : name(n), sign_grade(sg), exec_grade(eg)
-{
-	sign = false;
-	std::cout << "ShrubberyCreationForm " << name << " (sign grade: " << sign_grade << ", execute grade: " << exec_grade << ") constructor with parameters." << std::endl;
 }
 
 ShrubberyCreationForm::~ShrubberyCreationForm() { std::cout << "ShrubberyCreationForm " << name << " destructor." << std::endl; }
