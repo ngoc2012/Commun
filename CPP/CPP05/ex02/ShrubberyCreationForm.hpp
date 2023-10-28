a127226a8c9a80437a2f010f140e024d5996ac44
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-28 15:44:01 +0200

    all

--- CPP/CPP05/ex02/ShrubberyCreationForm.hpp
+++ CPP/CPP05/ex02/ShrubberyCreationForm.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/28 14:52:37 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/28 15:42:07 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@
 
 class ShrubberyCreationForm: public AForm
 {
-	private:
-		void	beExecuted() const;
 	public:
 		ShrubberyCreationForm(const ShrubberyCreationForm&);
 		ShrubberyCreationForm &operator=(const ShrubberyCreationForm& op);
