5e50e01af0f0a28e06b47719206e7bd194c7e766
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-28 14:52:01 +0200

    all

--- CPP/CPP05/ex02/ShrubberyCreationForm.hpp
+++ CPP/CPP05/ex02/ShrubberyCreationForm.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/28 14:09:24 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/28 14:51:05 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,5 @@ class ShrubberyCreationForm: public ShrubberyCreationForm
 		ShrubberyCreationForm(const std::string);
 		virtual ~ShrubberyCreationForm();
 };
-
-//std::ostream&   operator<<(std::ostream&, const ShrubberyCreationForm&);
 
 #endif
