00528482c2421d9ebfbc9014ae3b0cd5dd296b43
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-30 09:08:01 +0100

    all

--- CPP/CPP05/ex02/PresidentialPardonForm.cpp
+++ CPP/CPP05/ex02/PresidentialPardonForm.cpp
@@ -1,62 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ShrubberyCreationForm.cpp                          :+:      :+:    :+:   */
+/*   PresidentialPardonForm.cpp                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/28 18:15:10 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/30 09:07:09 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
-//ShrubberyCreationForm::ShrubberyCreationForm(std::string t) : target(t)
-ShrubberyCreationForm::ShrubberyCreationForm(std::string t)
+PresidentialPardonForm::PresidentialPardonForm(std::string t)
 {
 	sign = false;
 	target = t;
 	sign_grade = 145;
 	exec_grade = 137;
-	std::cout << "ShrubberyCreationForm " << name << " default constructor." << std::endl;
+	std::cout << "PresidentialPardonForm " << name << " default constructor." << std::endl;
 }
 
-ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& src): AForm(src)
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& src): AForm(src)
 {
-	std::cout << "ShrubberyCreationForm " << name << " copy constructor." << std::endl;
+	std::cout << "PresidentialPardonForm " << name << " copy constructor." << std::endl;
 }
 
-ShrubberyCreationForm&	ShrubberyCreationForm::operator=( ShrubberyCreationForm const & src )
+PresidentialPardonForm&	PresidentialPardonForm::operator=( PresidentialPardonForm const & src )
 {
 	AForm::operator=(src);
 	return (*this);
 }
 
-ShrubberyCreationForm::~ShrubberyCreationForm() { std::cout << "ShrubberyCreationForm " << name << " destructor." << std::endl; }
+PresidentialPardonForm::~PresidentialPardonForm() { std::cout << "PresidentialPardonForm " << name << " destructor." << std::endl; }
 
-void	ShrubberyCreationForm::beExecuted() const
+void	PresidentialPardonForm::beExecuted() const
 {
-	std::ofstream file;
-
-	file.open(std::string(target + "_shrubbery").c_str(),
-		std::ios::out | std::ios::trunc);
-	// https://ascii.co.uk/art/tree
-	if(file.is_open())
-	{
-		file << "    oxoxoo    ooxoo" << std::endl;
-		file << "  ooxoxo oo  oxoxooo" << std::endl;
-		file << " oooo xxoxoo ooo ooox" << std::endl;
-		file << " oxo o oxoxo  xoxxoxo" << std::endl;
-		file << "  oxo xooxoooo o ooo" << std::endl;
-		file << "    ooo\\oo\\  /o/o" << std::endl;
-		file << "        \\  \\/ /" << std::endl;
-		file << "         |   /" << std::endl;
-		file << "         |  |" << std::endl;
-		file << "         | D|" << std::endl;
-		file << "         |  |" << std::endl;
-		file << "         |  |" << std::endl;
-		file << "  ______/____\\____" << std::endl;
-		file.close();
-	}
+	std::cout << target << " has been pardoned by Zaphod Beeblebrox." << std::endl;
 }
