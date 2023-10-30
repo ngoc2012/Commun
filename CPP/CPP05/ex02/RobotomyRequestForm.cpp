57ecc7557a7cc20a94533da749f549ccc1d8658c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-30 10:48:01 +0100

    all

--- CPP/CPP05/ex02/RobotomyRequestForm.cpp
+++ CPP/CPP05/ex02/RobotomyRequestForm.cpp
@@ -6,40 +6,39 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/30 09:01:38 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/30 10:47:32 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "RobotmyRequestForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
-//RobotmyRequestForm::RobotmyRequestForm(std::string t) : target(t)
-RobotmyRequestForm::RobotmyRequestForm(std::string t)
+RobotomyRequestForm::RobotomyRequestForm(std::string t)
 {
 	sign = false;
 	target = t;
 	sign_grade = 72;
 	exec_grade = 45;
-	std::cout << "RobotmyRequestForm " << name << " default constructor." << std::endl;
+	std::cout << "RobotomyRequestForm " << name << " default constructor." << std::endl;
 }
 
-RobotmyRequestForm::RobotmyRequestForm(const RobotmyRequestForm& src): AForm(src)
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm& src): AForm(src)
 {
-	std::cout << "RobotmyRequestForm " << name << " copy constructor." << std::endl;
+	std::cout << "RobotomyRequestForm " << name << " copy constructor." << std::endl;
 }
 
-RobotmyRequestForm&	RobotmyRequestForm::operator=( RobotmyRequestForm const & src )
+RobotomyRequestForm&	RobotomyRequestForm::operator=( RobotomyRequestForm const & src )
 {
 	AForm::operator=(src);
 	return (*this);
 }
 
-RobotmyRequestForm::~RobotmyRequestForm() { std::cout << "RobotmyRequestForm " << name << " destructor." << std::endl; }
+RobotomyRequestForm::~RobotomyRequestForm() { std::cout << "RobotomyRequestForm " << name << " destructor." << std::endl; }
 
-void	RobotmyRequestForm::beExecuted() const
+void	RobotomyRequestForm::beExecuted() const
 {
 	std::cout << "Vrmmmmm...vrmm..vrmm......." << std::endl;
 	if(rand() % 2)
-		std::cout << this->getTarget() << " was robotomized!" << std::endl;
+		std::cout << target << " was robotomized!" << std::endl;
 	else
 		std::cout << "The robotization messed up..." << std::endl;
 }
