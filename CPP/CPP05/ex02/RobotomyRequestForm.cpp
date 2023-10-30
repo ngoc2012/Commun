6acb8e56326196e9b6592629d42440b20a2d73f3
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-30 10:52:01 +0100

    all

--- CPP/CPP05/ex02/RobotomyRequestForm.cpp
+++ CPP/CPP05/ex02/RobotomyRequestForm.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/30 10:47:32 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/30 10:51:04 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ RobotomyRequestForm::~RobotomyRequestForm() { std::cout << "RobotomyRequestForm 
 void	RobotomyRequestForm::beExecuted() const
 {
 	std::cout << "Vrmmmmm...vrmm..vrmm......." << std::endl;
+	int	i = rand();
+	std::cout << i << i % 2 << std::endl;
 	if(rand() % 2)
 		std::cout << target << " was robotomized!" << std::endl;
 	else
