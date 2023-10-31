827701301971d9850ec4c1b0bfa07ceea3eeb1f1
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-31 09:46:01 +0100

    all

--- CPP/CPP05/ex03/Intern.cpp
+++ CPP/CPP05/ex03/Intern.cpp
@@ -1,76 +1,72 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   AForm.cpp                                          :+:      :+:    :+:   */
+/*   Intern.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/28 18:02:34 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/10/31 09:06:38 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "AForm.hpp"
+#include "Intern.hpp"
 
-AForm::AForm() {std::cout << "AForm " << name << " default constructor." << std::endl;}
+Intern::Intern() {std::cout << "Intern " << name << " default constructor." << std::endl;}
 
-AForm::AForm(const AForm& src) {
+Intern::Intern(const Intern& src) {
 	*this = src;
-	std::cout << "AForm " << name << " copy constructor." << std::endl;
+	std::cout << "Intern " << name << " copy constructor." << std::endl;
 }
 
-AForm&	AForm::operator=( AForm const & src )
+Intern&	Intern::operator=( Intern const & src )
 {
-	name = src.name;
-	sign = src.sign;
-	sign_grade = src.sign_grade;
-	exec_grade = src.exec_grade;
 	return (*this);
 }
 
-AForm::AForm(const std::string n, int sg, int eg) : name(n), sign_grade(sg), exec_grade(eg)
+Intern::Intern(const std::string n, int sg, int eg) : name(n), sign_grade(sg), exec_grade(eg)
 {
 	sign = false;
-	std::cout << "AForm " << name << " (sign grade: " << sign_grade << ", execute grade: " << exec_grade << ") constructor with parameters." << std::endl;
+	std::cout << "Intern " << name << " (sign grade: " << sign_grade << ", execute grade: " << exec_grade << ") constructor with parameters." << std::endl;
 }
 
-AForm::~AForm() { std::cout << "AForm " << name << " destructor." << std::endl; }
+Intern::~Intern() { std::cout << "Intern " << name << " destructor." << std::endl; }
 
-std::string	AForm::getName( void ) const {return (name);}
-bool		AForm::getSign( void ) const {return (sign);}
-int		AForm::getSignGrade( void ) const {return (sign_grade);}
-int		AForm::getExecGrade( void ) const {return (exec_grade);}
-void		AForm::setSign( void ) {sign = true;}
+std::string	Intern::getName( void ) const {return (name);}
+bool		Intern::getSign( void ) const {return (sign);}
+int		Intern::getSignGrade( void ) const {return (sign_grade);}
+int		Intern::getExecGrade( void ) const {return (exec_grade);}
+void		Intern::setSign( void ) {sign = true;}
 
-std::ostream&   operator<<(std::ostream& o, const AForm& b)
+std::ostream&   operator<<(std::ostream& o, const Intern& b)
 {
-	o << "AForm " << b.getName() << ", sign grade " << b.getSignGrade() << ", execute grade " << b.getExecGrade() << std::endl;
+	o << "Intern " << b.getName() << ", sign grade " << b.getSignGrade() << ", execute grade " << b.getExecGrade() << std::endl;
 	return (o);
 }
 
-void		AForm::beSigned( Bureaucrat& b)
+void		Intern::beSigned( Bureaucrat& b)
 {
 	if (sign)
 	{
-		std::cout << "AForm " << name << " already signed." << std::endl;
+		std::cout << "Intern " << name << " already signed." << std::endl;
 		return ;
 	}
 	if (b.getGrade() > sign_grade)
 	{
-		std::cerr << "AForm " << name << " could not be signed by " << b.getName() << " because: ";
-		throw AForm::GradeTooLowException();
+		std::cerr << "Intern " << name << " could not be signed by " << b.getName() << " because: ";
+		throw Intern::GradeTooLowException();
 	}
 	sign = true;
-	std::cout << "AForm " << name << " signed by " << b.getName() <<  "." << std::endl;
+	std::cout << "Intern " << name << " signed by " << b.getName() <<  "." << std::endl;
 }
-const char* AForm::GradeTooHighException::what() const throw() { return ("Grade is too high."); }
-const char* AForm::GradeTooLowException::what() const throw() { return ("Grade is too low."); }
-const char* AForm::FormNotSigned::what() const throw() { return ("Form not signed."); }
-void		AForm::execute(Bureaucrat const & executor) const
+const char* Intern::GradeTooHighException::what() const throw() { return ("Grade is too high."); }
+const char* Intern::GradeTooLowException::what() const throw() { return ("Grade is too low."); }
+const char* Intern::FormNotSigned::what() const throw() { return ("Form not signed."); }
+void		Intern::execute(Bureaucrat const & executor) const
 {
 	if (!sign)
-		throw AForm:: FormNotSigned();
+		throw Intern:: FormNotSigned();
 	else if (executor.getGrade() > exec_grade)
-		throw AForm::GradeTooLowException();
+		throw Intern::GradeTooLowException();
 	beExecuted();
 }
