b3b3d3b078a0b477650a8ced6a800247e710374c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-10-31 17:34:01 +0100

    all

---
+++ Webserv/srcs/server.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
+/*   Updated: 2023/10/31 17:32:17 by ngoc             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <arpa/inet.h>	// htons
+#include <unistd.h>	// close
+#include <iostream>
+
+# define BUFFER	1028
+
+int	main()
+{
+	int	s_fd = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in	addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(4242);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	int	c = connect(s_fd, (struct sockaddr*)&addr, sizeof(addr)); 
+	if (c < 0)
+		std::cerr << "Client : connect error" << std::endl;
+	char	response[BUFFER + 1];
+	int	ret = recv(s_fd, response, BUFFER, 0);
+	std::cout << "ret = " << ret << std::endl;
+	response[ret] = 0;
+	std::cout << response ;
+	//std::cout << std::endl;
+	//c = connect(s_fd, (struct sockaddr*)&addr, sizeof(addr)); 
+	ret = recv(s_fd, response, BUFFER, 0);
+	std::cout << "ret = " << ret << std::endl;
+	response[ret] = 0;
+	std::cout << response ;
+	//while (ret && ret > 0)
+	//{
+	//	ret = recv(s_fd, response, BUFFER, 0);
+	//	response[ret] = 0;
+	//	std::cout << response ;
+	//}
+	std::cout << std::endl;
+	close(s_fd);
+	std::cout << "End client" << std::endl;
+}
