4c49c629d72dbed0a81a401b721eb9365537a16e
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-01 14:44:01 +0100

    all

--- CPP/CPP05/ex03/main.cpp
+++ CPP/CPP05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/31 08:56:44 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/01 14:43:47 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,85 +28,95 @@ int	main()
 	std::cout << b10;
 
 	Intern someRandomIntern;
-	Form* rrf;
-	rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+	Form* rrf = someRandomIntern.makeForm("robotomy request1", "Bender");
 
-	ShrubberyCreationForm s0("haha");
-	try {
-		std::cout << "s0.beSigned(b145);" << std::endl;
-		s0.beSigned(b145);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	try {
-		std::cout << "b70.signForm(s0);" << std::endl;
-		b70.signForm(s0);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	try {
-		std::cout << "s0.execute(b145);" << std::endl;
-		s0.execute(b145);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	try {
-		std::cout << "s0.execute(b70);" << std::endl;
-		s0.execute(b70);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-
-	RobotomyRequestForm r0("robot");
-	try {
-		std::cout << "r0.beSigned(b145);" << std::endl;
-		r0.beSigned(b145);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	try {
-		std::cout << "b70.signForm(r0);" << std::endl;
-		b70.signForm(r0);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	try {
-		std::cout << "r0.execute(b70);" << std::endl;
-		r0.execute(b70);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	try {
-		std::cout << "r0.execute(b40);" << std::endl;
-		r0.execute(b40);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
+	Form* s0 = someRandomIntern.makeForm("shrubbery creation", "Shru");
+	if (s0)
+	{
+		try {
+			std::cout << "s0.beSigned(b145);" << std::endl;
+			s0.beSigned(b145);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::cout << "b70.signForm(s0);" << std::endl;
+			b70.signForm(s0);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::cout << "s0.execute(b145);" << std::endl;
+			s0.execute(b145);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::cout << "s0.execute(b70);" << std::endl;
+			s0.execute(b70);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
 	}
 
-	PresidentialPardonForm p0("PPF");
-	try {
-		std::cout << "p0.beSigned(b10);" << std::endl;
-		p0.beSigned(b10);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
+	Form* r0 = someRandomIntern.makeForm("robotomy request", "Bender");
+	if (r0)
+	{
+		RobotomyRequestForm r0("robot");
+		try {
+			std::cout << "r0.beSigned(b145);" << std::endl;
+			r0.beSigned(b145);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::cout << "b70.signForm(r0);" << std::endl;
+			b70.signForm(r0);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::cout << "r0.execute(b70);" << std::endl;
+			r0.execute(b70);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::cout << "r0.execute(b40);" << std::endl;
+			r0.execute(b40);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
 	}
-	try {
-		std::cout << "b2.signForm(p0);" << std::endl;
-		b2.signForm(p0);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	try {
-		std::cout << "p0.execute(b10);" << std::endl;
-		p0.execute(b10);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
-	try {
-		std::cout << "p0.execute(b2);" << std::endl;
-		p0.execute(b2);
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
+
+	Form* p0 = someRandomIntern.makeForm("presidential pardon", "Macron");
+	if (p0)
+	{
+		PresidentialPardonForm p0("PPF");
+		try {
+			std::cout << "p0.beSigned(b10);" << std::endl;
+			p0.beSigned(b10);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::cout << "b2.signForm(p0);" << std::endl;
+			b2.signForm(p0);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::cout << "p0.execute(b10);" << std::endl;
+			p0.execute(b10);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::cout << "p0.execute(b2);" << std::endl;
+			p0.execute(b2);
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
 	}
 	return (0);
 }
