4bb5aed9cdf358078b04ef05a77dd8c175f21a95
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-01 15:33:18 +0100

    all

--- CPP/CPP05/ex03/Intern.hpp
+++ CPP/CPP05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/01 14:44:44 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/01 14:46:40 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@
 
 class Intern
 {
-	public:
+	private:
 		Intern(const Intern&);
 		Intern &operator=(const Intern& op);
 		Intern(const std::string, int, int);
+	public:
+		Intern();
 		virtual ~Intern();
 
 		AForm	*makeForm(std::string, std::string);
