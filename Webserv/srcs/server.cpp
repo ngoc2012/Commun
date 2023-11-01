f0a4fc1108efa477f2813a44f0967aa70738da6a
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-01 08:04:01 +0100

    all

--- Webserv/srcs/server.cpp
+++ Webserv/srcs/server.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/01 07:59:28 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/01 08:02:19 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,11 +69,26 @@ int	main()
 		close(listen_sk);
 		exit(-1);
 	}
+
 	struct sockaddr_in	addr;
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(PORT);
 	addr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
 
+	if (bind(listen_sk, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+	{
+		perror("bind() failed");
+		close(listen_sk);
+		exit(-1);
+	}
+
+	//The  backlog  argument defines the maximum length to which the queue of pending connections for sockfd may grow.
+	if (listen(listen_sk, MAX_CLIENTS) < 0)
+	{
+		perror("listen() failed");
+		close(listen_sd);
+		exit(-1);
+	}
 	int	c = connect(s_fd, (struct sockaddr*)&addr, sizeof(addr)); 
 	if (c < 0)
 		std::cerr << "Client : connect error" << std::endl;
