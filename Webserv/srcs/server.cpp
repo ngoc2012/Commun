9d912b43e5cf5ee6ecc6cf281def560df026503d
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-01 09:56:01 +0100

    all

--- Webserv/srcs/server.cpp
+++ Webserv/srcs/server.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/01 09:50:21 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/01 09:54:31 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -157,6 +157,38 @@ int	main()
 			}
 			else
 			{
+				int	close_conn = 0;
+				do
+				{
+					//Receive data from client
+					int	ret = recv(s_fd, response, BUFFER, 0);
+					if (rc < 0)
+					{
+						if (errno != EWOULDBLOCK)
+						{
+							perror("  recv() failed");
+							close_conn = TRUE;
+						}
+						break;
+					}
+					std::cout << "ret = " << ret << std::endl;
+					response[ret] = 0;
+					std::cout << response ;
+					//std::cout << std::endl;
+					//c = connect(s_fd, (struct sockaddr*)&addr, sizeof(addr)); 
+					ret = recv(s_fd, response, BUFFER, 0);
+					std::cout << "ret = " << ret << std::endl;
+					response[ret] = 0;
+					std::cout << response ;
+					//while (ret && ret > 0)
+					//{
+					//	ret = recv(s_fd, response, BUFFER, 0);
+					//	response[ret] = 0;
+					//	std::cout << response ;
+					//}
+					std::cout << std::endl;
+
+				} while (1);
 			}
 		}
 	} while (!end_server);
