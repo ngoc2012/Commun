33f18e280273318ae62ff699a749a7870d819053
Minh ngoc NGUYEN <minh-ngu@made-f0Br3s6.clusters.42paris.fr>
2023-11-01 17:20:01 +0100

    all

--- CPP/CPP01/ex04/main.cpp
+++ CPP/CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:45:45 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/01 17:17:39 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2023/11/01 17:18:58 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,7 @@ int	main(int argc, char **argv)
 			std::cout << pos << std::endl;
 			line.erase(pos, toFind.length());
 			line.insert(pos, toReplace);
-			std::cout << toFind[pos + toReplace.length()] << std::endl;
-			pos = line.find(toFind[pos + toReplace.length()]);
+			pos = line.find(toFind, pos + toReplace.length());
 			std::cout << "X:" << pos << std::endl;
 		}
 		out << line << "\n";
