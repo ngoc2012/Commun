877f8aebf4c89c38df8d542f097a707a4ca38534
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-02 11:42:01 +0100

    all

--- CPP/CPP04/ex03/MateriaSource.cpp
+++ CPP/CPP04/ex03/MateriaSource.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/02 11:29:22 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/02 11:41:00 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ void	MateriaSource::learnMateria(AMateria* m)
 	while (++i < SLOTS_MS && materias[i])
 		if (materias[i] == m)
 		{
-			std::cerr << "Error: Materia " << m->getType() << " does existed in this MateriaSource." << std::endl;
+			std::cerr << "ERROR: Materia " << m->getType() << " does existed in this MateriaSource." << std::endl;
 			return ;
 		}
 	if (i == SLOTS_MS)
@@ -62,6 +62,6 @@ AMateria	*MateriaSource::createMateria(std::string const &type)
 	for ( int i = 0; i < SLOTS_MS; i++ )
 		if ( materias[i] && materias[i]->getType() == type )
 			return materias[i]->clone();
-	std::cerr << "Error: Type "<< type << " not found." << std::endl;
+	std::cerr << "ERROR: Type "<< type << " not found." << std::endl;
 	return 0;
 }
