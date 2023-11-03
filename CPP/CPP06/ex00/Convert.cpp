8afbb61b64257175816c578e31d7fb5c2b0c1f80
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-03 13:12:01 +0100

    all

--- CPP/CPP06/ex00/Convert.cpp
+++ CPP/CPP06/ex00/Convert.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/03 12:00:15 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/03 13:11:46 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,9 @@ Convert::~Convert() { std::cout << "Convert destructor." << std::endl; }
 
 void     Convert::getChar( char* n )
 {
-	int	ni;
-
-	ni = static_cast<int>(atof(n));
+	if (atof(str) == std::numeric_limits<double>::quiet_NaN())
+		std::cout << "nan";
+	int	ni = static_cast<int>(atof(n));
 	if (ni >= std::numeric_limits<char>::min()
 		&& ni <= std::numeric_limits<char>::max()
 		&& ni >= 32 && ni <= 126)
@@ -45,6 +45,8 @@ void     Convert::getChar( char* n )
 
 void     Convert::getInt( char* n )
 {
+	if (atof(str) == std::numeric_limits<double>::quiet_NaN())
+		std::cout << "nan";
 	float	c;
 	c = static_cast<float>(atof(n));
 	if (c >= std::numeric_limits<int>::min()
@@ -56,13 +58,23 @@ void     Convert::getInt( char* n )
 
 void     Convert::getFloat( char* n )
 {
-	float	c;
-	c = static_cast<float>(atof(n));
-	std::cout << c;
+	if (atof(str) == std::numeric_limits<double>::quiet_NaN())
+		std::cout << "nanf";
+	double	c;
+	c = static_cast<double>(atof(n));
+	if (c >= std::numeric_limits<float>::min()
+		&& c <= std::numeric_limits<float>::max())
+		std::cout << c;
+	else if (c < std::numeric_limits<float>::min())
+		std::cout << "-inff";
+	else
+		std::cout << "+inff";
 }
 
 void     Convert::getDouble( char* n )
 {
+	if (atof(str) == std::numeric_limits<double>::quiet_NaN())
+		std::cout << "nanf";
 	double	c;
 	c = atof(n);
 	std::cout << c;
