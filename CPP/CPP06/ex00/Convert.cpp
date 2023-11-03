5e7b06aebed681fdcac167454e6b94cd0dde3f3e
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-03 10:26:21 +0100

    all

--- CPP/CPP06/ex00/Convert.cpp
+++ CPP/CPP06/ex00/Convert.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/03 09:13:43 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/03 09:14:19 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,27 +36,27 @@ Convert::~Convert() { std::cout << "Convert destructor." << std::endl; }
 void     Convert::getChar( std::string n )
 {
 	char	c;
-	c = stoi(n);
+	c = std::stoi(n);
 	std::cout << c;
 }
 
 void     Convert::getInt( std::string n )
 {
 	int	c;
-	c = stoi(n);
+	c = std::stoi(n);
 	std::cout << c;
 }
 
 void     Convert::getFloat( std::string n )
 {
 	float	c;
-	c = stof(n);
+	c = std::stof(n);
 	std::cout << c;
 }
 
 void     Convert::getDouble( std::string n )
 {
 	double	c;
-	c = stod(n);
+	c = std::stod(n);
 	std::cout << c;
 }
