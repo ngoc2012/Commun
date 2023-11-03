c667e9d5ba34c7374f9b8f48d123a8d451cc3802
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-03 08:56:01 +0100

    all

--- CPP/CPP06/ex00/main.cpp
+++ CPP/CPP06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/03 08:53:03 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/03 08:54:38 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,10 @@ int main(int argc, char **argv)
 		std::cerr << "Usage: convert [0..1]" << std::endl;
 		return (1);
 	}
-	std::cout << "char  : " << std::endl;
-	std::cout << "int   : " << std::endl;
-	std::cout << "float : " << std::endl;
-	std::cout << "double: " << std::endl;
+	Convert		c;
+	std::cout << "char  : " << c.getChar(argv[1])	<< std::endl;
+	std::cout << "int   : " << c.getInt(argv[1])	<< std::endl;
+	std::cout << "float : " << c.getFloat(argv[1])  << std::endl;
+	std::cout << "double: " << c.getDouble(argv[1]) << std::endl;
 	return (0);
 }
