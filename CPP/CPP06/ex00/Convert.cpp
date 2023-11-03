52b17737752a54025af80583e72ce1892626f873
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-03 11:56:01 +0100

    all

--- CPP/CPP06/ex00/Convert.cpp
+++ CPP/CPP06/ex00/Convert.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/03 11:26:32 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/03 11:55:06 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,14 @@ Convert::~Convert() { std::cout << "Convert destructor." << std::endl; }
 
 void     Convert::getChar( char* n )
 {
-	char	c;
-	c = static_cast<char>(atof(n));
-	if (c == std::numeric_limits<char>::quiet_NaN())
-		std::cout << "nan";
+	//char	c;
+	int	ni;
+
+	ni = static_cast<int>(atof(n));
+	if (ni >= std::numeric_limits<char>::min() && ni <= std::numeric_limits<char>::max())
+		std::cout << static_cast<char>(atof(n));
 	else
-		std::cout << c;
+		std::cout << "nan";
 }
 
 void     Convert::getInt( char* n )
