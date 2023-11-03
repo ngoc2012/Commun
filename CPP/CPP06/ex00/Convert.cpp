789bb57382455f4df8a5d34d0ed9b83ece5f58bf
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-03 11:58:01 +0100

    all

--- CPP/CPP06/ex00/Convert.cpp
+++ CPP/CPP06/ex00/Convert.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:16:32 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/03 11:55:06 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/03 11:56:39 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,9 @@ void     Convert::getChar( char* n )
 	int	ni;
 
 	ni = static_cast<int>(atof(n));
-	if (ni >= std::numeric_limits<char>::min() && ni <= std::numeric_limits<char>::max())
+	if (ni >= std::numeric_limits<char>::min()
+		&& ni <= std::numeric_limits<char>::max()
+		&& ni >= 32 && ni <= 126)
 		std::cout << static_cast<char>(atof(n));
 	else
 		std::cout << "nan";
