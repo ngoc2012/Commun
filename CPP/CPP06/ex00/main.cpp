812c3c95c59de9390c9cf37a9b2b7513dcd4ed74
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-04 14:08:01 +0100

    all

--- CPP/CPP06/ex00/main.cpp
+++ CPP/CPP06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/04 14:01:41 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/04 14:07:30 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,11 @@ int main(int argc, char **argv)
 	}
 
 	Convert		c;
+	double	flt_max = 340282346638528859811704183484516925440;
+	int	i = 0;
+	while (flt_max++ < std::numeric_limits<float>::max())
+		i++;
+	std::cout << i << std::endl;
 	std::cout << "atof: " << atof(argv[1]) << std::endl;
 	std::cout << "char: "; c.getChar(argv[1]);	std::cout << std::endl;
 	std::cout << "int: "; c.getInt(argv[1]);	std::cout << std::endl;
