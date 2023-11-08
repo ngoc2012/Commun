5e154543a9265c8976f1c345bd0c3002718ef771
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-08 15:30:01 +0100

    all

--- Webserv/includes/Server.hpp
+++ Webserv/includes/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/08 15:21:08 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/08 15:29:09 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,13 @@
 #include <stdlib.h>
 #include <cstdio>	// perror
 #include <cstring>	// memcpy
+
 #include "ClientRequest.hpp"
 
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
-# define BUFFER	1024
+# define BUFFER	4
 
 
 //https://www.ibm.com/docs/en/ztpf/2020?topic=overview-blocking-nonblocking
