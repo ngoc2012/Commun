dbfa24abee5c147c2d788434cef65ae9e4f5b873
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-10 09:24:01 +0100

    all

--- Webserv/srcs/Configuration.cpp
+++ Webserv/srcs/Configuration.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/10 09:12:38 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/10 09:23:59 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,3 +31,12 @@ void	Configuration::clean()
 }
 std::string	Configuration::get_http_request(void) {return (_http_request);}
 void	Configuration::set_http_request(std::string s) {_http_request = s;}
+Configuration::Configuration(const char* ip, int port)
+{
+	_ip_address = ip;
+	_port = port;
+	_max_clients = 128;
+}
+
+		char			*get_ip_address(void);
+		short unsigned int	get_port();
