74cf623f1c0816415dcd7d344418e31a84d7d213
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-11 08:12:01 +0100

    all

--- Webserv/srcs/main.cpp
+++ Webserv/srcs/main.cpp
@@ -6,31 +6,37 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 21:21:18 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/11 08:08:55 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/11 08:11:46 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 #include "Configuration.hpp"
 
+Server	*g_server;
+
 void	main_signal_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
-		ft_putchar_fd('\n', STDIN_FILENO);
-		rl_replace_line("", STDIN_FILENO);
-		rl_on_new_line();
-		rl_redisplay();
-		g_exit_code = 130;
+		std::cout << "Server ended" << std::endl;
+
 	}
 }
 
 int	main()
 {
+	struct sigaction	act;
+	act.sa_flags = SA_RESTART;
+	act.sa_handler = main_signal_handler;
+	sigemptyset(&act.sa_mask);
+	sigaction(SIGINT, &act, NULL);
+
 	std::vector<Configuration>	confs;
 	confs.push_back(Configuration("127.0.0.1", 4242));
 	confs.push_back(Configuration("127.0.0.1", 4141));
 	Server server(&confs);
+	g_server = &server;
 	server.start();
 	return (0);
 }
