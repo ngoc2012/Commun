c8d7042d9b4c7d89548570d1105a043092a1201d
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-11 08:06:01 +0100

    all

--- Webserv/includes/Configuration.hpp
+++ Webserv/includes/Configuration.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/10 18:54:01 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/11 08:05:24 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 class Configuration
 {
 	private:
-		const char		*_ip_address;
+		char			*_ip_address;
 		short unsigned int	_port;
 		int			_max_clients;
 
@@ -31,10 +31,10 @@ class Configuration
 		Configuration(const char*, int);
 		virtual ~Configuration();
 
-		char			*get_ip_address(void);
-		short unsigned int	get_port(void);
-		int			get_max_clients(void);
-		int			get_listen_sk(void);
+		char			*get_ip_address(void) const;
+		short unsigned int	get_port(void) const;
+		int			get_max_clients(void) const;
+		int			get_listen_sk(void) const;
 
 		void			set_listen_sk(int);
 };
--- Webserv/srcs/Configuration.cpp
+++ Webserv/srcs/Configuration.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/11 08:03:57 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/11 08:05:37 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ Configuration&	Configuration::operator=( Configuration const & src )
 Configuration::~Configuration() {}
 Configuration::Configuration(const char* ip, int port)
 {
-	_ip_address = ip;
+	_ip_address = (char *) ip;
 	_port = port;
 	_max_clients = 128;
 	_listen_sk = -1;
