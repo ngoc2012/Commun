e8a7a853123f5914fd4e75fedad04d83f8b80fa1
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-12 21:14:01 +0100

    all

--- CPP/CPP06/ex00/Convert.hpp
+++ CPP/CPP06/ex00/Convert.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/04 15:21:10 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/12 21:13:44 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include <sstream>	// istringstream
 #include <cstring>	// strlen
 
-enum	e_types {CHAR, INT, FLOAT, DOUBLE};
+enum	e_types {NONE, CHAR, INT, FLOAT, DOUBLE};
 
 class Convert
 {
