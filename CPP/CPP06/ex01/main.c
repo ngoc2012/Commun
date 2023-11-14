2ee45e95415c0bda46af393274c8e85c42c5261b
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-14 14:54:01 +0100

    all

--- CPP/CPP06/ex01/main.c
+++ CPP/CPP06/ex01/main.c
@@ -1,64 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/10/10 18:15:21 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/14 14:53:17 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Cat.hpp"
-#include "Dog.hpp"
-#define N_ANIMAL 100
+#include <iostream>
 
-int main()
+struct Data
 {
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	delete(i);
-	delete(j);
-	Dog* m = new Dog();
-	m->setBrain("haha", 25);
-	m->setBrain("hehe", 70);
-	m->setBrain("moemoe", -1);
-	m->setBrain("hoaoha", 100);
-	std::cout << "m brain" << std::endl;
-	m->showBrain();
-	Dog k;
-	k = *m;
-	std::cout << "k brain" << std::endl;
-	k.showBrain();
-	Dog h(k);
-	std::cout << "h brain" << std::endl;
-	h.showBrain();
-	delete(m);
-	Cat* m1 = new Cat();
-	m1->setBrain("hoho", 1);
-	m1->setBrain("enen", 99);
-	m1->setBrain("moemoe", -1);
-	m1->setBrain("hoaoha", 100);
-	std::cout << "m1 brain" << std::endl;
-	m1->showBrain();
-	Cat k1;
-	k1 = *m1;
-	std::cout << "k1 brain" << std::endl;
-	k1.showBrain();
-	Cat h1(k1);
-	std::cout << "h1 brain" << std::endl;
-	h1.showBrain();
-	delete(m1);
-	Animal	*zoo[N_ANIMAL];
-	for (int i = 0; i < N_ANIMAL; i++)
-	{
-		if (i % 2)
-			zoo[i] = new Dog();
-		else
-			zoo[i] = new Cat();
-	}
-	for (int i = 0; i < N_ANIMAL; i++)
-		delete(zoo[i]);
-	return (0);
+	std::string	name;
+	float	level;
+};
+
+uintptr_t    serialize( Data *data ) {
+	return ( reinterpret_cast< uintptr_t >( data ) );
+}
+
+Data*   deserialize( uintptr_t data ) {
+	return ( reinterpret_cast< Data* >( data ) );
+}
+
+int main( void )
+{
+	Data *data = new Data;
+
+	data->name = "achraf";
+	data->age = 30;
+
+	std::cout << "Name: " << deserialize( serialize( data ) )->name << std::endl;
+	std::cout << "Age: " << deserialize( serialize( data ) )->age << std::endl;
+
+	delete data;
+
+	return EXIT_SUCCESS;
 }
