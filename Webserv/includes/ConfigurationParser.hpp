2c7d721c8f045cd489b294884c34ef6d5c74e7ee
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-14 17:38:01 +0100

    all

--- Webserv/includes/ConfigurationParser.hpp
+++ Webserv/includes/ConfigurationParser.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/14 17:34:45 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/14 17:36:22 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ class ConfigurationParser
 		const char*	keys_level_1[] = {"listen", "server_name", "location"}
 		const char*	keys_location[] = {"listen", "server_name", "location"}
 
-		ConfigurationParser();
 		ConfigurationParser(const ConfigurationParser&);
 		ConfigurationParser &operator=(const ConfigurationParser& op);
 	public:
-		ConfigurationParser(Server&, const char *);
-		virtual ~ConfigurationParser();
+		ConfigurationParser();
+		std::vector<Server>	get_servers(const char *);
+		virtual			~ConfigurationParser();
 };
 
 #endif
