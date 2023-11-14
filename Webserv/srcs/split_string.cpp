9410425320ed13f05d8e5010cf2b90818015705d
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-14 22:22:01 +0100

    all

--- Webserv/srcs/split_string.cpp
+++ Webserv/srcs/split_string.cpp
@@ -6,18 +6,19 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 22:14:49 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/14 22:19:50 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/14 22:21:49 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
 
-std::vector<std::string>	split_string(const std::string& input, std::string delimiters) {
+std::vector<std::string>	split_string(const std::string& input, std::string& delimiters)
+{
 	std::vector<std::string>	tokens;
 	std::string			token;
 
 	for (std::size_t i = 0; i < input.length(); ++i)
-		if (delimiters.find(input[i]) == std::string::npos)
+		if (delimiters->find(input[i]) == std::string::npos)
 			token += input[i];
 		else
 		{
@@ -28,5 +29,5 @@ std::vector<std::string>	split_string(const std::string& input, std::string deli
 			}
 		}
 	tokens.push_back(token);
-	return tokens;
+	return (tokens);
 }
