07d7f5fe8c3a91e8fedd7c76c2e19dfe9ff6f8a7
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-14 14:56:01 +0100

    all

--- CPP/CPP06/ex01/main.c
+++ CPP/CPP06/ex01/main.c
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/14 14:53:17 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/14 14:54:46 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 
 struct Data
 {
-	std::string	name;
-	float	level;
+	std::string	login;
+	float		level;
 };
 
-uintptr_t    serialize( Data *data ) {
+uintptr_t	serialize( Data *data ) {
 	return ( reinterpret_cast< uintptr_t >( data ) );
 }
 
-Data*   deserialize( uintptr_t data ) {
+Data*	deserialize( uintptr_t data ) {
 	return ( reinterpret_cast< Data* >( data ) );
 }
 
-int main( void )
+int	main()
 {
 	Data *data = new Data;
 
-	data->name = "achraf";
-	data->age = 30;
+	data->login = "minh-ngu";
+	data->level = 4.5;
 
-	std::cout << "Name: " << deserialize( serialize( data ) )->name << std::endl;
-	std::cout << "Age: " << deserialize( serialize( data ) )->age << std::endl;
+	std::cout << "Login: " << deserialize( serialize( data ) )->name << std::endl;
+	std::cout << "Level: " << deserialize( serialize( data ) )->age << std::endl;
 
 	delete data;
 
