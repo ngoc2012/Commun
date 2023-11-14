7ce93914a64a2d6e19fa31444cc1c931422d46af
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-14 17:34:01 +0100

    all

--- Webserv/srcs/ConfigurationParser.cpp
+++ Webserv/srcs/ConfigurationParser.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/14 17:30:25 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/14 17:33:12 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,20 @@ ConfigurationParser&	ConfigurationParser::operator=( ConfigurationParser const &
 	return (*this);
 }
 ConfigurationParser::~ConfigurationParser() {}
-ConfigurationParser::ConfigurationParser(Server& server, const char conf*)
+ConfigurationParser::get_servers(Server& server, const char conf*)
 {
 	const char*	keys_level_0[] = {"server"}
 	const char*	keys_level_1[] = {"listen", "server_name", "location"}
 	const char*	keys_location[] = {"listen", "server_name", "location"}
+
+	std::ifstream conf_file(conf);
+	if (!conf_file.is_open()) {
+		std::cerr << "Error opening the file." << std::endl;
+		return 1;
+	}
+	std::string line;
+	while (std::getline(conf_file, line)) {
+		std::cout << "Read line: " << line << std::endl;
+	}
+	inputFile.close();
 }
