70c995e74894afca315fb5fdb27eafd1aeb60b29
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-14 17:10:01 +0100

    all

--- Webserv/includes/ConfigurationParser.hpp
+++ Webserv/includes/ConfigurationParser.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/14 17:06:56 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/14 17:09:57 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,11 @@ class ConfigurationParser
 {
 	private:
 		ConfigurationParser();
-	public:
 		ConfigurationParser(const ConfigurationParser&);
 		ConfigurationParser &operator=(const ConfigurationParser& op);
-		ConfigurationParser(const char*, int);
+	public:
+		ConfigurationParser(Server&, const char *);
 		virtual ~ConfigurationParser();
-
-		char			*get_ip_address(void) const;
-		short unsigned int	get_port(void) const;
-		int			get_max_clients(void) const;
-		int			get_listen_sk(void) const;
-
-		void			set_listen_sk(int);
 };
 
 #endif
