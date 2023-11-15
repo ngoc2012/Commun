dd34011d46d6fd9766e64bd69de273326ab22267
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-15 19:12:01 +0100

    all

--- Webserv/srcs/Host.cpp
+++ Webserv/srcs/Host.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/15 19:07:48 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/15 19:11:36 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,23 +97,23 @@ void		close_all_listen_sk(std::vector<Server> &confs)
 			close((*it).get_listen_sk());
 }
 
-void	Host::get_listen_sk(Server &c)
+void	Host::get_listen_sk(Server* c)
 {
-	c.set_listen_sk(socket(AF_INET, SOCK_STREAM, 0));
-	if (c.get_listen_sk() < 0)
+	c->set_listen_sk(socket(AF_INET, SOCK_STREAM, 0));
+	if (c->get_listen_sk() < 0)
 	{
 		perror("listen socket: socket() failed");
 		exit(-1);
 	}
 	int    on = 1;
-	if (setsockopt(c.get_listen_sk(), SOL_SOCKET,  SO_REUSEADDR,
+	if (setsockopt(c->get_listen_sk(), SOL_SOCKET,  SO_REUSEADDR,
                    (char *)&on, sizeof(on)) < 0)
 	{
 		perror("reusable socket: setsockopt() failed");
 		close_all_listen_sk(*_servers);
 		exit(-1);
 	}
-	fcntl(c.get_listen_sk(), F_SETFL, O_NONBLOCK);	// ioctl not allowed
+	fcntl(c->get_listen_sk(), F_SETFL, O_NONBLOCK);	// ioctl not allowed
 }
 
 void	Host::bind_addr(Server &c)
