0aaf46047e233bfd2218a9b93d8b05cce3b6fbe4
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-16 19:16:01 +0100

    all

--- Webserv/srcs/ClientRequest.cpp
+++ Webserv/srcs/ClientRequest.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/16 19:10:16 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/16 19:15:58 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,50 +77,6 @@ void	Host::read_client_request(void)
 		else
 		{
 			response[ret] = 0;
-			s += std::string(response);
-			if (method == "")
-			{
-				pos = s.find("Content-Type:");
-				if ( pos != std::string::npos)
-				{
-					pos0 = s.find(";", pos);
-					if ( pos0 != std::string::npos)
-					{
-						_request.set_method(s.substr(pos + 14, pos0 - pos - 14));
-						std::cout << "|" << _request.get_method() << "|" << std::endl;
-					}
-				}
-			}
-			if (method == "multipart/form-data")
-			{
-				// The combination \r\n is often used to indicate the end of a line of text in text files
-				// Find the start of the file content
-				const std::string boundary = "boundary=";
-				size_t boundaryPos = s.find(boundary);
-				//if (boundaryPos == std::string::npos) {
-				//	std::cerr << "Boundary not found in Content-Type header." << std::endl;
-				//	return;
-				//}
-
-				size_t startPos = s.find("\r\n\r\n", boundaryPos) + 4;
-				std::string	s0 = s.substr(startPos);
-				pos = s0.find("Content-Type:");
-				if ( pos != std::string::npos)
-				{
-					startPos = s0.find("\r\n\r\n", pos) + 4;
-					s0 = s0.substr(startPos);
-				}
-				//std::cout << s0 << std::endl;
-				/*
-				size_t endPos = s.find("--" + line.substr(boundaryPos + boundary.length()));
-
-				// Extract the file content
-				std::string fileData = fileContent.substr(startPos, endPos - startPos);
-
-				// Save the file on the server
-				saveFile("uploaded_file.txt", fileData);
-				*/
-			}
 			_request.set_http_request(s);
 		}
 	}
@@ -133,3 +89,46 @@ void	ClientRequest::cat_http_request(std::string s) {_http_request += s;}
 
 std::string	ClientRequest::get_method(void) const {return (_method);}
 std::string	ClientRequest::get_http_request(void) const {return (_http_request);}
+
+/*
+s += std::string(response);
+if (method == "")
+{
+	pos = s.find("Content-Type:");
+	if ( pos != std::string::npos)
+	{
+		pos0 = s.find(";", pos);
+		if ( pos0 != std::string::npos)
+		{
+			_request.set_method(s.substr(pos + 14, pos0 - pos - 14));
+			std::cout << "|" << _request.get_method() << "|" << std::endl;
+		}
+	}
+}
+if (method == "multipart/form-data")
+{
+	// The combination \r\n is often used to indicate the end of a line of text in text files
+	// Find the start of the file content
+	const std::string boundary = "boundary=";
+	size_t boundaryPos = s.find(boundary);
+	//if (boundaryPos == std::string::npos) {
+	//	std::cerr << "Boundary not found in Content-Type header." << std::endl;
+	//	return;
+	//}
+
+	size_t startPos = s.find("\r\n\r\n", boundaryPos) + 4;
+	std::string	s0 = s.substr(startPos);
+	pos = s0.find("Content-Type:");
+	if ( pos != std::string::npos)
+	{
+		startPos = s0.find("\r\n\r\n", pos) + 4;
+		s0 = s0.substr(startPos);
+	}
+	////std::cout << s0 << std::endl;
+	//size_t endPos = s.find("--" + line.substr(boundaryPos + boundary.length()));
+	//// Extract the file content
+	//std::string fileData = fileContent.substr(startPos, endPos - startPos);
+	//// Save the file on the server
+	//saveFile("uploaded_file.txt", fileData);
+}
+*/
