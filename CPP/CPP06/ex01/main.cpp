d0485269f9721621e3a5f94ddb1f809142345ed2
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-21 18:10:01 +0100

    all

--- CPP/CPP06/ex01/main.cpp
+++ CPP/CPP06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/21 18:07:57 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/21 18:09:33 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 
 int	main()
 {
-	Data *data = new Data;
-	data->login = "minh-ngu";
-	data->level = 4.5;
+	Data data;
+	data.login = "minh-ngu";
+	data.level = 4.5;
 	
-	Serializer seri();
+	Serializer seri;
 
-	std::cout << "Login: " << seri.deserialize( seri.serialize( data ) )->login << std::endl;
-	std::cout << "Level: " << seri.deserialize( seri.serialize( data ) )->level << std::endl;
-
-	delete(data);
+	std::cout << "Login: " << seri.deserialize( seri.serialize( &data ) )->login << std::endl;
+	std::cout << "Level: " << seri.deserialize( seri.serialize( &data ) )->level << std::endl;
 
 	return (0);
 }
