7f2e9c8cf28d6dc782f9baa5a40ccbba7b0911b2
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-21 19:14:01 +0100

    all

--- CPP/CPP06/ex02/Base.hpp
+++ CPP/CPP06/ex02/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/21 19:09:46 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/21 19:12:37 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ class Base
 {
 	private:
 		Base(const Base&);
-		Base &operator=(const Base& op);
+		Base	&operator=(const Base& op);
 	public:
 		Base();
 		virtual ~Base();
 
-		uintptr_t	serialize( Data *data );
-		Data*		deserialize( uintptr_t data );
+		Base*	generate(void);
+		void	identify(Base* b);
 };
 
 #endif
