ae41656486655b4d7df734bc62833dba9af831b4
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-23 10:30:01 +0100

    all

--- Webserv/srcs/Response.cpp
+++ Webserv/srcs/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/23 10:27:29 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/23 10:28:51 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 Response::Response()
 {
 	std::cout << "Response Default constructor" << std::endl;
-	clean();
 }
 Response::Response(const Response& src) { *this = src; }
 Response&	Response::operator=( Response const & src )
@@ -31,10 +30,5 @@ Response::Response(int sk, Host* h, Server* s) : _socket(sk), _host(h), _server(
 }
 Response::~Response()
 {
-	std::cout << "Destruction client request" << std::endl;
-	if (_socket > 0)
-	{
-		std::cout << "Close socket: " << _socket << std::endl;
-		close(_socket);
-	}
+	std::cout << "Destruction response" << std::endl;
 }
