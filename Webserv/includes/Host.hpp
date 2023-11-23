2639dd954b91e0c17790681bd156daff5848b90a
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-23 17:56:01 +0100

    all

--- Webserv/includes/Host.hpp
+++ Webserv/includes/Host.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/23 16:01:13 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/23 17:55:22 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,8 @@ class	Host
 
 		int				_sk_ready;
 		int				_max_sk;		// Max of all fd
-		fd_set              		_master_set;		// Set of all read fd
+		fd_set              		_master_read_set;	// Set of all read fd
+		fd_set              		_master_write_set;	// Set of all read fd
 		fd_set              		_read_set;		// Set of active read fd
 		fd_set              		_write_set;		// Set of active write fd
 		fd_set              		_server_set;
@@ -44,7 +45,7 @@ class	Host
 		std::map<int, ClientRequest*>	_sk_client_request;
 
 		bool				select_available_sk(void);
-		void  				add_sk_2_master_set(int, Server*);
+		void  				add_sk_2_master_read_set(int, Server*);
 
 		Host();
 		Host(const Host&);
--- Webserv/srcs/Host.cpp
+++ Webserv/srcs/Host.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/23 16:43:03 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/23 17:54:52 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,17 +49,17 @@ Host::~Host()
 	std::cout << "End server" << std::endl;
 }
 
-void  	Host::add_sk_2_master_set(int new_sk, Server* s)
+void  	Host::add_sk_2_master_read_set(int new_sk, Server* s)
 {
 	if (new_sk > _max_sk)
 		_max_sk = new_sk;
-	FD_SET(new_sk, &_master_set);
+	FD_SET(new_sk, &_master_read_set);
 	_sk_server[new_sk] = s;
 }
 
 void	Host::new_client_request_sk(int new_sk, Server* s)
 {
-	add_sk_2_master_set(new_sk, s);
+	add_sk_2_master_read_set(new_sk, s);
 	_sk_client_request[new_sk] = new ClientRequest(new_sk, this, s);
 }
 
@@ -83,7 +83,7 @@ void	Host::start(void)
 {
 	if (_parser_error || !check_servers_conf())
 		return ;
-	FD_ZERO(&_master_set);
+	FD_ZERO(&_master_read_set);
 	FD_ZERO(&_server_set);
 	int	listen_sk;
 	for (std::vector<Server*>::iterator it = _servers.begin(); it != _servers.end();)
@@ -92,7 +92,7 @@ void	Host::start(void)
 		listen_sk = (*it)->server_socket();
 		if (listen_sk > 0)
 		{
-			add_sk_2_master_set(listen_sk, *it);
+			add_sk_2_master_read_set(listen_sk, *it);
 			FD_SET(listen_sk, &_server_set);
 			++it;
 		}
@@ -106,8 +106,8 @@ void	Host::start(void)
 		return ;
 	do
 	{
-		memcpy(&_read_set, &_master_set, sizeof(_master_set));
-		memcpy(&_write_set, &_master_set, sizeof(_master_set));
+		memcpy(&_read_set, &_master_read_set, sizeof(_master_set));
+		memcpy(&_write_set, &_master_read_set, sizeof(_master_set));
 		for (std::map<int, Server*>::iterator it = _sk_server.begin(); it != _sk_server.end();++it)
 			FD_CLR(it->first, &_write_set);
 		if (select_available_sk() == false)
@@ -159,12 +159,12 @@ void	Host::delete_response(int i)
 
 void	Host::close_client_sk(int i)
 {
-	FD_CLR(i, &_master_set);
+	FD_CLR(i, &_master_read_set);
 	delete (_sk_client_request[i]);
 	_sk_client_request.erase(i);
 	// If i is max_sk -> find another max_sk
 	if (i == _max_sk)
-		while (!FD_ISSET(_max_sk, &_master_set))
+		while (!FD_ISSET(_max_sk, &_master_read_set))
 			_max_sk -= 1;
 }
 
