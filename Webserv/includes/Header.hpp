d83c142039880c2888823f0e85d5ea991e45c174
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-24 14:20:01 +0100

    all

--- Webserv/includes/Header.hpp
+++ Webserv/includes/Header.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/24 13:18:20 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/24 14:19:59 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,9 @@ class	Response;
 class	Header
 {
 	private:
-		int		_err_code;
-		Response*	_response;
+		int				_err_code;
+		Response*			_response;
+		std::map<int, std::string>	_status;
 
 		Header();
 		Header(const Header&);
