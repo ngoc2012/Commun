677e45ed9640b6db6c12fd5d41dd33b576c3ba4f
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-25 22:32:01 +0100

    all

--- Webserv/srcs/Response.cpp
+++ Webserv/srcs/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/25 22:13:08 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/25 22:31:41 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -208,7 +208,9 @@ size_t		Response::get_file_size(std::ring &file_name)
 
 void	Response::get(void)
 { 
-	char buffer[chunk_size];
+	size_t	chunk_size = _host->get_client_body_buffer_size();
+
+	char	buffer[chunk_size];
 	file.read(buffer, chunk_size);
 	int bytes_read = file.gcount();
 	send(client_socket, buffer, bytes_read, 0);
