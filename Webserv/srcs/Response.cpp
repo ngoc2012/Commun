dd755e4a8989d5ae0a895a60d3236c9dcba784fe
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-25 12:04:01 +0100

    all

--- Webserv/srcs/Response.cpp
+++ Webserv/srcs/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/25 12:01:59 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/11/25 12:03:12 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ bool	Response::compare_url(std::string url, std::string l_url)
 	// Folder
 	if (l_url[l_url.size() - 1] == '/')
 	{
-		l_url = l_url.substr(0, l_url.size() - 1);
+		std::string l_url = l_url.substr(0, l_url.size() - 1);
 	}
 	// File
 	if (url.size() > 1 && url.size() < l_url.size() && url.substr(0, l_url.size()) == l_url)
