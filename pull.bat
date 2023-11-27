84b3bb7434f0537cbfbe196a8b4c2495f8400bc4
NGUYEN Minh Ngoc <ngoc2012@yahoo.com>
2023-11-27 09:25:35 +0100

    all

---
+++ pull.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C://Users/Nguyen Minh Ngoc/Commun
+git pull
---
+++ push.bat
@@ -0,0 +1,5 @@
+@echo off
+cd C://Users/Nguyen Minh Ngoc/Commun
+git add -A -- :!*.o :!*.swp
+git commit -m "all"
+git push
