ad6543951d99d586d917d5480e956747decc5b64
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-28 18:10:01 +0100

    all

--- Transcendence/backend/views.py
+++ Transcendence/backend/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+#from django.shortcuts import render
+#from django.http import JsonResponse
 
 #def pong_game(request):
 #	# Handle game logic and return the current game state
