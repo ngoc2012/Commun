f7a5d7120764412f1e8fda010b032002d8e42680
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-28 10:32:01 +0100

    all

--- Transcendence/backend/urls.py
+++ Transcendence/backend/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import GameListCreateView, PlayerListCreateView
+from .views import get_games_list, get_players_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('games/', get_games_list, name='games'),
-    path('players/', get_games_list, name='players'),
+    path('players/', get_players_list, name='players'),
 ]
