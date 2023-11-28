305527ff73fe4e2f4b1940b6ce7aeb3a14a6ef0c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-28 22:26:01 +0100

    all

--- Transcendence/backend/settings.py
+++ Transcendence/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/home/ngoc/Commun/Transcendence/frontend'],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
         'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
