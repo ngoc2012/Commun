b41764d00d254f735038e68761e560a307dfd246
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-28 10:36:01 +0100

    all

--- Transcendence/backend/views.py
+++ Transcendence/backend/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-global games_list = {1, 2, 3, 4}
-global players_list = {"haha", "hehe"}
+games_list = {1, 2, 3, 4}
+players_list = {"haha", "hehe"}
 
 def pong_game(request):
 	# Handle game logic and return the current game state
@@ -10,7 +10,9 @@ def pong_game(request):
 	return JsonResponse(game_state)
 
 def get_games_list(request):
+	global games_list
 	return JsonResponse(games_list);
 
 def get_players_list(request):
+	global players_list
 	return JsonResponse(players_list);
