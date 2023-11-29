08a95a7ec481b1258f4cfefa813b2635a55c283e
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-29 08:56:01 +0100

    all

--- Transcendence/backend/urls.py
+++ Transcendence/backend/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api/', include('api.urls')),
+    path('api/', include('api')),
     path('', views.home, name='home')
 ]
