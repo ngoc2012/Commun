1b7f39ab3e3b16c0194d182c3ad45d289d5b4efe
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-30 15:54:01 +0100

    all

--- Transcendence/backend/views.py
+++ Transcendence/backend/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import redirect
-import datetime
+import time
 
 #def pong_game(request):
 ## Handle game logic and return the current game state
@@ -30,10 +30,11 @@ def new_player(request):
     while (user_name in players.keys()):
         i += 1
         user_name = "user" + str(i)
-    players[user_name] = {"heart_beat": datetime.datetime.now()};
+    players[user_name] = {"heart_beat": time.time()};
     return (JsonResponse({"user": user_name}));
 
 @csrf_exempt
 def players_list(request):
-    players[request.POST['user']]["heart_beat"] = datetime.datetime.now();
+    
+    players[request.POST['user']]["heart_beat"] = time.time();
     return (JsonResponse({"players": [i['user'] for i in players]}));
