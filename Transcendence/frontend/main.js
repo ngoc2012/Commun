33f80a133d39730f33ed80d21acf229638dd6ae4
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-11-30 15:12:01 +0100

    all

--- Transcendence/frontend/main.js
+++ Transcendence/frontend/main.js
@@ -3,7 +3,7 @@ class Game
     user_name = "";
 }
 
-var game = new Game();
+export var game = new Game();
 document.addEventListener('DOMContentLoaded', function (event) {
     update();
     new_player();
@@ -31,36 +31,6 @@ btn_invite.addEventListener("click", function () {
     }
 });
 
-// Handle keyboard input
-document.addEventListener('keydown', function (event) {
-    switch (event.key) {
-        case 'ArrowUp':
-            game.rightPaddle.dy = -5;
-            break;
-        case 'ArrowDown':
-            game.rightPaddle.dy = 5;
-            break;
-        case 'w':
-            game.leftPaddle.dy = -5;
-            break;
-        case 's':
-            game.leftPaddle.dy = 5;
-            break;
-    }
-});
-
-document.addEventListener('keyup', function (event) {
-    switch (event.key) {
-        case 'ArrowUp':
-        case 'ArrowDown':
-            game.rightPaddle.dy = 0;
-            break;
-        case 'w':
-        case 's':
-            game.leftPaddle.dy = 0;
-            break;
-    }
-});
 /*
 function getCookie(name) {
     var cookieValue = null;
@@ -99,37 +69,6 @@ function new_player() {
 }
 
 // AJAX Polling
-function pollGameState() {
-    // Make an AJAX request to get the current game state
-    $.ajax({
-        url: '/api/state/',
-        method: 'GET',
-        success: function(data) {
-            // Update game elements based on data received from the server
-            updateGame(data);
-        },
-        error: function(error) {
-            console.error('Error fetching game state:', error);
-        }
-    });
-}
-
-// Send player actions to the server
-function sendAction(action) {
-    // Make an AJAX request to send player action to the server
-    $.ajax({
-        url: '/api/action/',
-        method: 'POST',
-        data: { action: action },
-        success: function(response) {
-            // Handle server response if needed
-        },
-        error: function(error) {
-            console.error('Error sending player action:', error);
-        }
-    });
-}
-
 function update_players_list(action) {
     // Make an AJAX request to send player action to the server
     $.ajax({
--- Transcendence/frontend/pong_app.js
+++ Transcendence/frontend/pong_app.js
@@ -1,4 +1,5 @@
 import {Pong} from './Pong.js'
+import {game} from './main.js'
 
 var pong = new Pong();
 
@@ -32,3 +33,35 @@ document.addEventListener('keyup', function (event) {
             break;
     }
 });
+
+function pollGameState() {
+    // Make an AJAX request to get the current game state
+    $.ajax({
+        url: '/api/state/',
+        method: 'GET',
+        success: function(data) {
+            // Update game elements based on data received from the server
+            updateGame(data);
+        },
+        error: function(error) {
+            console.error('Error fetching game state:', error);
+        }
+    });
+}
+
+// Send player actions to the server
+function sendAction(action) {
+    // Make an AJAX request to send player action to the server
+    $.ajax({
+        url: '/api/action/',
+        method: 'POST',
+        data: { action: action },
+        success: function(response) {
+            // Handle server response if needed
+        },
+        error: function(error) {
+            console.error('Error sending player action:', error);
+        }
+    });
+}
+
