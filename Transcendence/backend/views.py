05e92c610da1ac8ea297287c1cb65b66bcc2d972
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-01 19:40:01 +0100

    all

--- Transcendence/backend/views.py
+++ Transcendence/backend/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import redirect
 import time
 
-players = {}
+online_players = {}
 games = {}
 game_id = 0
 
@@ -25,10 +25,10 @@ def pong(request):
 def new_player(request):
     i = 0
     user_name = "user" + str(i)
-    while (user_name in players.keys()):
+    while (user_name in online_players.keys()):
         i += 1
         user_name = "user" + str(i)
-    players[user_name] = {
+    online_players[user_name] = {
             "online": time.time(),
             "invitations": [];
             "accepted": -1;
@@ -36,36 +36,36 @@ def new_player(request):
     return (JsonResponse({"user": user_name}))
 
 def check_all_users_online():
-    users = list(players.keys())
+    users = list(online_players.keys())
     for user in users:
-        if time.time() - players[user]["online"] > 3:
-            players.pop(user)
+        if time.time() - online_players[user]["online"] > 3:
+            online_players.pop(user)
 
 @csrf_exempt
-def players_list(request): 
+def online_players_list(request): 
     #print(games)
     check_all_users_online()
     user = request.POST['user']
-    users = list(players.keys())
+    users = list(online_players.keys())
     invitations = []
     if user in users:
-        players[user]["online"] = time.time()
-    return (JsonResponse({"user_status": players[user], "players_list": users}))
+        online_players[user]["online"] = time.time()
+    return (JsonResponse({"user_status": online_players[user], "online_players_list": users}))
 
 def invite(request):
     for user in request.POST['players']
-        if (user not in players.keys()):
+        if (user not in online_players.keys()):
             return HttpResponseNotFound("Player " + user + " was not found in the game.")
 
     game_id += 1
     for user in request.POST['players']
-        players[user]['invitations'].append(game_id)
+        online_players[user]['invitations'].append(game_id)
 
     games[game_id] = {
         "id": game_id,
         "game": request.POST['game'],
         "status": "waiting",
-        "players": request.POST['players'],
+        "online_players": request.POST['players'],
         "host": request.POST['host'],
         "accepted": [request.POST['host']]
         }
@@ -77,9 +77,9 @@ def accept_invitation(request):
     if (g_id not in games.keys())
         return HttpResponseNotFound("Game " + str(g_id) + " was not found.")
     user = request.POST['user']
-    if (user not in games[g_id]["players"])
+    if (user not in games[g_id]["online_players"])
         return HttpResponseNotFound("Player " + user + " was not found in the game.")
-    if (players[user]['accepted'] != 1)
+    if (online_players[user]['accepted'] != 1)
         return HttpResponseNotFound("Player " + user + " was in another game.")
     if (user not in games[g_id]["accepted"])
         games[g_id]["accepted"].append(user)
@@ -90,7 +90,7 @@ def cancel_invitation(request):
     if (g_id not in games.keys())
         return HttpResponseNotFound("Game " + str(g_id) + " was not found.")
     user = request.POST['user']
-    if (user not in games[g_id]["players"])
+    if (user not in games[g_id]["online_players"])
         return HttpResponseNotFound("Player " + user + " was not found in the game.")
     games.pop(g_id)
     return (HttpResponse("canceled"))
