b026e2e8cf3da51b76aa425f85ec1d4573f15fec
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-02 23:12:01 +0100

    all

--- Transcendence/frontend/main.js
+++ Transcendence/frontend/main.js
@@ -6,7 +6,9 @@ var main = new Main();
 document.addEventListener('DOMContentLoaded', function (event) {
     main.new_player();
     var game = new Game(main);
-    //main.load('/home', () => {game.events()});
+    main.load('/home', () => {
+        console.log('load callback invoked');
+        game.events();
+    });
     //main.load('/home', game.events.bind(game));
-    main.load('/home', game.events);
 });
