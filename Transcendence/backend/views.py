1d1ac1bae8dbb29cc4d9750cd8729a0885fb5cd5
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-02 01:04:01 +0100

    all

--- Transcendence/backend/views.py
+++ Transcendence/backend/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponse
 from django.http import HttpResponseNotFound
-from django.views.decorators.csrf import csrf_exempt
+#from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import redirect
 import time
 
