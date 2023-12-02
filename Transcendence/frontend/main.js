d4c82a3376ff5bd17e326b4c112ea53e3352b20e
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-02 13:34:01 +0100

    all

--- Transcendence/frontend/main.js
+++ Transcendence/frontend/main.js
@@ -1,6 +1,6 @@
-import {Game} from './Game.js'
+import {Main} from './Main.js'
 
-var game = new Game();
+var main = new Main();
 
 document.addEventListener('DOMContentLoaded', function (event) {
     //game.dom_online_players_list = dom_online_players_list;
@@ -10,46 +10,3 @@ document.addEventListener('DOMContentLoaded', function (event) {
     game.update();
 });
 
-class Project
-{
-    user = "";
-    name = "";
-    id = -1;
-    status = "";
-    dom_status = document.getElementById("status");
-    dom_user_name = document.getElementById("user_name");
-    dom_game = document.getElementById("game");
-    update_time_interval = 2000;
-
-    new_player() {
-        $.ajax({
-            url: '/new_player',
-            method: 'GET',
-            //headers: {'X-CSRFToken': csrftoken},
-            success: (response) => {
-                this.user = response.user;
-                this.dom_user_name = this.user;
-            },
-            error: function(error) {
-                console.error('Error: sending new player demand', error.message);
-            }
-        });
-    }
-
-
-   load_page(page) {
-        $.ajax({
-            url: '/' + page + '/',
-            method: 'GET',
-            success: (html) => {
-                window.history.pushState({
-                    "user": this.user
-                }, page, '/' + page);
-                document.body.innerHTML = html;
-            },
-            error: function(error) {
-                console.error('Error: pong GET fail', error.message);
-            }
-        });
-    }
-}
