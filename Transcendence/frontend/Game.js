a7dae5d5a6efd6ae260aa195856f2e301bceb11f
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-03 08:16:01 +0100

    all

--- Transcendence/frontend/Game.js
+++ Transcendence/frontend/Game.js
@@ -183,7 +183,7 @@ export class Game
                     this.main.status = "";
                 }
                 else if (response.status === "playing") {
-                    this.main.load('/pong', () => game.events());
+                    this.main.load('/pong', () => this.main.pong.start());
                     //this.main.dom_status.textContent = "Game " + this.main.name +" " + this.main.id + " is ready";
                 }
             },
