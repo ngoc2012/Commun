3a35ce88d108ac5e55478fa8a423041a849f9e8d
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-06 21:28:01 +0100

    all

--- Webserv/srcs/Request.cpp
+++ Webserv/srcs/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/06 21:25:42 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/06 21:27:45 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -139,9 +139,9 @@ bool	Request::read_method(std::string& s)
 bool	Request::split_header_body(std::string& s)
 {
 	size_t	start = 0;
-	size_t  pos0 = s.find("\r\n\r\n");
-	_body = _header.substr(start, _header.length() - start);
-	_header = _header.substr(0, start);
+	size_t  pos = s.find("\r\n\r\n");
+	_body = _header.substr(pos + 2, _header.size() - pos - 2);
+	_header = _header.substr(0, pos);
 	return (true);
 }
 
