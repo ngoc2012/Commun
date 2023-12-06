0a88c9e010ed4383723f4e4d3681d595b569982a
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-06 15:08:01 +0100

    all

--- Webserv/includes/Cgi.cpp
+++ Webserv/includes/Cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/06 15:05:55 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/06 15:07:31 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 
 #ifndef CGI_HPP
 # define CGI_HPP
- 
+/* 
 class	Server;
 class	Location;
 class	Request;
 class	Configuration;
-
+*/
 class	Cgi
 {
 	private:
+        std::string _pass;
+        std::string _file;
 		Response*	_response;
 
+		Cgi();
 		Cgi(const Cgi&);
 		Cgi	&operator=(const Cgi& op);
 	public:
-		Cgi();
+		Cgi(std::string, std::string, Response*);
 		virtual ~Cgi();
 };
 
