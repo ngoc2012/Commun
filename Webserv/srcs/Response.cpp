cbcb14c37bd98d82278e81e4749b346644248656
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-07 21:52:01 +0100

    all

--- Webserv/srcs/Response.cpp
+++ Webserv/srcs/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/07 21:47:04 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/07 21:51:06 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,6 +117,19 @@ void	Response::send(void)
         _header = "";
     }
 	else if (_request->get_method() == GET)
+        switch (_request->get_method())
+        {
+            case GET:
+                get();
+                break;
+            case PUT:
+                download();
+                break;
+            default:
+                _body = "default";
+                _content_length = _body.length();
+                break;
+        }
 		get();
     if (_end)
         _host->close_client_sk(_socket);
