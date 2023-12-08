4fe33e3c7d50cfedcd1fce3cd980717a01343ba2
ngoc2012 <ngoc2012@yahoo.com>
2023-12-08 09:36:01 +0100

    all

--- Transcendence/backend/asgi.py
+++ Transcendence/backend/asgi.py
@@ -10,7 +10,20 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_asgi_application()
+#application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            [
+                re_path(r"ws/pong/(?P<room_name>\w+)/$", PongConsumer.as_asgi()),
+            ]
+        )
+    ),
+})
