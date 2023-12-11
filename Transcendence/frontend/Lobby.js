75f10f6b6d6afcd41b01c04283ed2f5778231e00
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-11 15:34:01 +0100

    all

--- Transcendence/frontend/Lobby.js
+++ Transcendence/frontend/Lobby.js
@@ -33,54 +33,58 @@ export class Lobby
         });
 
         this.dom_join.addEventListener("click", () => {
-            this.main.game = null;
-            this.main.game_socket = -1;
-            if (this.dom_rooms.selectedIndex !== -1) {
-                new_connection({
-                    socket: this.main.game_socket,
-                    name: "join",
-                    link: 'ws://127.0.0.1:8000/ws/join' + \
-                        '?user=' + this.main.id + \
-                        '&id=' this.dom_rooms.options[this.dom_rooms.selectedIndex].value,
-                    callback: {}
-                });
-            }
-
+            this.join();
         });
+        this.rooms_update();
+    }
+
+    end_game() {
+        this.main.game = null;
+        if (this.main.game_socket = -1)
+        {
+            this.main.game_socket.close;
+        }
+        this.main.game_socket = -1;
+    }
+
+    join() {
+        this.main.game = null;
+        this.main.game_socket = -1;
+        if (this.dom_rooms.selectedIndex !== -1) {
+            new_connection({
+                socket: this.main.game_socket,
+                name: "join",
+                link: 'ws://127.0.0.1:8000/ws/join' + \
+                '?user=' + this.main.id + \
+                '&id=' this.dom_rooms.options[this.dom_rooms.selectedIndex].value,
+                callback: {}
+            });
+        }
+    }
+
+    rooms_update() {
         new_connection({
             socket: this.main.rooms_socket,
             name: "join",
             link: 'ws://127.0.0.1:8000/ws/rooms',
             '?user=' + this.main.id + \
             '&id=' this.dom_rooms.options[this.dom_rooms.selectedIndex].value,
-            callback: {}
-        });
-        this.rooms_update();
-    }
-
-    rooms_update(game) {
-        this.main.rooms_socket = new WebSocket();
-        // Event handler for when the connection is established
-        //socket.addEventListener('open', (event) => {
-        //    socket.send(JSON.stringify({ message: 'Hello, server!' }));
-        //});
-
-        // Event handler for incoming messages from the server
-        socket.addEventListener('message', (event) => {
-            const data = JSON.parse(event.data);
-            //console.log('Received from server:', data.message);
-            var options_rooms = this.dom_rooms && this.dom_rooms.options;
-            this.dom_rooms.innerHTML = "";
-            if (options_rooms && data.rooms && data.rooms.length > 0) {
-                data.rooms.forEach((room) => {
-                    var option = document.createElement("option");
-                    option.value = room.id;
-                    option.text = "" + room.id;
-                    room.players.forEach((p) => {
-                        option.text += " - " + p;
-                    });
-                    this.dom_rooms.add(option);
-                });
+            callback: {
+                message: (data) => {
+                    var options_rooms = this.dom_rooms && this.dom_rooms.options;
+                    this.dom_rooms.innerHTML = "";
+                    if (options_rooms && data.rooms && data.rooms.length > 0) {
+                        data.rooms.forEach((room) => {
+                            var option = document.createElement("option");
+                            option.value = room.id;
+                            option.text = "" + room.id;
+                            room.players.forEach((p) => {
+                                option.text += " - " + p;
+                            });
+                            this.dom_rooms.add(option);
+                        });
+                    }
+                }
             }
         });
     }
