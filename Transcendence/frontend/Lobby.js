4c5b7ba4fb99f68a7f17598c42696bd6920c9e66
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-12 17:56:01 +0100

    all

--- Transcendence/frontend/Lobby.js
+++ Transcendence/frontend/Lobby.js
@@ -1,4 +1,5 @@
 import {Pong} from './Pong.js'
+import {new_connection} from './new_connection.js'
 
 export class Lobby
 {
@@ -64,8 +65,6 @@ export class Lobby
             name: "rooms update",
             socket: this.socket,
             link: 'ws://127.0.0.1:8000/ws/rooms',
-            '?user=' + this.main.id + \
-            '&id=' this.dom_rooms.options[this.dom_rooms.selectedIndex].value,
             callback: {
                 message: (data) => {
                     var options_rooms = this.dom_rooms && this.dom_rooms.options;
@@ -86,55 +85,4 @@ export class Lobby
             }
         });
     }
-
-    new_connection(param)
-    {
-        const timeout = setTimeout(() => {
-            param.socket.close();
-            console.error('WebSocket ' + param.name + ' connection could not be established within the timeout.');
-        }, CONNECTION_TIMEOUT);
-
-        param.socket = new WebSocket(param.link);
-        /*
-        chatSocket.onmessage = function(e) {
-            const data = JSON.parse(e.data);
-            document.querySelector('#chat-log').value += (data.message + '\n');
-        };
-
-        chatSocket.onclose = function(e) {
-            console.error('Chat socket closed unexpectedly');
-        };
-
-        document.querySelector('#chat-message-input').focus();
-        document.querySelector('#chat-message-input').onkeyup = function(e) {
-            if (e.key === 'Enter') {  // enter, return
-                document.querySelector('#chat-message-submit').click();
-            }
-        };
-         * */
-        param.socket.addEventListener('open', (event) => {
-            //socket.send(JSON.stringify({ message: 'Hello, server!' }));
-            const data = JSON.parse(event.data);
-            if ("open" in param.callback)
-                param.callback.open(data);
-        });
-        param.socket.addEventListener('message', (event) => {
-            const data = JSON.parse(event.data);
-            if ("message" in param.callback)
-                param.callback.message(data);
-        });
-        param.socket.addEventListener('error', (event) => {
-            clearTimeout(timeout); // Clear the timeout if there's an error
-            this.main.set_status = 'WebSocket ' + param.name + ' error:';
-            if ("error" in param.callback)
-                param.callback.error();
-        });
-        param.socket.addEventListener('close', (event) => {
-            clearTimeout(timeout); // Clear the timeout if the connection is closed
-            this.main.set_status = 'WebSocket ' + param.name + ' connection closed:';
-            if ("close" in param.callback)
-                param.callback.close();
-        });
-    }
-
 }
