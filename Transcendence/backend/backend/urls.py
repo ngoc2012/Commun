a1c5eb133d8619db8cb9c4ac0cb7ca9f724ba781
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-12 11:34:01 +0100

    all

--- Transcendence/backend/backend/urls.py
+++ Transcendence/backend/backend/urls.py
@@ -19,4 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('lobby/', views.lobby, name='lobby'),
+    path('pong/', views.pong, name='pong'),
+    path('new_player/', views.new_player, name='new_player'),
 ]
