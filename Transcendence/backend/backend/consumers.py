2ada79c518d89cb639d8e89599c72e4574aa83a3
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-13 08:54:01 +0100

    all

--- Transcendence/backend/backend/consumers.py
+++ Transcendence/backend/backend/consumers.py
@@ -4,61 +4,31 @@ from views import rooms
 
 class GameConsumer(AsyncWebsocketConsumer):
     global rooms
-    #async def connect(self):
-    #    # Get the unique identifier from the query parameters or headers
-    #    user_id = self.scope.get('user')  # Adjust this based on your authentication method
 
-    #    # Create a unique room name based on the identifier
-    #    room_name = f"user_{user_id}"
-
-    #    self.room_name = room_name
-    #    self.room_group_name = f"room_{self.room_name}"
-
-    #    # Add the player to the room group
-    #    await self.channel_layer.group_add(
-    #        self.room_group_name,
-    #        self.channel_name
-    #    )
-
-    #    await self.accept()
     async def connect(self):
-        self.user = self.scope.get('user')  # Adjust this based on your authentication method
-        self.rooms = "rooms"
+        self.group_name = "rooms"
 
-        # Join room group
+        # Join group
         await self.channel_layer.group_add(
-            self.room_group_name,
+            self.group_name,
             self.channel_name
         )
-
-        # Update game state
-        await self.channel_layer.group_send(
-            self.rooms,
-            {
-                'type': 'update_state',
-                'state': updated_state_data,
-            }
-        )
-        
-        # Notify player join
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'player_join',
-                'player_count': updated_player_count,
-            }
-        )
-
-
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
-            self.room_group_name,
+            self.group_name,
             self.channel_name
         )
 
-    async def receive(self, text_data):
+    async def receive(self, data):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'game_update_state',
+                'state': self.state,
+            }
+        )
         # Handle user input (if needed)
         pass
 
