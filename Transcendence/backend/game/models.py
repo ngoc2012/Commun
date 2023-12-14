ac6810597fdaea22c781b32023ab98594a8806b6
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-14 08:34:01 +0100

    all

--- Transcendence/backend/game/models.py
+++ Transcendence/backend/game/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class SharedModel(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    #description = models.TextField()
+    id = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
