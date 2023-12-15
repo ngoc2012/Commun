07bf79899c3e081a66dff7e0e4a6bdf2404bd587
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-15 16:34:01 +0100

    all

--- Webserv/srcs/Request.cpp
+++ Webserv/srcs/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/15 16:28:27 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/15 16:33:29 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -188,7 +188,7 @@ bool	Request::read_content_type(std::string& s, std::string& c)
 	std::vector<std::string>	words;
 	if (pos != NPOS)
 	{
-		words = split_string(s.substr(pos + 14, 50), ";\n");
+		words = split_string(s.substr(pos + 14, 50), " ;\n");
 		std::cout << "|" << words[0] << "|" << std::endl;
 		for (int i = 0; i < 18; i++)
 			if (words[0] == std::string(types[i]))
