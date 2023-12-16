fb88ba2881d06938c8cbdf6a932c535accdb42d7
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-16 11:20:01 +0100

    all

--- Webserv/includes/Response.hpp
+++ Webserv/includes/Response.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/16 11:17:53 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/16 11:18:58 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ class	Response
 		std::ifstream	_upload_file;
 		std::ofstream	_download_file;
 		size_t		    _body_buffer;
+		size_t		    _body_max;
 
 		std::string	get_file_extension(std::string&);
 		size_t		get_file_size(std::string &file_name);
--- Webserv/srcs/Response.cpp
+++ Webserv/srcs/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/16 10:57:15 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/16 11:18:42 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ Response::Response()
 	_status_code = 200;
 	_location = 0;
 	_end = false;
+	_body_max = _host->get_client_max_body_size() * MEGABYTE;
+	_body_buffer = _host->get_client_body_buffer_size() * KILOBYTE;
 	//std::cout << "Response Default constructor" << std::endl;
 }
 Response::Response(const Response& src) { *this = src; }
