0a59faf64e49237e2f4f9c02fc61fef918f2b19c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-18 10:50:01 +0100

    all

--- Webserv/srcs/Response.cpp
+++ Webserv/srcs/Response.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/16 16:32:44 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/18 10:49:20 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ Response::~Response()
 	//std::cout << "Destruction response: " << _socket << std::endl;
 }
 
-int     Response::body()
+void     Response::body()
 {
 }
 
