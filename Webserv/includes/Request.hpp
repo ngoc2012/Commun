b53d1bac0dccd6bd3d1a5f26b27cc851d676256c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-18 13:56:01 +0100

    all

--- Webserv/includes/Request.hpp
+++ Webserv/includes/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/16 14:23:00 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/18 13:55:50 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,7 @@ class	Request
         std::string	get_body_in_header(void) const;
         std::string	get_content_type(void) const;
         size_t		get_body_size(void) const;
+        std::string	get_header(void) const;
 };
 
 #endif
