9d8b3ec39fd945ed7312ab973570b0daead5d3be
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-22 09:50:01 +0100

    all

--- Webserv/includes/Request.hpp
+++ Webserv/includes/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/22 09:47:55 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/22 09:48:03 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ class	Request
 
 		int		        _fd_in;
 		int		        _status_code;
-        char*   	    _buffer[REQUEST_BUFFER + 1];
+        char*   	    _buffer;
 		size_t		    _body_buffer;
 		size_t		    _body_max;
 
