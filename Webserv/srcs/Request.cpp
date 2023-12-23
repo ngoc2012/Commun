1aa29311b612c2406ea34ca65f62835432ed50c7
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-23 15:28:02 +0100

    all

--- Webserv/srcs/Request.cpp
+++ Webserv/srcs/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/23 15:25:20 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/23 15:27:43 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -217,7 +217,9 @@ bool	Request::read_content_length()
 	}
     //const char*   base = "0123456789";
     _content_length = ft::atoi_base(_header.substr(pos, pos1).c_str(), "0123456789");
-    std::cout << _header.substr(pos, pos1) << std::endl;
+    std::cout << pos << " " << pos1 << std::endl;
+    std::cout << "|" << _header.substr(pos, pos1) << "|" << std::endl;
+    std::cout << _header.substr(pos, pos1).c_str() << std::endl;
     std::cout << "_content_length: " << ft::atoi_base(_header.substr(pos, pos1).c_str(), "0123456789") << _content_length << std::endl;
     return (true);
 }
