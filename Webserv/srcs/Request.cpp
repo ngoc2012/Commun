d93e1058387cfd516a11193f0cb07ec166587527
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-23 16:28:01 +0100

    all

--- Webserv/srcs/Request.cpp
+++ Webserv/srcs/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/23 15:30:30 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/23 16:28:00 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -219,10 +219,11 @@ bool	Request::read_content_length()
     //const char*   base = "0123456789";
     //_content_length = ft::atoi_base(_header.substr(pos, pos1).c_str(), "0123456789");
     _content_length = std::atoi(_header.substr(pos, pos1).c_str());
-    std::cout << pos << " " << pos1 << std::endl;
-    std::cout << "|" << _header.substr(pos, pos1) << "|" << std::endl;
-    std::cout << _header.substr(pos, pos1).c_str() << std::endl;
-    std::cout << "_content_length: " << ft::atoi_base(_header.substr(pos, pos1).c_str(), "0123456789") << _content_length << std::endl;
+    //std::cout << pos << " " << pos1 << std::endl;
+    //std::cout << "|" << _header.substr(pos, pos1) << "|" << std::endl;
+    //std::cout << _header.substr(pos, pos1).c_str() << std::endl;
+    //std::cout << "_content_length: " << ft::atoi_base(_header.substr(pos, pos1).c_str(), "0123456789") << _content_length << std::endl;
+    std::cout << "_content_length: " << _content_length << std::endl;
     return (true);
 }
 
