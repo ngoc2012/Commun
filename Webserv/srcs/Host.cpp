d61c4a450b35d9c785675523b6500a2feea24798
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-26 14:28:01 +0100

    all

--- Webserv/srcs/Host.cpp
+++ Webserv/srcs/Host.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/26 13:57:14 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/26 14:27:52 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -286,12 +286,13 @@ void	Host::mimes(void)
     _mimes["avi"] = "video/x-msvideo";
 }
 
-int			Host::get_max_clients(void) const {return (_max_clients);}
-std::map<int, Server*>	Host::get_sk_server(void) const {return (_sk_server);}
-std::map<int, Request*>	Host::get_sk_request(void) const {return (_sk_request);}
+int			                        Host::get_max_clients(void) const {return (_max_clients);}
+std::map<int, Server*>	            Host::get_sk_server(void) const {return (_sk_server);}
+std::map<int, Request*>	            Host::get_sk_request(void) const {return (_sk_request);}
 //std::map<int, Response*>	Host::get_sk_response(void) const {return (_sk_response);}
-size_t			Host::get_client_max_body_size(void) const {return (_client_max_body_size);}
-size_t			Host::get_client_body_buffer_size(void) const {return (_client_body_buffer_size);}
+size_t			                    Host::get_client_max_body_size(void) const {return (_client_max_body_size);}
+size_t			                    Host::get_client_body_buffer_size(void) const {return (_client_body_buffer_size);}
+std::map<std::string, std::string>&	Host::get_mimes(void) const {return (&_mimes);}
 
 void			Host::set_client_max_body_size(size_t n) {_client_max_body_size = n;}
 void			Host::set_client_body_buffer_size(size_t n) {_client_body_buffer_size = n;}
