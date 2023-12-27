ddbd271484334210b081632de4dadea283128e46
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-27 14:48:01 +0100

    all

--- Webserv/srcs/str_replace.c
+++ Webserv/srcs/str_replace.c
@@ -6,17 +6,21 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 14:23:28 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/27 14:24:28 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/27 14:47:29 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 namespace ft {
 
-while ( pos != std::string::npos ) {
-			line.erase(pos, toFind.length());
-			line.insert(pos, toReplace);
-			pos = line.find(toFind, pos + toReplace.length());
-		}
+std::string str_replace(std::string src, std::string find, std::string rep)
+{
+    size_t  pos = src.find(find);
+    while ( pos != std::string::npos ) {
+        src.erase(pos, find.length());
+        src.insert(pos, rep);
+        pos = src.find(find, pos + rep.length());
+    }
+}
 
 }
