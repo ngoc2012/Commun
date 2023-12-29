7ecee11d4b4bc0c759e045e41c0a07c89108a929
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2023-12-29 21:06:01 +0100

    all

--- Webserv/srcs/Cgi.cpp
+++ Webserv/srcs/Cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/29 21:03:53 by ngoc             ###   ########.fr       */
+/*   Updated: 2023/12/29 21:04:20 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,11 +97,9 @@ void    Cgi::execute()
         int     fd_in = _request->get_fd_in();
         fseek(fd_in, 0, SEEK_SET);
         char    buffer[BUFFER_SIZE];
-
         size_t  bytesRead;
         while ((bytesRead = read(fd_in, buffer, BUFFER_SIZE)) > 0)
             write(pipe_in[1], buffer, bytesRead);
-        _request->set_fd_in(pipe_in[1]);
         _request->get_response()->set_fd_out(pipe_out[0]);
         waitpid(_pid, NULL, 0);
     }
