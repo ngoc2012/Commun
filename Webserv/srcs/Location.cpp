39f1e3bbf358157282dabbdff538b71014397a46
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-02 11:08:01 +0100

    all

--- Webserv/srcs/Location.cpp
+++ Webserv/srcs/Location.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/02 11:05:48 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/02 11:07:21 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,7 @@ std::string	Location::get_full_file_name(std::string url, std::string root)
     if (_index.size())
     {
         if (stat(file_name.c_str(), &info) == 0
-                && S_ISDIR(info.st_mode) && !_autoindex)
+                && S_ISDIR(info.st_mode))
             file_name += "/index.html";
         return (file_name);
     }
