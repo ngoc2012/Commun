20708681708aca1783b917dfdc5904833c6db1cc
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-02 16:34:02 +0100

    all

--- Webserv/srcs/Location.cpp
+++ Webserv/srcs/Location.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/02 11:34:54 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/02 16:33:48 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,7 @@ bool	Location::find_method(e_method m)
 	return (false);
 }
 
-std::string	Location::get_full_file_name(std::string url, std::string root)
+std::string	Location::get_full_file_name(std::string url, std::string root, e_method e)
 {
     std::string file_name;
 
