5b9929043bb7d897a2f84103655b1497c3783ad0
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-02 19:06:45 +0100

    all

--- Webserv/srcs/Location.cpp
+++ Webserv/srcs/Location.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/02 16:35:33 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/02 17:38:30 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,8 +94,13 @@ std::string	Location::get_full_file_name(std::string url, std::string root, e_me
         file_name += root;
     else
         file_name += _alias;
-    if (url.size() > _url.size())
-        file_name += url.substr(_url.size(), url.size() - 1);
+    file_name += "/";
+    int     pos = 0;
+    while (url[pos] = _url[pos])
+        pos++;
+    //if (url.size() > _url.size())
+    //    file_name += url.substr(_url.size(), url.size() - 1);
+    file_name += _url.substr(pos);
     if (_autoindex || e == PUT || e == POST)
         return (file_name);
     struct stat	info;
