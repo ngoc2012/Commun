d4c6dfb86474d8baae4da06683dde14cb916c279
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-03 14:12:57 +0100

    all

--- Webserv/includes/Address.hpp
+++ Webserv/includes/Address.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/03 11:57:57 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/03 11:59:40 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,21 @@ class	Address
 		std::string		        _ip_address;
 		short unsigned int	    _port;
 
+		Address();
 		Address(const Address&);
 		Address	&operator=(const Address& op);
 	public:
-		Address();
+		Address(std::string, short unsigned int);
 		virtual ~Address();
 
         void    push(Server*);
         int	    bind_addr(void);
-        std::string		    get_ip_address(void) const;
-        short unsigned int	get_port(void) const;
+
+        //std::string		    get_ip_address(void) const;
+        //short unsigned int	get_port(void) const;
+
+        //void                set_ip_address(std::string);
+        //void	            set_port(short unsigned int);
 };
 
 #endif
