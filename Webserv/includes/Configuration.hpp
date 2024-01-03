2b277d653448723a782e3d836beb9448e2334a84
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-03 14:28:02 +0100

    all

--- Webserv/includes/Configuration.hpp
+++ Webserv/includes/Configuration.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/03 11:11:06 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/03 14:26:04 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ class Configuration
 		static bool			    location_parser(std::string, Location*, std::vector<std::string>&);
 		static bool			    server_parser(std::string, Server*, std::vector<std::string>&);
 		static bool			    host_parser(std::string, Host*, std::vector<std::string>&);
+        static bool             add_server(Server*, std::map<std::string, Address*>&);
 
 		Configuration(const Configuration&);
 		Configuration	&operator=(const Configuration& op);
