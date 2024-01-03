6230e27746e23fee3100afb22f4df92c7ab846b1
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-03 10:46:01 +0100

    all

--- Webserv/includes/Configuration.hpp
+++ Webserv/includes/Configuration.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/03 10:42:49 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/03 10:45:52 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,13 @@ class Location;
 class Configuration
 {
 	private:
-		bool			listen(Server*, std::vector<std::string>);
-		void			conf_file_error(std::string, int);
-		std::string		remove_comments(std::string&);
-		std::string		remove_spaces_end(std::string&);
-		bool			location_parser(std::string, Location*, std::vector<std::string>&);
-		bool			server_parser(std::string, Server*, std::vector<std::string>&);
-		bool			host_parser(std::string, Host*, std::vector<std::string>&);
+		static bool			listen(Server*, std::vector<std::string>);
+		static void			conf_file_error(std::string, int);
+		static std::string		remove_comments(std::string&);
+		static std::string		remove_spaces_end(std::string&);
+		static bool			location_parser(std::string, Location*, std::vector<std::string>&);
+		static bool			server_parser(std::string, Server*, std::vector<std::string>&);
+		static bool			host_parser(std::string, Host*, std::vector<std::string>&);
 
 		Configuration(const Configuration&);
 		Configuration	&operator=(const Configuration& op);
