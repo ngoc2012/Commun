5c0e3f7a1cdd4c8110caece15d73bb0e6537fe5c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-05 21:34:01 +0100

    all

--- Webserv/srcs/ft/split_string.cpp
+++ Webserv/srcs/ft/split_string.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 22:14:49 by ngoc              #+#    #+#             */
-/*   Updated: 2023/12/23 08:27:06 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/05 21:33:45 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ namespace ft {
 std::vector<std::string>	split_string(const std::string& input, std::string delimiters)
 {
 	std::vector<std::string>	tokens;
-	std::string			token;
+	std::string			        token;
+    int                         start;
+    int                         end;
 
 	for (std::size_t i = 0; i < input.length(); ++i)
 		if (delimiters.find(input[i]) == std::string::npos)
