f7255873652918d55831ed5475e99f1b7dc37f04
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-05 18:26:01 +0100

    all

--- Webserv/srcs/RequestHeader.cpp
+++ Webserv/srcs/RequestHeader.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/05 18:23:19 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/05 18:25:08 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,8 +80,8 @@ std::string	    RequestHeader::parse_host_name()
     words = ft::split_string(_str->substr(last_pos, _pos), " 	");
     if (words.size() != 2)
     {
-        std::cout << words.size() << " "  << _str->substr(last_pos, _pos) << std::endl;
-        std::cout << words[0] << "|" << words[1] << "|" << words[2] << std::endl;
+        std::cout << words.size() << " "  << _str->substr(last_pos, _pos) << std::flush;
+        std::cout << words[0] << "|" << words[1] << "|" << words[2] << std::flush;
         std::cerr << "Error request header: Host line invalid." << std::endl;
         return ("");
     }
