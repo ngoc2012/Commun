abe159538a121be8252c2e147660cc28b5f9774e
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-05 21:40:01 +0100

    all

--- Webserv/srcs/ft/split_string.cpp
+++ Webserv/srcs/ft/split_string.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 22:14:49 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/05 21:36:16 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/05 21:38:53 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ namespace ft {
 std::vector<std::string>	split_string(const std::string& input, std::string delimiters)
 {
 	std::vector<std::string>	tokens;
-	std::string			        token;
     std::size_t                 start;
     std::size_t                 end;
 
@@ -28,17 +27,14 @@ std::vector<std::string>	split_string(const std::string& input, std::string deli
     while (start < input.size())
     {
 		if (delimiters.find(input[start]) != std::string::npos)
+        {
+            if (end > start)
+                tokens.push_back(input.substr(start, end));
             start++;
-	for (std::size_t i = 0; i < input.length(); ++i)
-			token += input[i];
-		else
-		{
-			if (token.length() > 0)
-			{
-				tokens.push_back(token);
-				token.clear();
-			}
-		}
+            end = start;
+        }
+        else
+            end++;
     }
 	if (token.length() > 0)
 		tokens.push_back(token);
