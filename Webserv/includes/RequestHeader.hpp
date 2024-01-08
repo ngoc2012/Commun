5989c1eac08655127ea80d088f03d38f610631cb
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-08 08:52:01 +0100

    all

--- Webserv/includes/RequestHeader.hpp
+++ Webserv/includes/RequestHeader.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:57:07 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/08 08:32:30 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/08 08:50:32 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ class	RequestHeader
         std::string     parse_host_name();
         std::string     parse_content_type();
         size_t          parse_content_length();
-        size_t          parse_transfer_encoding();
+        bool            parse_transfer_encoding();
 
         void	set_host(Host*);
         void    set_str(std::string*);
