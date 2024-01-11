f3e18711118f5f56e9f6a96abaef7a444093d37b
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-11 21:42:01 +0100

    all

--- Transcendence/backend/backend/models.py
+++ Transcendence/backend/backend/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+class PlayersModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    login = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    #session_id = models.CharField(max_length=40)
+    #expires = models.DateTimeField()
+    x = models.IntegerField(blank=True, null=True)
+    y = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class RoomsModel(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
@@ -11,16 +24,6 @@ class RoomsModel(models.Model):
     def __str__(self):
         return self.name
 
-class PlayersModel(models.Model):
-    id = models.AutoField(primary_key=True)
-    login = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    #session_id = models.CharField(max_length=40)
-    #expires = models.DateTimeField()
+class PlayerRoomModel(models.Model):
+    player = models.ForeignKey(PlayersModel, on_delete=models.CASCADE)
     room = models.ForeignKey(RoomsModel, on_delete=models.SET_NULL, null=True)
-    x = models.IntegerField(blank=True, null=True)
-    y = models.IntegerField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
