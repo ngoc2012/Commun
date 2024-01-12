d6a31cf1fdba37f8e6e2eca08fec1058f28ddd95
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-12 06:56:01 +0100

    all

--- Transcendence/backend/pong/urls.py
+++ Transcendence/backend/pong/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .consumers import PongConsumer
 
 app_name = 'pong'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('room/(?P<room_name>\w+)/$', PongConsumer.as_asgi()),
 ]
