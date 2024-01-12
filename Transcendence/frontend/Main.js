7e8da9a2b8005f6c52aeb1f90c9df88d05dd520c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-12 07:38:01 +0100

    all

--- Transcendence/frontend/Main.js
+++ Transcendence/frontend/Main.js
@@ -1,4 +1,5 @@
 import {Lobby} from './Lobby.js'
+import {Signup} from './Signup.js'
 
 export class Main
 {
@@ -10,11 +11,15 @@ export class Main
     constructor()
     {
         this.lobby = new Lobby(this);
+        this.signup = new Signup(this);
         this.dom_login = document.getElementById("login");
         this.dom_signup = document.getElementById("signup");
         this.dom_status = document.getElementById("status");
         this.dom_user_name = document.getElementById("user_name");
         this.dom_container = document.getElementById("container");
+        this.dom_signup.addEventListener("click", () => {
+            this.load('/signup', () => this.signup.events());
+        });
     }
 
     new_player() {
