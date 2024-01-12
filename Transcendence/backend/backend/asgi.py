1d70a7cb2dbaf512c921741d72eb824845d1369c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-12 20:08:01 +0100

    all

--- Transcendence/backend/backend/asgi.py
+++ Transcendence/backend/backend/asgi.py
@@ -17,18 +17,38 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.urls import path
 from .consumers import RoomsConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = ProtocolTypeRouter({
-    # Django's ASGI application to handle traditional HTTP requests
-    "http": get_asgi_application(),
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+#
+#application = ProtocolTypeRouter({
+#    # Django's ASGI application to handle traditional HTTP requests
+#    "http": get_asgi_application(),
+#
+#    # WebSocket handler
+#    "websocket": AllowedHostsOriginValidator(
+#        AuthMiddlewareStack(
+#            URLRouter([
+#                path("ws/rooms/", RoomsConsumer.as_asgi()),
+#            ])
+#        )
+#    ),
+#})
 
-    # WebSocket handler
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter([
-                path("ws/rooms/", RoomsConsumer.as_asgi()),
-            ])
-        )
-    ),
-})
+
+from chat.routing import websocket_urlpatterns
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+import chat.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
