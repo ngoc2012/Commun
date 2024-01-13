cbf29e2d43716e2df546294e04c166af402a2b40
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-13 06:14:01 +0100

    all

--- Transcendence/backend/game/consumers.py
+++ Transcendence/backend/game/consumers.py
@@ -1,6 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from game.models import RoomsModel
+from .models import RoomsModel
 
 class RoomsConsumer(AsyncWebsocketConsumer):
 
