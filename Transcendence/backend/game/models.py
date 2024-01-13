49bee72a9e51fa2438fa203167aa960e669fabe5
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-13 20:48:01 +0100

    all

--- Transcendence/backend/game/models.py
+++ Transcendence/backend/game/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.utils import timezone
 
 class PlayersModel(models.Model):
     id = models.AutoField(primary_key=True)
@@ -20,11 +21,13 @@ class RoomsModel(models.Model):
     game = models.CharField(max_length=20)
     nplayers = models.IntegerField(blank=True, null=True)
     owner = models.ForeignKey(PlayersModel, on_delete=models.CASCADE)
-    expires = models.DateTimeField()
+    expiration = models.DateTimeField(default=timezone.now() + timezone.timedelta(days=1))
     x = models.IntegerField(blank=True, null=True)
     y = models.IntegerField(blank=True, null=True)
     def __str__(self):
         return self.id
+    def has_expired(self):
+        return self.expiration < timezone.now()
 
 class PlayerRoomModel(models.Model):
     player = models.ForeignKey(PlayersModel, on_delete=models.CASCADE)
