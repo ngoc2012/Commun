6eb59c2688fee6ce6c90b9f51622f5259fea10f6
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-13 06:20:01 +0100

    all

--- Transcendence/backend/game/routing.py
+++
@@ -1,9 +0,0 @@
-# chat/routing.py
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r"ws/game/rooms/$", consumers.RoomsConsumer.as_asgi()),
-    
-]
