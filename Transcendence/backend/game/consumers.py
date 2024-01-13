a11db314ec8e776e2107edc8770fd415a1b00d7c
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-13 07:04:01 +0100

    all

--- Transcendence/backend/game/consumers.py
+++ Transcendence/backend/game/consumers.py
@@ -3,9 +3,27 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 from .models import RoomsModel
 
+def update_rooms(event):
+    action = event['action']
+    if (action['action'] == "new"):
+        RoomsModel(
+            game="pong",
+            name=action['name'],
+            nplayers=1,
+            owner=action['owner']
+        ).save()
+    if (action['action'] == "delete"):
+        RoomsModel.objects.get(id=action['id']).delete()
+    return ([
+        {
+            "id": i.id,
+            "name": i.name
+        } for i in RoomsModel.objects.all()
+        ])
+        
 class RoomsConsumer(WebsocketConsumer):
 
-    async def connect(self):
+    def connect(self):
         self.accept()
         #self.group_name = "rooms"
         #await self.channel_layer.group_add(
@@ -13,35 +31,21 @@ class RoomsConsumer(WebsocketConsumer):
         #    self.channel_name
         #)
 
-    async def disconnect(self, close_code):
+    def disconnect(self, close_code):
         pass
         #await self.channel_layer.group_discard(
         #    self.group_name,
         #    self.channel_name
         #)
 
-    async def receive(self, text_data):
-        await self.channel_layer.group_send(
-            self.group_name,
-            {
-                'type': 'update_rooms',
-                'action': json.loads(text_data),
-            }
-        )
+    def receive(self, text_data):
+        self.send(text_data=json.dumps(update_rooms(json.loads(text_data))))
+        #await self.channel_layer.group_send(
+        #    self.group_name,
+        #    {
+        #        'type': 'update_rooms',
+        #        'action': json.loads(text_data),
+        #    }
+        #)
 
-    async def update_rooms(self, event):
-        action = event['action']
-        if (action['action'] == "new"):
-            RoomsModel(
-                game="pong",
-                name=action['name'],
-                nplayers=1,
-                owner=action['owner']
-            ).save()
-        if (action['action'] == "delete"):
-            RoomsModel.objects.get(id=action['id']).delete()
-        await self.send(text_data=json.dumps([
-            {
-                "id": i.id,
-                "name": i.name
-            } for i in RoomsModel.objects.all()]))
+
