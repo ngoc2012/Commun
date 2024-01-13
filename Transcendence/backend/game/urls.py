bd2828bb0891fc618d2ffab0ddc6044a80fc06cc
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-13 06:12:02 +0100

    all

---
+++ Transcendence/backend/game/urls.py
@@ -0,0 +1,11 @@
+# chat/urls.py
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("rooms/", views.room, name="room"),
+]
+
