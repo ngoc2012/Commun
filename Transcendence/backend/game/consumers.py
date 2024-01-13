5d2a1436285b920879f4d5e1bd14898c622fd799
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-13 10:16:01 +0100

    all

--- Transcendence/backend/game/consumers.py
+++ Transcendence/backend/game/consumers.py
@@ -32,10 +32,54 @@ class RoomsConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, close_code):
-        self.channel_layer().group_discard(
+        self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
         )
 
     def receive(self, text_data):
-        self.send(text_data=json.dumps(update_rooms(json.loads(text_data))))
+        data = json.loads(text_data)
+        self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type': 'update_rooms',
+                'action': data,
+            }
+        )
+        #self.send(text_data=json.dumps(update_rooms(json.loads(text_data))))
+
+    def update_rooms(self, event):
+        action = event['action']
+        if (action['action'] == "new"):
+            RoomsModel(
+                    game="pong",
+                    name=action['name'],
+                    nplayers=1,
+                    owner=action['owner']
+                    ).save()
+        if (action['action'] == "delete"):
+            RoomsModel.objects.get(id=action['id']).delete()
+        self.send(text_data=json.dumps([
+            {
+                "id": i.id,
+                "name": i.name
+            } for i in RoomsModel.objects.all()]))
+        #return ([
+        #    {
+        #        "id": i.id,
+        #        "name": i.name
+        #        } for i in RoomsModel.objects.all()
+        #    ])
+        #action = event['action']
+        #if action['action'] == "new":
+        #    # Your implementation for handling a new action
+        #elif action['action'] == "delete":
+        #    # Your implementation for handling a delete action
+
+        ## Send the updated room data to the group
+        #await self.send(text_data=json.dumps([
+        #    {
+        #        "id": i.id,
+        #        "name": i.name
+        #    } for i in RoomsModel.objects.all()]))
+
