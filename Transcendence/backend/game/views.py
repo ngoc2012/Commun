cb913b4f17fb93db820d30fc1d405eb64c5a0711
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-14 10:58:01 +0100

    all

--- Transcendence/backend/game/views.py
+++ Transcendence/backend/game/views.py
@@ -4,6 +4,21 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import RoomsModel, PlayersModel
 
 # Delete all instances of YourModel
+import uuid
+
+# Fetch all instances
+instances = RoomsModel.objects.all()
+
+# Update each instance's UUID to the standard format
+for instance in instances:
+    try:
+        instance.uuid_field = uuid.UUID(str(instance.uuid_field))
+        instance.save()
+    except ValueError:
+        # Handle the case where the UUID is not in a valid format
+        pass
+
+# Now you can delete all instances
 RoomsModel.objects.all().delete()
 
 @csrf_exempt
