60f48ce4d73e916dab53b407863c1badf21143d9
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-14 09:02:01 +0100

    all

--- Transcendence/backend/game/views.py
+++ Transcendence/backend/game/views.py
@@ -20,8 +20,6 @@ def new_player(request):
         nplayers=1,
         owner=owner
     ).save()
-    #if (action['action'] == "delete"):
-    #    RoomsModel.objects.get(id=action['id']).delete()
     return (JsonResponse({
         'id': new_game.id,
         'name': new_game.name
@@ -36,7 +34,9 @@ def delete(request):
     if not PlayersModel.objects.filter(login=request.POST['login']).exists():
         return (HttpResponse("Error: Login '" + request.POST['login'] + "' does not exist!"))
     owner = PlayersModel.objects.get(login=request.POST['login'])
-    if 
-    #    RoomsModel.objects.get(id=action['id']).delete()
-    #    RoomsModel.objects.get(id=action['id']).delete()
-    return (HttpResponse("Done"))
+    if not RoomsModel.objects.filter(id=request.POST['game_id']).exists():
+        return (HttpResponse("Error: Room with id '" + request.POST['game_id'] + "' does not exist!"))
+    room = RoomsModel.objects.get(id=request.POST['game_id'])
+    if room.owner == owner:
+        return (HttpResponse("Room " + room.id + " deleted"))
+    return (HttpResponse("Error: Login '" + request.POST['login'] + "' is not the owner of '" + request.POST['game_id'] + "'!"))
