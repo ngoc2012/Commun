89ae73eba0ab6ea13f40a7dafc525f959f0c2241
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-14 20:40:01 +0100

    all

--- Transcendence/backend/game/consumers.py
+++ Transcendence/backend/game/consumers.py
@@ -34,17 +34,17 @@ class RoomsConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         print(text_data)
         print(self.group_name)
+        rooms = RoomsModel.objects.all()
+        rooms_data = await room_list(rooms)
         await self.channel_layer.group_send(
             self.group_name,
             {
                 'type': 'chat.message',
-                'data': text_data
+                'data': rooms_data
             }
         )
     
     async def chat_message(self, event):
-        print(event)
+        #print(event)
         #await self.send(text_data='rooms_data')
-        rooms = RoomsModel.objects.all()
-        rooms_data = await room_list(rooms)
-        await self.send(text_data=rooms_data)
+        await self.send(text_data=event['data'])
