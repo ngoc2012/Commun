923f785d526a3a4abf4bab5ad748f0dd58cda8f1
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-14 13:26:02 +0100

    all

--- Transcendence/backend/game/views.py
+++ Transcendence/backend/game/views.py
@@ -35,7 +35,7 @@ def join(request):
         return (HttpResponse("Error: No login!"))
     if not PlayersModel.objects.filter(login=request.POST['login']).exists():
         return (HttpResponse("Error: Login '" + request.POST['login'] + "' does not exist!"))
-    owner = PlayersModel.objects.get(login=request.POST['login'])
+    #uuid_obj = UUID(uuid_str)
     if not RoomsModel.objects.filter(id=request.POST['game_id']).exists():
         return (HttpResponse("Error: Room with id '" + request.POST['game_id'] + "' does not exist!"))
     room = RoomsModel.objects.get(id=request.POST['game_id'])
@@ -54,6 +54,7 @@ def delete(request):
     if not PlayersModel.objects.filter(login=request.POST['login']).exists():
         return (HttpResponse("Error: Login '" + request.POST['login'] + "' does not exist!"))
     owner = PlayersModel.objects.get(login=request.POST['login'])
+    #uuid_obj = UUID(uuid_str)
     if not RoomsModel.objects.filter(id=request.POST['game_id']).exists():
         return (HttpResponse("Error: Room with id '" + request.POST['game_id'] + "' does not exist!"))
     room = RoomsModel.objects.get(id=request.POST['game_id'])
