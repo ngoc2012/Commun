624383f22f17f246e238b717f4d3c054e7979ca3
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-15 09:04:01 +0100

    all

--- Transcendence/backend/pong/views.py
+++ Transcendence/backend/pong/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from game.models import PlayersModel, PlayerRoomModel
+from game.models import RoomsModel, PlayersModel, PlayerRoomModel
 
 # Create your views here.
 def index(request):
@@ -10,6 +10,11 @@ def index(request):
 
 @csrf_exempt
 def state(request):
+    if 'game_id' not in request.POST:
+        return (HttpResponse("Error: No game id!"))
     if 'login' not in request.POST or request.POST['login'] == "":
         return (HttpResponse("Error: No login!"))
     player = PlayersModel.objects.get(login=request.POST['login'])
+    if not RoomsModel.objects.filter(id=request.POST['game_id']).exists():
+        return (HttpResponse("Error: Room with id '" + request.POST['game_id'] + "' does not exist!"))
+    room = RoomsModel.objects.get(id=request.POST['game_id'])
