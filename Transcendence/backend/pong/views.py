92b47e7fb26fe1a628720aae87e5b5d07f9a7fe9
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-15 10:26:01 +0100

    all

--- Transcendence/backend/pong/views.py
+++ Transcendence/backend/pong/views.py
@@ -29,9 +29,9 @@ def state(request):
         print(request.POST['action'])
         if request.POST['action'] == 'down' and player_room.y < pong_data['HEIGHT'] - pong_data['PADDLE_HEIGHT']:
             player_room.y += pong_data['STEP']
-            if room.owner == player:
+            if room.server == player:
                 room.y += pong_data['STEP']
         if request.POST['action'] == 'up' and player_room.y > 0:
             player_room.y -= pong_data['STEP']
-            if room.owner == player:
+            if room.server == player:
                 room.y -= pong_data['STEP']
