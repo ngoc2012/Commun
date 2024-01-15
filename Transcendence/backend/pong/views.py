2014b6841fa55149c33799d63e4544ec1c292423
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-15 17:56:01 +0100

    all

--- Transcendence/backend/pong/views.py
+++ Transcendence/backend/pong/views.py
@@ -25,18 +25,34 @@ def state(request):
     if not PlayerRoomModel.objects.filter(player=player,room=room).exists():
         return (HttpResponse("Error: Player is not playing this game!"))
     player_room = PlayerRoomModel.objects.get(player=player,room=room)
-    if room.game == 'pong':
-        #print(request.POST['action'])
-        if request.POST['action'] == 'down' and player_room.y < pong_data['HEIGHT'] - pong_data['PADDLE_HEIGHT']:
-            player_room.y += pong_data['STEP']
-            player_room.save()
-            if room.server == player:
-                room.y += pong_data['STEP']
-                room.save()
-        if request.POST['action'] == 'up' and player_room.y > 0:
-            player_room.y -= pong_data['STEP']
-            player_room.save()
-            if room.server == player:
-                room.y -= pong_data['STEP']
-                room.save()
+    #print(request.POST['action'])
+    if request.POST['action'] == 'down' and player_room.y < pong_data['HEIGHT'] - pong_data['PADDLE_HEIGHT']:
+        player_room.y += pong_data['STEP']
+        player_room.save()
+        if room.server == player:
+            room.y += pong_data['STEP']
+            room.save()
+    if request.POST['action'] == 'up' and player_room.y > 0:
+        player_room.y -= pong_data['STEP']
+        player_room.save()
+        if room.server == player:
+            room.y -= pong_data['STEP']
+            room.save()
     return (HttpResponse("Done"))
+
+@csrf_exempt
+def state(request):
+    if 'action' not in request.POST:
+        return (HttpResponse("Error: No action!"))
+    if 'game_id' not in request.POST:
+        return (HttpResponse("Error: No game id!"))
+    if 'login' not in request.POST or request.POST['login'] == "":
+        return (HttpResponse("Error: No login!"))
+    player = PlayersModel.objects.get(login=request.POST['login'])
+    if not RoomsModel.objects.filter(id=request.POST['game_id']).exists():
+        return (HttpResponse("Error: Room with id '" + request.POST['game_id'] + "' does not exist!"))
+    room = RoomsModel.objects.get(id=request.POST['game_id'])
+    if not PlayerRoomModel.objects.filter(player=player,room=room).exists():
+        return (HttpResponse("Error: Player is not playing this game!"))
+    player_room = PlayerRoomModel.objects.get(player=player,room=room)
+    if room.game == 'pong':
