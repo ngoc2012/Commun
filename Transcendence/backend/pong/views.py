944e026a5a0a45e15bd5ce5708bf4ab946f85938
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-15 18:12:01 +0100

    all

--- Transcendence/backend/pong/views.py
+++ Transcendence/backend/pong/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from game.models import RoomsModel, PlayersModel, PlayerRoomModel
+from asgiref.sync import sync_to_async
 
 from pong.data import pong_data
 from channels.layers import get_channel_layer
