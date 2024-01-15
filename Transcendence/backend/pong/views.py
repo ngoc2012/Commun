25208e09aacf2c15e6cc0972b8e897225158e2b6
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-15 09:02:01 +0100

    all

--- Transcendence/backend/pong/views.py
+++ Transcendence/backend/pong/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from game.models import PlayersModel, PlayerRoomModel
 
 # Create your views here.
 def index(request):
