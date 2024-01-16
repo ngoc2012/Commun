5f44ff5e1b988d215b7f2e1d413e689cdc938096
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-16 19:00:01 +0100

    all

--- Transcendence/backend/pong/consumers.py
+++ Transcendence/backend/pong/consumers.py
@@ -80,6 +80,7 @@ class PongConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         if text_data == 'start':
             await self.game_loop()
+        
         await self.channel_layer.group_send(
             self.room_id,
             {
