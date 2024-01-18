2ffd476ce96717ca6068147c34b3ad6ea26b8c54
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-18 18:18:02 +0100

    all

--- Transcendence (copy)/backend/game/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import PlayersModel, RoomsModel, PlayerRoomModel
-
-admin.site.register(PlayersModel)
-admin.site.register(RoomsModel)
-admin.site.register(PlayerRoomModel)
--- Transcendence (copy)/frontend/index.js
+++
@@ -1,7 +0,0 @@
-import {Main} from './Main.js'
-
-var main = new Main();
-
-document.addEventListener('DOMContentLoaded', () => {
-    main.load('/lobby', () => main.lobby.events());
-});
