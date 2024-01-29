e21de08c19458d88540b3f57cf5f10f9d3f63106
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-29 17:56:01 +0100

    all

--- CPP/CPP09/ex02/main.cpp
+++ CPP/CPP09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/29 14:45:37 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/29 17:54:26 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char **argv)
     PmergeMe    p;
 
     std::vector<int> a;
-    for (int i = 2; i < j + 3; i++)
+    for (int i = 1; i < argc; i++)
         a.push_back(std::atoi(argv[i]));
     p.set_a(&a);
     p.print();
