14eccde2c6caf4a0e94712eb4961b3fd377b5409
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-29 13:56:02 +0100

    all

--- CPP/CPP09/ex02/main.cpp
+++ CPP/CPP09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/29 13:51:50 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/29 13:54:45 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,12 @@ int	main(int argc, char **argv)
     PmergeMe    p;
     std::vector<int> a;
 
-	for (int i = 1; i < argc; i++)
+	for (int i = 2; i < argc; i++)
         a.push_back(std::atoi(argv[i]));
     p.set_a(&a);
     p.print();
-    p.insertInSortedArray();
+    p.insertInSortedArray(a, std::atoi(argv[1]));
+    p.print();
 
 	return (0);
 }
