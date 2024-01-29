350629a05a1bc8a4e45162f080b244d8e2163960
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-29 14:46:02 +0100

    all

--- CPP/CPP09/ex02/main.cpp
+++ CPP/CPP09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/29 14:36:47 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/29 14:45:37 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,19 @@ int	main(int argc, char **argv)
 {
     PmergeMe    p;
 
-    for (int j = 0; j < argc - 2; j++)
-    {
-        std::vector<int> a;
-        for (int i = 2; i < j + 3; i++)
-            a.push_back(std::atoi(argv[i]));
-        p.set_a(&a);
-        p.print();
-        std::sort(a.begin(), a.end());
-        p.print();
-        p.insertInSortedArray(a, std::atoi(argv[1]));
-        p.print();
-        if (p.isSorted(a))
-            std::cout << "Sorted" << std::endl;
-        else
-            std::cout << "Not sorted" << std::endl;
-    }
+    std::vector<int> a;
+    for (int i = 2; i < j + 3; i++)
+        a.push_back(std::atoi(argv[i]));
+    p.set_a(&a);
+    p.print();
+    std::sort(a.begin(), a.end());
+    p.print();
+    p.insertInSortedArray(a, std::atoi(argv[1]));
+    p.print();
+    if (p.isSorted(a))
+        std::cout << "Sorted" << std::endl;
+    else
+        std::cout << "Not sorted" << std::endl;
 
 	return (0);
 }
