1cabade0e66c1676b1c3bfed7abab359ed4901e0
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-29 06:16:01 +0100

    all

--- CPP/CPP07/ex00/whatever_.hpp
+++ CPP/CPP07/ex00/whatever_.hpp
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
 template< typename T >
 void    swap( T& a, T& b ) {
     T tmp = a;
