23af02b8a4856794285d0c984294716a1bb2de5f
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-29 21:22:01 +0100

    all

--- CPP/CPP09/ex02/PmergeMe.cpp
+++ CPP/CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/29 19:36:04 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/29 21:21:25 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void    PmergeMe<T>::sort()
 {
     for (size_t i = 0; i < _a->size() / 2 - 1; ++i)
         _p.push_back(PairedValue<T>((*_a)[i * 2], (*_a)[i * 2 + 1]));
+    std::sort(p._p.begin(), p._p.end());
 }
 
 template <typename T>
