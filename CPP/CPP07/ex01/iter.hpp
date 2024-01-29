a047ec1a82085b5967a4d1bb1c5f2940aaf76fc9
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-29 07:20:01 +0100

    all

--- CPP/CPP07/ex01/iter.hpp
+++ CPP/CPP07/ex01/iter.hpp
@@ -6,16 +6,15 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 06:19:06 by ngoc              #+#    #+#             */
-/*   Updated: 2024/01/29 07:14:03 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/01/29 07:19:36 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 template<typename T>
-void    swap(T& a, T& b)
+void    iter(T& a, size_t len, void (*f)(T &))
 {
-    T tmp = a;
-    a = b;
-    b = tmp;
+    for (size_t i = 0; i < len; i++)
+        f(a[i]);
 }
 
 template<typename T>
