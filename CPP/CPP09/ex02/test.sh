b34e002bb6301edc94c65aad851d73f7d0c218fe
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-01 09:26:01 +0100

    all

--- CPP/CPP09/ex02/test.sh
+++ CPP/CPP09/ex02/test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#for i in {5..5000}
-#do
-#    ./exe `shuf -i 1-100000 -n $i | tr "\n" " "`
-#    #valgrind ./exe `shuf -i 1-100000 -n $i | tr "\n" " "`
-#done
-
-for i in {2..5}
+for i in {1..5000}
 do
-    for j in {1..50}; do
-        ./exe `shuf -i 1-100000 -n $i | tr "\n" " "`
-    done
+    ./exe `shuf -i 1-100000 -n $i | tr "\n" " "`
+    #valgrind ./exe `shuf -i 1-100000 -n $i | tr "\n" " "`
 done
+
+#for i in {2..5}
+#do
+#    for j in {1..50}; do
+#        ./exe `shuf -i 1-100000 -n $i | tr "\n" " "`
+#    done
+#done
