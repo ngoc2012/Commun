77594827634ca80b21310723157617c4340fa60b
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-01 13:44:01 +0100

    all

--- CPP/CPP09/ex02/main.cpp
+++ CPP/CPP09/ex02/main.cpp
@@ -21,7 +21,7 @@ int	main(int argc, char **argv)
 {
     if (argc < 2)
         std::cout << "Use: ./exe 1 2 .. " << std::endl;
-    std::vector<PairedValue> a;
+    std::vector<PairedValue<int>> a;
 
     /*
     // Check insert sort
