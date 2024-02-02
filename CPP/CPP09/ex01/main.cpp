4087f3fa242eda08c1dfaa034af82a93d8b33e42
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-02 10:12:01 +0100

    all

--- CPP/CPP09/ex01/main.cpp
+++ CPP/CPP09/ex01/main.cpp
@@ -6,14 +6,14 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/02 10:09:33 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/02 10:10:47 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <iostream>
 #include <stack>
 #include <sstream>
 
-void    evaluateRPN(const std::string& expression)
+void    eval(const std::string& expression)
 {
     std::stack<int>         stack;
     std::istringstream      iss(expression);
@@ -74,6 +74,6 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    evaluateRPN(std::string(argv[1]));
+    eval(std::string(argv[1]));
     return 0;
 }
