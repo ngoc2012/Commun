b91727be5aab9d325f15ec0dc14a74776df07739
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-02 10:14:01 +0100

    all

--- CPP/CPP09/ex01/main.cpp
+++ CPP/CPP09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/02 10:10:47 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/02 10:12:15 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <iostream>
@@ -15,32 +15,32 @@
 
 void    eval(const std::string& expression)
 {
-    std::stack<int>         stack;
+    std::stack<int>         st;
     std::istringstream      iss(expression);
-    std::string             token;
+    std::string             c;
 
-    while (iss >> token) {
-        if (token == "+" || token == "-" || token == "*" || token == "/") {
-            if (stack.size() < 2)
+    while (iss >> c) {
+        if (c == "+" || c == "-" || c == "*" || c == "/") {
+            if (st.size() < 2)
             {
-                std::cerr << "Error: Insufficient operands for operator " << token << std::endl;
+                std::cerr << "Error: Insufficient operands for operator " << c << std::endl;
                 return ;
             }
 
-            int operand2 = stack.top();
-            stack.pop();
+            int operand2 = st.top();
+            st.pop();
 
-            int operand1 = stack.top();
-            stack.pop();
+            int operand1 = st.top();
+            st.pop();
 
             int result;
-            if (token == "+")
+            if (c == "+")
                 result = operand1 + operand2;
-            else if (token == "-")
+            else if (c == "-")
                 result = operand1 - operand2;
-            else if (token == "*")
+            else if (c == "*")
                 result = operand1 * operand2;
-            else if (token == "/") {
+            else if (c == "/") {
                 if (operand2 == 0)
                 {
                     std::cerr << "Error: Division by zero" << std::endl;
@@ -49,18 +49,18 @@ void    eval(const std::string& expression)
                 result = operand1 / operand2;
             }
 
-            stack.push(result);
+            st.push(result);
         }
         else
         {
             int number;
-            std::istringstream(token) >> number;
-            stack.push(number);
+            std::istringstream(c) >> number;
+            st.push(number);
         }
     }
 
-    if (stack.size() == 1)
-        std::cout << stack.top() << std::endl;
+    if (st.size() == 1)
+        std::cout << st.top() << std::endl;
     else
         std::cerr << "Error: Invalid expression" << std::endl;
 }
