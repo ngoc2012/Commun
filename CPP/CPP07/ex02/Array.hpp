3c54b244757c8754bad1db6930949f2ff4bfb180
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-13 22:40:01 +0100

    all

--- CPP/CPP07/ex02/Array.hpp
+++ CPP/CPP07/ex02/Array.hpp
@@ -38,6 +38,54 @@ class Array
     	};
 };
 
-#include "Array.tpp"
+template <typename T>
+Array<T>::Array()
+{
+	_a = new T();
+	_size = 0;
+}
+
+template <typename T>
+Array<T>::Array(size_t n)
+{
+	_a = new T[n];
+	_size = n;
+}
+
+template <typename T>
+Array<T>::Array(const Array& src)
+{
+	*this = src;
+}
+
+template <typename T>
+Array<T>&	Array<T>::operator=( Array const & src )
+{
+	for (size_t i = 0; i < src.size(); i++ )
+        _a[i] = src[i];
+	return (*this);
+}
+
+template <typename T>
+Array<T>::~Array() { delete [] _a; }
+
+template <typename T>
+T& Array<T>::operator[](size_t i) const
+{
+    if (i >= _size)
+        throw IndexError();
+    return _a[i];
+}
+
+template <typename T>
+size_t	Array<T>::size() const { return (_size);}
+
+template <typename T>
+std::ostream& operator<<( std::ostream& out, const Array<T>& a )
+{
+    for (size_t i = 0;i < a.size();i++)
+        out << a[i] << ", ";
+    return out;
+}
 
 #endif
