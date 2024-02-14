a4ca3097bbfd36068914d99317d07be6959376f6
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-14 17:50:01 +0100

    all

--- CPP/CPP09/ex00/main.cpp
+++ CPP/CPP09/ex00/main.cpp
@@ -6,15 +6,14 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/14 17:42:45 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/14 17:49:12 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <map>
 
-#include "Span.hpp"
+//#include "Span.hpp"
 
 int	main(int argc, char **argv)
 {
@@ -23,55 +22,22 @@ int	main(int argc, char **argv)
         std::cerr << "Usage: ./btc filename." << std::endl;
         return (1);
     }
-    struct stat	info;
-    if (stat(argv[1], &info) || S_ISDIR(info.st_mode))
+    std::ifstream	        f(argv[1]);
+	if (!f.is_open())
     {
-        std::cerr << "Error: File does not exist." << std::endl;
-        return (1);
-    }
+		std::cerr << "Error: Opening the file." << std::endl;
+		return (1);
+	}
 
-        
-    Span sp = Span(5);
-    
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
-
-    Span                s(argc - 1);
-    std::vector<int>    v;
-
-    for (int i = 1; i < argc; i++)
-        v.push_back(std::atoi(argv[i]));
-
-    try {
-        s.addNumber(v.begin(), v.end());
-    } catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-    
-    std::cout << s << std::endl;
-    try {
-        std::cout << "Shortest span: " << s.shortestSpan() << std::endl;
-        std::cout << "Longest span: " << s.longestSpan() << std::endl;
-    } catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-
-    try {
-        s.addNumber(-14);
-    } catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-
-    try {
-        std::cout << "Shortest span: " << s.shortestSpan() << std::endl;
-        std::cout << "Longest span: " << s.longestSpan() << std::endl;
-    } catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
+    std::string     line;
+    if (!std::getline(f, line))
+    {
+		std::cerr << "Error: File empty." << std::endl;
+		return (1);
+	}
+	while (std::getline(f, line))
+	{
+        std::cout << line << std::endl;
     }
     
 	return (0);
