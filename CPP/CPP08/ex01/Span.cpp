80f0b48b945d053ef0958a1a8ae94c466fde4ea2
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-14 09:00:02 +0100

    all

--- CPP/CPP08/ex01/Span.cpp
+++ CPP/CPP08/ex01/Span.cpp
@@ -6,45 +6,17 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 08:54:04 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/14 08:54:31 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/14 08:59:19 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-Span<T>::Span()
-{
-	_a = new T();
-	_size = 0;
-}
+#include "Span.hpp"
 
-template <typename T>
-Span<T>::Span(size_t n)
-{
-	_a = new T[n];
-	_size = n;
-	for (size_t i = 0; i < n; i++ )
-		_a[i] = 0;
-}
+Span::Span(unsigned int n): _N(n) {}
 
-template <typename T>
-Span<T>::Span(const Span& src)
-{
-	*this = src;
-}
+Span::~Span() {}
 
-template <typename T>
-Span<T>&	Span<T>::operator=( Span const & src )
-{
-	for (size_t i = 0; i < src.size(); i++ )
-		_a[i] = src[i];
-	return (*this);
-}
-
-template <typename T>
-Span<T>::~Span() { delete [] _a; }
-
-size_t	Span<T>::size() const { return (_size);}
-
-std::ostream& operator<<( std::ostream& out, const Span<T>& a )
+std::ostream& operator<<( std::ostream& out, const Span& a )
 {
 	if (!a.size())
 	{
