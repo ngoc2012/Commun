9a692690694da55bd8da5ce3f3c7fa70d9421e30
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-14 17:42:01 +0100

    all

--- CPP/CPP09/ex00/main.cpp
+++ CPP/CPP09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/14 17:37:20 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/14 17:41:14 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,15 @@ int	main(int argc, char **argv)
     if (argc != 2)
     {
         std::cerr << "Usage: ./btc filename." << std::endl;
+        return (1);
     }
+    std::string     file_name = std::string(argv[1]);
+    struct stat	info;
+    if (stat(argv[1], &info) || S_ISDIR(info.st_mode))
+        return (file_name);
+    if ()
+        return (file_name);
+
         
     Span sp = Span(5);
     
