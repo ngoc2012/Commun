88abc1c1e2cdbfc8dfdfdfceeca04cec5844ddde
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-14 10:02:01 +0100

    all

--- CPP/CPP08/ex01/main.cpp
+++ CPP/CPP08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/14 09:51:02 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/14 10:01:51 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int	main(int argc, char **argv)
     Span    s(argc - 1);
 
     for (int i = 1; i < argc; i++)
-        s.addNumber(std::atoi(argv[i]));
+    {
+        try { s.addNumber(std::atoi(argv[i]));}
+        catch {std::cout << e.what() << std::endl;}
+    }
     std::cout << s << std::endl;
     std::cout << "Shortest span: " << s.shortestSpan() << std::endl;
     std::cout << "Longest span: " << s.longestSpan() << std::endl;
