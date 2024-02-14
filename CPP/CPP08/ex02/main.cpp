ad06222a80f886ab51d4d0dd3f006ef77fd79f14
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-14 21:20:01 +0100

    all

--- CPP/CPP08/ex02/main.cpp
+++ CPP/CPP08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/14 21:08:00 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/14 21:18:38 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,6 @@ int	main()
         std::cout << *it << std::endl;
         ++it;
     }
-    //std::stack<int>     s(mstack);
+    std::stack<int>     s(mstack);
     return 0;
 }
