36feb1ed62f5e5c309d48bd736315534d379f700
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-14 07:26:01 +0100

    all

--- CPP/CPP08/ex00/main.cpp
+++ CPP/CPP08/ex00/main.cpp
@@ -6,21 +6,23 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2023/11/21 19:31:24 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/14 07:24:21 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <vector>
+#include <list>
 
 #include "Array.hpp"
 
 int	main()
 {
-	Array<int>	a_int(10);
+    std::vector<int>	a_int(10);
 
 	for (size_t i = 0; i < 10; i++)
 		a_int[i] = i + 1;
-	std::cout << a_int << std::endl;
+	std::cout << easyfind(a_int, 5) << std::endl;
 
 	Array<double>	a_d(10);
 	for (size_t i = 0; i < 10; i++)
