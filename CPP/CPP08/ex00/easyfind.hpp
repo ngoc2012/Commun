cbd33a72d533677d2812566cb512f3f381c504eb
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-14 07:34:01 +0100

    all

--- CPP/CPP08/ex00/easyfind.hpp
+++ CPP/CPP08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/14 07:22:17 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/14 07:33:07 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@
 template <typename T>
 T*    easyfind(T& c, int v )
 {
-    if (std::find(c.begin(), c.end(), v) != c.end())
-        return (c);
+    T*  o = std::find(c.begin(), c.end(), v);
+    if (o != c.end())
+        return (o);
     else
         return (0);
 }
--- CPP/CPP08/ex00/main.cpp
+++ CPP/CPP08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/14 07:31:46 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/14 07:33:38 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,13 @@
 int	main(int argc, char **argv)
 {
     std::vector<int>	v;
+    std::list<int>	    l;
 
 	for (size_t i = 0; i < 10; i++)
+    {
 		v.push_back(i + 1);
+		l.push_back(i + 1);
+    }
 
 	for (size_t i = 1; i < argc; i++)
     {
@@ -30,6 +34,10 @@ int	main(int argc, char **argv)
             std::cout << "Found " << n << " in vector" << std::endl;
         else
             std::cout << "Not found " << n << " in vector" << std::endl;
+        if (easyfind(l, n))
+            std::cout << "Found " << n << " in list" << std::endl;
+        else
+            std::cout << "Not found " << n << " in list" << std::endl;
     }
 
 	return (0);
