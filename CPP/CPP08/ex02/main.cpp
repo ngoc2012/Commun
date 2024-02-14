ba0ff70a93b655a308ad40a1ad4e7afdd0ab07a9
Minh ngoc NGUYEN <minh-ngu@paul-f3Br7s7.clusters.42paris.fr>
2024-02-14 14:58:01 +0100

    all

--- CPP/CPP08/ex02/main.cpp
+++ CPP/CPP08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/14 14:55:23 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2024/02/14 14:57:00 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,25 @@
 
 int	main()
 {
-	Array<int>	a_int(10);
-
-	for (size_t i = 0; i < 10; i++)
-		a_int[i] = i + 1;
-	std::cout << a_int << std::endl;
-
-	Array<double>	a_d(10);
-	for (size_t i = 0; i < 10; i++)
-		a_d[i] = i + 1;
-	std::cout << a_d << std::endl;
-
-	try {
-		a_int[10] = 5;
-	} catch (Array<int>::IndexError& e)
-	{
-		std::cerr << e.what() << std::endl;
-	}
-	return (0);
+    MutantStack<int>    mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+    MutantStack<int>::iterator  it = mstack.begin();
+    MutantStack<int>::iterator  ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int>     s(mstack);
+    return 0;
 }
