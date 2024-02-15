9e59815bb64c7824096e7722baf6ad396cbbbe44
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-15 17:54:01 +0100

    all

--- CPP/CPP09/ex00/BitcoinExchange.hpp
+++ CPP/CPP09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/15 14:19:51 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2024/02/15 17:52:21 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 class BitcoinExchange
 {
 	private:
-        std::map<std::string, float>    _prices;
+        std::map<int, float>        _prices;
 		
 		BitcoinExchange();
 		BitcoinExchange(const BitcoinExchange&);
