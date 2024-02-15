b315ef1622d262efec2a7b867b65f87d8923eef3
Minh ngoc NGUYEN <minh-ngu@paul-f3Br7s7.clusters.42paris.fr>
2024-02-15 10:36:01 +0100

    all

--- CPP/CPP09/ex00/main.cpp
+++ CPP/CPP09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/14 17:51:11 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/15 10:35:51 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <fstream> // Add this line for ifstream
 #include <map>
 
-//#include "Span.hpp"
+#include "BitcoinExchange.hpp"
 
 int	main(int argc, char **argv)
 {
@@ -24,20 +24,20 @@ int	main(int argc, char **argv)
         return (1);
     }
     std::ifstream	        f(argv[1]);
-	if (!f.is_open())
+    if (!f.is_open())
     {
-		std::cerr << "Error: Opening the file." << std::endl;
-		return (1);
-	}
+        std::cerr << "Error: Opening the file." << std::endl;
+        return (1);
+    }
 
     std::string     line;
     if (!std::getline(f, line))
     {
-		std::cerr << "Error: File empty." << std::endl;
-		return (1);
-	}
-	while (std::getline(f, line))
-	{
+        std::cerr << "Error: File empty." << std::endl;
+        return (1);
+    }
+    while (std::getline(f, line))
+    {
         std::cout << line << std::endl;
         std::cout << line.find(" | ") << std::endl;
     }
