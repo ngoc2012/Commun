8a7524ecfdb7b377f3e4cc8e155b5f2347f7a202
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-15 08:36:01 +0100

    all

--- CPP/CPP08/ex02/main.cpp
+++ CPP/CPP08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/15 08:33:44 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/15 08:34:34 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@ int	main()
 {
     std::cout << "============== std::list ===============" << std::endl;
     std::list<int>    mstack;
-    mstack.push(5);
-    mstack.push(17);
+    mstack.push_back(5);
+    mstack.push_back(17);
     std::cout << mstack.top() << std::endl;
     mstack.pop();
     std::cout << mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    mstack.push(0);
+    mstack.push_back(3);
+    mstack.push_back(5);
+    mstack.push_back(737);
+    mstack.push_back(0);
     std::list<int>::iterator  it = mstack.begin();
     std::list<int>::iterator  ite = mstack.end();
     ++it;
