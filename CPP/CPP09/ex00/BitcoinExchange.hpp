414c2ae2325cc3e73a8ba688ab2ad5b6a73bd8fc
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-15 11:40:01 +0100

    all

--- CPP/CPP09/ex00/BitcoinExchange.hpp
+++ CPP/CPP09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:50:42 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/15 11:16:42 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2024/02/15 11:39:38 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,10 @@ class BitcoinExchange
         float   exchange(std::string, float) const;
 
         class OutOfDate : public std::exception
+		{
+			public: virtual const char* what() const throw();
+		};
+        class DataError : public std::exception
 		{
 			public: virtual const char* what() const throw();
 		};
