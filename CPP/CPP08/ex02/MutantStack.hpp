cd07fe9a58a61fd7922e7b78b670c0e9b5a32ba3
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-15 06:46:01 +0100

    all

--- CPP/CPP08/ex02/MutantStack.hpp
+++ CPP/CPP08/ex02/MutantStack.hpp
@@ -6,11 +6,12 @@
 /*   By: ael-khni <ael-khni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 18:21:41 by ael-khni          #+#    #+#             */
-/*   Updated: 2022/08/25 14:30:38 by ael-khni         ###   ########.fr       */
+/*   Updated: 2024/02/15 06:45:30 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
 
 #include <iostream>
 #include <algorithm>
@@ -29,14 +30,15 @@ public:
     MutantStack( void ) {};
     ~MutantStack( void ) {};
 
-    MutantStack( const MutantStack& rhs ) { *this = rhs; }
-    MutantStack&    operator=( const MutantStack& rhs ) {
-        std::stack< T, Container >::operator=( rhs );
-        return *this;
-    }
+    MutantStack( const MutantStack& rhs );
+    MutantStack&    operator=( const MutantStack& rhs );
 
     typedef typename Container::iterator    iterator;
 
-    iterator    begin() { return this->c.begin(); }
-    iterator    end() { return this->c.end(); }
+    iterator    begin();
+    iterator    end();
 };
+
+#include "MutantStack.tpp"
+
+#endif
