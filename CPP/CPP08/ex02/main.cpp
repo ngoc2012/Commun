8defef781d34ae15abe5dd08703b970f7f9a8820
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-15 08:38:01 +0100

    all

--- CPP/CPP08/ex02/main.cpp
+++ CPP/CPP08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 19:17:48 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/15 08:34:34 by ngoc             ###   ########.fr       */
+/*   Updated: 2024/02/15 08:37:31 by ngoc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,18 @@
 int	main()
 {
     std::cout << "============== std::list ===============" << std::endl;
-    std::list<int>    mstack;
-    mstack.push_back(5);
-    mstack.push_back(17);
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << mstack.size() << std::endl;
-    mstack.push_back(3);
-    mstack.push_back(5);
-    mstack.push_back(737);
-    mstack.push_back(0);
-    std::list<int>::iterator  it = mstack.begin();
-    std::list<int>::iterator  ite = mstack.end();
+    std::list<int>    mstack0;
+    mstack0.push_back(5);
+    mstack0.push_back(17);
+    std::cout << mstack0.front() << std::endl;
+    mstack0.pop_front();
+    std::cout << mstack0.size() << std::endl;
+    mstack0.push_back(3);
+    mstack0.push_back(5);
+    mstack0.push_back(737);
+    mstack0.push_back(0);
+    std::list<int>::iterator  it = mstack0.begin();
+    std::list<int>::iterator  ite = mstack0.end();
     ++it;
     --it;
     while (it != ite)
@@ -38,7 +38,7 @@ int	main()
         std::cout << *it << std::endl;
         ++it;
     }
-    std::stack<int>     s(mstack);
+    //std::stack<int>     s(mstack0);
 
     std::cout << "============== MutantStack ===============" << std::endl;
     MutantStack<int>    mstack;
