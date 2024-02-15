541b1a7115b542378d0342681ab40bc92d006b36
Minh ngoc NGUYEN <minh-ngu@paul-f3Br7s7.clusters.42paris.fr>
2024-02-15 11:20:01 +0100

    all

--- CPP/CPP09/ex00/BitcoinExchange.cpp
+++ CPP/CPP09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: ngoc <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 08:54:04 by ngoc              #+#    #+#             */
-/*   Updated: 2024/02/15 11:15:06 by minh-ngu         ###   ########.fr       */
+/*   Updated: 2024/02/15 11:19:58 by minh-ngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,6 @@ BitcoinExchange::BitcoinExchange(const char *data)
 
 BitcoinExchange::~BitcoinExchange() {}
 
+float   BitcoinExchange::exchange(std::string date, float b) const
+{
+}
