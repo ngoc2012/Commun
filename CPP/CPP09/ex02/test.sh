32d09115f1dea3861e4125205c5f20295799138e
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-02-16 11:12:01 +0100

    all

--- CPP/CPP09/ex02/test.sh
+++ CPP/CPP09/ex02/test.sh
@@ -2,13 +2,13 @@
 
 for i in {1..5000}
 do
-    #./exe `shuf -i 1-100000 -n $i | tr "\n" " "`
-    valgrind ./exe `shuf -i 1-100000 -n $i | tr "\n" " "`
+    #./PmergeMe `shuf -i 1-100000 -n $i | tr "\n" " "`
+    valgrind ./PmergeMe `shuf -i 1-100000 -n $i | tr "\n" " "`
 done
 
 #for i in {2..5}
 #do
 #    for j in {1..5}; do
-#        ./exe `shuf -i 1-100000 -n $i | tr "\n" " "`
+#        ./PmergeMe `shuf -i 1-100000 -n $i | tr "\n" " "`
 #    done
 #done
